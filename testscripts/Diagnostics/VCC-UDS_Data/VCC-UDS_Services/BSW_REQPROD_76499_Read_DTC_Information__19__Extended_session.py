28b1f249d60b2d77d514b142d986984513e9f980
Della Torre, Lorenzo <lorenzo.della.torre@volvocars.com>
2019-03-25 13:22:06 +0100

    Upload New File

---
+++ testscripts/Diagnostics/VCC-UDS_Data/VCC-UDS_Services/BSW_REQPROD_76499_Read_DTC_Information__19__Extended_session.py
@@ -0,0 +1,323 @@
+#inspired by https://grpc.io/docs/tutorials/basic/python.html
+
+# Copyright 2015 gRPC authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""The Python implementation of the gRPC route guide client."""
+
+from __future__ import print_function
+from datetime import datetime
+import threading
+from threading import Thread
+
+import random
+import time
+
+import grpc
+import string
+
+import logging
+import os
+import sys
+#sys.path.append('generated')
+
+import common_pb2
+import volvo_grpc_network_api_pb2 
+import volvo_grpc_network_api_pb2_grpc
+import volvo_grpc_functional_api_pb2
+import volvo_grpc_functional_api_pb2_grpc
+
+from support_can import Support_CAN
+SC = Support_CAN()
+
+from support_test_odtb2 import Support_test_ODTB2
+SuTe = Support_test_ODTB2()
+
+# Global variable:
+testresult = True
+
+# precondition for test running:
+#  BECM has to be kept alive: start heartbeat
+def precondition(stub, s, r, ns):
+    global testresult
+    
+    # start heartbeat, repeat every 0.8 second
+    SC._heartbeat = True
+    t = Thread (target=SC.send_heartbeat, args = (stub, "EcmFront1NMFr", "Front1CANCfg1", b'\x20\x40\x00\xFF\x00\x00\x00\x00',0.8))
+    t.daemon = True
+    t.start()
+    # wait for BECM to wake up
+    time.sleep(5)
+    # Register signals
+    
+    #messages = list()
+    can_send = "Vcu1ToBecmFront1DiagReqFrame"
+    can_rec = "BecmToVcu1Front1DiagResFrame"
+    can_nspace = "Front1CANCfg1"
+    
+
+    # timeout = more than maxtime script takes
+    # needed as thread for registered signals won't stop without timeout
+    #timeout = 300   #seconds
+    timeout = 60   #seconds
+    SC.subscribe_signal(stub, s, r, ns, timeout)
+    #record signal we send as well
+    SC.subscribe_signal(stub, r, s, ns, timeout)
+    
+    print()
+    #step_0(stub, s, r, ns)
+    
+    print ("precondition testok:", testresult, "\n")
+    
+# teststep 0: Complete ECU Part/Serial Number(s)
+def step_0(stub, s, r, ns):
+    global testresult
+    can_m_send = b'\x22\xED\xA0'
+    can_mr_extra = ''
+    
+    stepno = 0
+    purpose = "Complete ECU Part/Serial Number(s)"
+    timeout = 5
+    min_no_messages = 1
+    max_no_messages = 0
+    
+    testresult = testresult and SuTe.teststep(stub, can_m_send, can_mr_extra, s, r, ns, stepno, purpose, timeout, min_no_messages, max_no_messages)
+    
+# teststep 1: Change to Extended session
+def step_1(stub, s, r, ns):
+    global testresult
+    
+    stepno = 1
+    purpose = "Change to Extended session"
+    timeout = 1
+    min_no_messages = 1
+    max_no_messages = 1
+
+    can_m_send = b'\x10\x03'
+    can_mr_extra = ''
+    
+    testresult = testresult and SuTe.teststep(stub, can_m_send, can_mr_extra, s, r, ns, stepno, purpose, timeout, min_no_messages, max_no_messages)
+    #time.sleep(1)
+
+
+# teststep 2: verify session
+def step_2(stub, s, r, ns):
+    global testresult
+    
+    stepno = 2
+    purpose = "Verify extended session"
+    timeout = 1
+    min_no_messages = 1
+    max_no_messages = 1
+
+    can_m_send = b'\x22\xF1\x86'
+    can_mr_extra = b'\x03'
+    
+    testresult = testresult and SuTe.teststep(stub, can_m_send, can_mr_extra, s, r, ns, stepno, purpose, timeout, min_no_messages, max_no_messages)
+    time.sleep(1)
+
+#support function for reading out DTC/DID data:
+    #services
+    #"DiagnosticSessionControl"=10
+    #"reportDTCExtDataRecordByDTCNumber"=19 06
+    #"reportDTCSnapdhotRecordByDTCNumber"= 19 04
+    #"reportDTCByStatusMask" = 19 02 + "confirmedDTC"=03 / "testFailed" = 00
+    #"ReadDataByIentifier" = 22
+         
+# teststep 3: verify that padded bytes in SF contain 0x00
+def step_3(stub, s, r, ns):
+    global testresult
+    #global can_frames
+    #global can_messages
+    
+    
+    #SC.can_m_send( "Read counters", b'\x0B\x45\x00') #Request current session
+    can_m_send = SC.can_m_send( "reportDTCSnapdhotRecordByDTCNumber", b'\x0B\x45\x00' , " ")
+    can_mr_extra = ''
+    #print(SC.can_m_send( "Read counters", b'\x0B\x45\x00'))
+    stepno = 3
+    purpose = "verify that DTC info are sent"
+    timeout = 1 #wait a second for reply to be send
+    min_no_messages = 0
+    max_no_messages = 0
+  
+    testresult = testresult and SuTe.teststep(stub, can_m_send, can_mr_extra, s, r, ns, stepno, purpose, timeout, min_no_messages, max_no_messages)
+    
+    #SuTe.test_message(SC.can_frames[r], teststring='0462F18601000000')
+    #print ("Step ", stepno, " teststatus:", testresult, "\n")
+    return(SC.can_frames[r])
+
+# teststep 4: verify session
+def step_4(stub, s, r, ns):
+    global testresult
+    
+    stepno = 4
+    purpose = "Verify Extended session"
+    timeout = 1
+    min_no_messages = 1
+    max_no_messages = 1
+
+    can_m_send = b'\x22\xF1\x86'
+    can_mr_extra = b'\x03'
+    
+    testresult = testresult and SuTe.teststep(stub, can_m_send, can_mr_extra, s, r, ns, stepno, purpose, timeout, min_no_messages, max_no_messages)
+    time.sleep(1)
+
+# teststep 5: change to default session
+def step_5(stub, s, r, ns):
+    global testresult
+    
+    stepno = 5
+    purpose = "Change to Default session"
+    timeout = 1
+    min_no_messages = 1
+    max_no_messages = 1
+
+    can_m_send = b'\x10\x01'
+    can_mr_extra = ''
+    
+    testresult = testresult and SuTe.teststep(stub, can_m_send, can_mr_extra, s, r, ns, stepno, purpose, timeout, min_no_messages, max_no_messages)
+    time.sleep(1)
+
+# teststep 6: verify session
+def step_6(stub, s, r, ns):
+    global testresult
+    
+    stepno = 6
+    purpose = "Verify default session"
+    timeout = 1
+    min_no_messages = 1
+    max_no_messages = 1
+
+    can_m_send = b'\x22\xF1\x86'
+    can_mr_extra = b'\x01'
+    
+    testresult = testresult and SuTe.teststep(stub, can_m_send, can_mr_extra, s, r, ns, stepno, purpose, timeout, min_no_messages, max_no_messages)
+    time.sleep(1)
+
+def run():
+    global testresult
+
+    #start logging
+    # to be implemented
+    
+    # where to connect to signal_broker
+    #channel = grpc.insecure_channel('localhost:50051')
+    
+    # old Raspberry board Rpi 3B#channel
+    #channel = grpc.insecure_channel('10.247.249.204:50051')
+    
+    # new Raspberry-board Rpi 3B+
+    # ToDo: get IP via DNS
+    channel = grpc.insecure_channel('10.246.47.27:50051')
+    functional_stub = volvo_grpc_functional_api_pb2_grpc.FunctionalServiceStub(channel)
+    network_stub = volvo_grpc_network_api_pb2_grpc.NetworkServiceStub(channel)
+
+    can_send = "Vcu1ToBecmFront1DiagReqFrame"
+    can_receive = "BecmToVcu1Front1DiagResFrame"
+    can_namespace = "Front1CANCfg1"
+
+    # Test PreCondition
+    root = logging.getLogger()
+    root.setLevel(logging.DEBUG)
+    
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    root.addHandler(ch)
+    root.info('BEGIN:  %s' % os.path.basename(__file__))
+    
+    
+    print ("Testcase start: ", datetime.now())
+    starttime = time.time()
+    print ("time ", time.time())
+    print()
+    ############################################
+    # precondition
+    ############################################
+    precondition(network_stub, can_send, can_receive, can_namespace)
+    #print ("after precond active threads ", threading.active_count())
+    #print ("after precond thread enumerate ", threading.enumerate())
+
+    #subscribe_to_BecmFront1NMFr(network_stub)
+    
+    ############################################
+    # teststeps
+    ############################################
+    # step 1:
+    # action: change BECM to programming
+    # result: BECM reports mode
+    step_1(network_stub, can_send, can_receive, can_namespace)
+    
+    # step2:
+    # action: Request battery temp sensor error status
+    # result: BECM reports status as MF frame, padded bytes in last frame
+    step_2(network_stub, can_send, can_receive, can_namespace)
+
+    step_3(network_stub, can_send, can_receive, can_namespace)
+    step_4(network_stub, can_send, can_receive, can_namespace)
+    step_5(network_stub, can_send, can_receive, can_namespace)
+    step_6(network_stub, can_send, can_receive, can_namespace)
+    
+
+        
+    ############################################
+    # postCondition
+    ############################################
+            
+    print()
+    print ("time ", time.time())
+    print ("Testcase end: ", datetime.now())
+    print ("Time needed for testrun (seconds): ", int(time.time() - starttime))
+
+    
+    print ("Do cleanup now...")
+    print ("Stop heartbeat sent")
+    SC._heartbeat = False
+    #time.sleep(5)
+
+    #print ("Signals to unsubscribe")
+    #print ("Number of signals subscribed ", len(SC.can_subscribes))
+    #print ("Can signals subscribed to: ", SC.can_subscribes)
+    for unsubsc in SC.can_subscribes:
+        print ("unsubscribe signal: ", unsubsc)
+        SC.can_subscribes[unsubsc][0].cancel()
+        #print ("can_subscribes obj ", SC.can_subscribes[unsubsc][0])
+
+        print ("waiting for threads to finish")
+    time.sleep(5)
+    
+    print ("active threads remaining: " , threading.active_count())
+    #cleanup
+    #postcondition(network_stub)
+    while threading.active_count() > 1:
+        item =(threading.enumerate())[-1]
+        print ("thread to join ", item)
+        item.join(5)
+        time.sleep(5)
+        print ("active thread after join ", threading.active_count() )
+        print ("thread enumerate ", threading.enumerate())
+            
+    print ("Test cleanup end: ", datetime.now())
+    print()
+    if testresult:
+        print ("Testcase result: PASSED")
+    else:
+        print ("Testcase result: FAILED")
+
+    
+if __name__ == '__main__':
+    run()
+
