ade454079a91d8cbf2186ce930ffcd6148f10856
Weiler, Hans-Klaus <hans-klaus.weiler@volvocars.com>
2019-04-03 09:11:16 +0200

    Replace BSW_REQPROD_60019_N_Ar_Timeout_Non-Prog_Session.py

--- testscripts/Communication/DoCAN/BSW_REQPROD_60019_N_Ar_Timeout_Non-Prog_Session.py
+++ testscripts/Communication/DoCAN/BSW_REQPROD_60019_N_Ar_Timeout_Non-Prog_Session.py
@@ -30,14 +30,14 @@ import string
 import logging
 import os
 import sys
-#sys.path.append('generated')
+
+sys.path.append('generated')
 
 import volvo_grpc_network_api_pb2
 import volvo_grpc_network_api_pb2_grpc
 import volvo_grpc_functional_api_pb2
 import volvo_grpc_functional_api_pb2_grpc
 import common_pb2
-
 
 from support_can import Support_CAN
 SC = Support_CAN()
@@ -47,6 +47,8 @@ SuTe = Support_test_ODTB2()
 
 # Global variable:
 testresult = True
+
+
 
 # precondition for test running:
 #  BECM has to be kept alive: start heartbeat
@@ -93,17 +95,10 @@ def step_0(stub, s, r, ns):
     purpose = "Complete ECU Part/Serial Number(s)"
     timeout = 5
     min_no_messages = 1
-    max_no_messages = 0
+    max_no_messages = 1
     
     testresult = testresult and SuTe.teststep(stub, can_m_send, can_mr_extra, s, r, ns, stepno, purpose, timeout, min_no_messages, max_no_messages)
-    #print(SuTe.PrettyP_serial_number ("ECU Serial Number", SC.can_messages[r][0][2][144:152]))
-    print(SuTe.PrettyP ("Application_Diagnostic_Database", SC.can_messages[r][0][2][14:28]), "\n")
-    print(SuTe.PrettyP ("ECU_Core_Assembly", SC.can_messages[r][0][2][32:46]), "\n")
-    print(SuTe.PrettyP ("ECU_Delivery_Assembly", SC.can_messages[r][0][2][50:64]), "\n")
-    print(SuTe.PrettyP ("SWLM", SC.can_messages[r][0][2][70:84]), "\n")
-    print(SuTe.PrettyP ("SWL2", SC.can_messages[r][0][2][84:98]), "\n")
-    print(SuTe.PrettyP ("SWP1", SC.can_messages[r][0][2][98:112]), "\n")
-    print(SuTe.PrettyP ("SWL3", SC.can_messages[r][0][2][112:126]), "\n")
+    print(SuTe.PP_CombinedDID_EDA0(SC.can_messages[r][0][2], title=''))
     
 # teststep 1: Change to Extended session
 def step_1(stub, s, r, ns):
@@ -119,15 +114,15 @@ def step_1(stub, s, r, ns):
     can_mr_extra = ''
     
     testresult = testresult and SuTe.teststep(stub, can_m_send, can_mr_extra, s, r, ns, stepno, purpose, timeout, min_no_messages, max_no_messages)
-    #time.sleep(1)
 
-    # teststep 2: verify session
+    
+# teststep 2: verify session
 def step_2(stub, s, r, ns):
     global testresult
     
     stepno = 2
     purpose = "Verify extended session"
-    timeout = 5
+    timeout = 1
     min_no_messages = -1
     max_no_messages = -1
 
@@ -135,352 +130,101 @@ def step_2(stub, s, r, ns):
     can_mr_extra = b'\x03'
     
     testresult = testresult and SuTe.teststep(stub, can_m_send, can_mr_extra, s, r, ns, stepno, purpose, timeout, min_no_messages, max_no_messages)
-    time.sleep(1)
+    #time.sleep(1)
     
-# teststep 3: request EDA0 - complete ECU part/serial number default session
+# teststep 3: request EDA0 - with FC delay < timeout 1000 ms
 def step_3(stub, s, r, ns):
     global testresult
-    #global FC_flag
-    #global FC_responses
-    #global FC_delay #in ms
-    #global BS
-    #global ST
-    global BS1
-    BS1 = 1
+    
+    stepno = 3
+    purpose = "request EDA0 - with FC delay < timeout 1000 ms"
+    timeout = 5
+    min_no_messages = -1
+    max_no_messages = -1
+
+    BS1 = 11
     BS=0
     ST=0
-    FC_flag = 48 #Wait
-    FC_delay = 800 #wait 900ms before sending FC frame back
-    FC_auto = False
+    FC_flag =   48 #continue to send
+    FC_delay = 950 #wait 800ms before sending FC frame back
+    FC_auto = True
 
     can_m_send = SC.can_m_send( "ReadDataByIentifier", b'\xED\xA0', "")
     can_mr_extra = ''
-    #can_mr_extra = b'\x02'
-    
-    stepno = 3
-    purpose = "request EDA0 - complete ECU part/serial number to get MF reply"
-    #timeout = 5
-    timeout = 0.1 #Don't wait - need to send FC frames
-    min_no_messages = 0
-    max_no_messages = 0
     
     #clear_all_can_frames()
-    #print ("Step2: all can frames for receiver : ", can_frames)
-    #rec_frames = len(can_frames[r])
     SC.change_MF_FC(r, BS, ST, FC_delay, FC_flag, FC_auto)
     testresult = testresult and SuTe.teststep(stub, can_m_send, can_mr_extra, s, r, ns, stepno, purpose, timeout, min_no_messages, max_no_messages)
-    #can_subscribes[r][5] += 1
-    #update_can_messages_1(stub, r)
-    #wait_for_First_frame(rec_frames, time.time(), r)
-    #print ("Step2: frames received ", len(can_frames[r]), "\n")
-    #print ("Step2a: all can frames for receiver : ", can_frames)
+
     if len(SC.can_frames[r]) == BS1:
+        print ("Timeout due to FC delay: ")
         print ("number of frames received as expected: ", len(SC.can_frames[r]))
     else:
         testresult = False
-        print("number of frames received not expected")
+        print("FAIL: Wrong number of frames received. Expeced", BS1, "Received:", len(SC.can_frames[r]))
     print ("Step3: frames received ", len(SC.can_frames[r]), "\n")
-
-# teststep 4: wait maxtime to send reply for first frame, send reply
+    
+# teststep 4: request EDA0 - with FC delay > timeout 1000 ms
 def step_4(stub, s, r, ns):
-    #global can_subscribes
-    #global FC_flag
-    #global FC_delay #in ms
-    #global BS
-    #global ST
-    global BS1
-    #global can_frames
     global testresult
+    
     stepno = 4
-    purpose = "Verify FC with number of WAIT frames"
-    #timeout = 5
-    #min_no_messages = 1
-    #max_no_messages = 0
-    
-    print ("Step ", stepno, ":")
-    print ("Purpose: ", purpose)
-    # do a loop:
-    # send intended number of FC Wait frames
-    #
-    # controll number of frames sent from ECU
+    purpose = "request EDA0 - with FC delay > timeout 1000 ms"
+    timeout = 5
+    min_no_messages = -1
+    max_no_messages = -1
 
-    FC_flag = 49    # b'\x31'
-    BS = 0          # b'\x00'
-    ST = 0          # b'\x00'
-    FC_delay = 800  # requirement: wait max 900ms before sending FC frame back
-    FC_auto = False
-    #max_delay = 254   #number of delays wanted
-    #max_delay = 24   #number of delays wanted
+    BS1 = 1
+    BS=0
+    ST=0
+    FC_flag =   48 #continue to send
+    FC_delay = 1050 #wait 800ms before sending FC frame back
+    FC_auto = True
+
+    can_m_send = SC.can_m_send( "ReadDataByIentifier", b'\xED\xA0', "")
+    can_mr_extra = ''
+    
+    #clear_all_can_frames()
     SC.change_MF_FC(r, BS, ST, FC_delay, FC_flag, FC_auto)
-    # delay for TS in ms: if TS_delay < 127, if TS_delay between 0xF1 and 0xF9 then 100-900 usec
-    # this one will allow delay bigger than 127 ms as needed for testing purposes
-    #if ((TS_delay > 0xF1) & (TS_delay < 0xFA)):
-    #    TSdelay = (TS_delay & 0x0F) / 10000
-    #else:
-    #    TSdelay = TS_delay / 1000
-    time.sleep(FC_delay/1000)
-    #print ("Step3a: frames received ", len(can_frames[r]), "\n")
-    SC.send_FC_frame(stub, s, ns, SC.can_subscribes[r][4], SC.can_subscribes[r][1], SC.can_subscribes[r][2])
-        #FC = threading.Timer(FC_delay/1000, send_FC_frame, (stub,can_send,can_namespace,FC_flag,BS,ST)).start()
-    #can_subscribes[r][5] += 1    
-    #print ("DelayNo.: ", FC_responses, ", Number of can_frames received: ", len(can_frames[r]))
-    #print ("Step3: frames received ", len(can_frames[r]), "\n")
-        #if len(can_frames[r]) ==  
-    #for i in can_frames[r]:
-        
-    #    print("frame time:", i[0], "\n")
-    #print ("Step3: frames: ", len(can_frames[r]), "\n")
+    testresult = testresult and SuTe.teststep(stub, can_m_send, can_mr_extra, s, r, ns, stepno, purpose, timeout, min_no_messages, max_no_messages)
+
+    if len(SC.can_frames[r]) == BS1:
+        print ("Timeout due to FC delay: ")
+        print ("number of frames received as expected: ", len(SC.can_frames[r]))
+    else:
+        testresult = False
+        print("FAIL: Wrong number of frames received. Expeced", BS1, "Received:", len(SC.can_frames[r]))
+    print ("Step4: frames received ", len(SC.can_frames[r]), "\n")
+
  
-# teststep 5: send flow control with WAIT flag (0x31), BS=0, ST=0
+# teststep 5: set back FC_delay to default
 def step_5(stub, s, r, ns):
-    #global FC_flag
-    #global FC_responses
-    #global BS
-    global BS1
-    #global ST
-    #global FC_delay
     global testresult
-    #global can_subscribes
-    #can_m_send = b'\x31\x00\x00\x00\x00\x00\x00\x00'
-    #can_mr_extra = ''
-
-    #can_m_send = b''
-    #can_mr_extra = ''
-
-
-    FC_flag = 48    # b'\x30'
-    BS = 2          # b'\x00'
-    ST = 0          # b'\x00'
-    FC_delay = 0
-    FC_auto = True
-    BS1 = BS + 1
+    
     stepno = 5
-    purpose = "Change FC to continue (0x30) and BS = 2"
-    timeout = 0.1
-    #min_no_messages = 0
-    #max_no_messages = 0
+    purpose = "set back FC_delay to default"
+    timeout = 5 
+    min_no_messages = -1
+    max_no_messages = -1
+
+    BS1 = 1
+    BS=0
+    ST=0
+    FC_flag =   48 #continue to send
+    FC_delay =  0 
+    FC_auto = True
     
     SuTe.print_test_purpose(stepno, purpose)
     #update_can_messages_2(stub, r)
     #print(can_frames, "\n")
-    time.sleep( SC.can_subscribes[r][3] / 1000)
     SC.change_MF_FC(r, BS, ST, FC_delay, FC_flag, FC_auto)
-    #send_FC_frame(stub,s,ns,FC_flag,BS,ST)
-    SC.send_FC_frame(stub, s, ns, SC.can_subscribes[r][4], SC.can_subscribes[r][1], SC.can_subscribes[r][2])
-    time.sleep(timeout)
-    SC.update_can_messages(r)
-    #can_subscribes[r][5] += 1
-    #print ("Step4: N# frames received ", len(can_frames[r])) 
-    if len(SC.can_frames[r]) == BS1:
-        print ("number of frames received as expected: ", len(SC.can_frames[r]))
-    else:
-        testresult = False
-        print("number of frames received not expected")
-    print ("Step5: frames received ", len(SC.can_frames[r]), "\n")
-    #print ("Step4: frames received ", can_frames[r], "\n")
-# teststep 6: wait maxtime to send reply for first frame, send reply
+
+
+# teststep 6: verify session
 def step_6(stub, s, r, ns):
-    
-    #global can_subscribes
-    #global FC_flag
-    #global FC_delay #in ms
-    #global BS
-    #global ST
-    #global can_frames
     global testresult
+    
     stepno = 6
-    purpose = "Verify FC with number of WAIT frames"
-    #timeout = 5
-    #min_no_messages = 1
-    #max_no_messages = 0
-    
-    print ("Step ", stepno, ":")
-    print ("Purpose: ", purpose)
-    # do a loop:
-    # send intended number of FC Wait frames
-    #
-    # controll number of frames sent from ECU
-    FC_flag = 49    # b'\x31'
-    BS = 0          # b'\x00'
-    ST = 0          # b'\x00'
-    FC_delay = 1000  # requirement: wait max 900ms before sending FC frame back
-    FC_auto = False
-    #max_delay = 254   #number of delays wanted
-    #max_delay = 24   #number of delays wanted
-    SC.change_MF_FC(r, BS, ST, FC_delay, FC_flag, FC_auto)
-    # delay for TS in ms: if TS_delay < 127, if TS_delay between 0xF1 and 0xF9 then 100-900 usec
-    # this one will allow delay bigger than 127 ms as needed for testing purposes
-    #if ((TS_delay > 0xF1) & (TS_delay < 0xFA)):
-    #    TSdelay = (TS_delay & 0x0F) / 10000
-    #else:
-    #    TSdelay = TS_delay / 1000
-    time.sleep(FC_delay/1000)
-    #print ("Step3a: frames received ", len(can_frames[r]), "\n")
-    SC.send_FC_frame(stub, s, ns, SC.can_subscribes[r][4], SC.can_subscribes[r][1], SC.can_subscribes[r][2])
-        #FC = threading.Timer(FC_delay/1000, send_FC_frame, (stub,can_send,can_namespace,FC_flag,BS,ST)).start()
-    #can_subscribes[r][5] += 1    
-    #print ("DelayNo.: ", FC_responses, ", Number of can_frames received: ", len(can_frames[r]))
-    #print ("Step3: frames received ", len(can_frames[r]), "\n")
-        #if len(can_frames[r]) ==  
-    #for i in can_frames[r]:
-        
-    #    print("frame time:", i[0], "\n")
-    #print ("Step3: frames: ", len(can_frames[r]), "\n")
-        
-    #    print("frame time:", i[0], "\n")  
-
-# teststep 7: send flow control with WAIT flag (0x31), BS=3, ST=0
-def step_7(stub, s, r, ns):
-    #global FC_flag
-    #global FC_responses
-    #global BS
-    global BS1
-    #global ST
-    #global FC_delay
-    global testresult
-
-    #can_m_send = b'\x31\x00\x00\x00\x00\x00\x00\x00'
-    #can_mr_extra = ''
-
-    #can_m_send = b''
-    #can_mr_extra = ''
-
-
-    FC_flag = 48    # b'\x30'
-    BS = 3          # b'\x00'
-    ST = 0          # b'\x00'
-    BS1 = 3
-    FC_delay = 0
-    FC_auto = True
-
-    stepno = 7
-    purpose = "Change FC to continue (0x30) and BS = 3"
-    timeout = 0.1
-    #min_no_messages = 0
-    #max_no_messages = 0
-    
-    SuTe.print_test_purpose(stepno, purpose)
-    #update_can_messages_2(stub, r)
-    #print(can_frames, "\n")
-    time.sleep( SC.can_subscribes[r][3] / 1000)
-    SC.change_MF_FC(r, BS, ST, FC_delay, FC_flag, FC_auto)
-    #send_FC_frame(stub,s,ns,FC_flag,BS,ST)
-    SC.send_FC_frame(stub, s, ns, SC.can_subscribes[r][4], SC.can_subscribes[r][1], SC.can_subscribes[r][2])
-    time.sleep(timeout)
-    #update_can_messages(stub, r)
-    #print ("Step4: N# frames received ", len(can_frames[r])) 
-    #print ("Step6: frames received ", can_frames[r])
-    print ("Step7: frames received ", len(SC.can_frames[r]), "\n")
-    if len(SC.can_frames[r]) == BS1:
-        print ("number of frames received as expected: ", len(SC.can_frames[r]))
-    else:
-        testresult = False
-        print("number of frames received not expected") 
-
-# teststep 8: wait maxtime to send reply for first frame, send reply
-def step_8(stub, s, r, ns):
-    
-    #global can_subscribes
-    #global FC_flag
-    #global FC_delay #in ms
-    #global BS
-    #global ST
-    #global can_frames
-    global testresult
-    stepno = 8
-    purpose = "Verify FC with number of WAIT frames"
-    #timeout = 5
-    #min_no_messages = 1
-    #max_no_messages = 0
-    
-    print ("Step ", stepno, ":")
-    print ("Purpose: ", purpose)
-    # do a loop:
-    # send intended number of FC Wait frames
-    #
-    # controll number of frames sent from ECU
-
-    FC_flag = 49    # b'\x31'
-    BS = 0          # b'\x00'
-    ST = 0          # b'\x00'
-    FC_delay = 1000  # requirement: wait max 900ms before sending FC frame back
-    FC_auto = False
-    #max_delay = 254   #number of delays wanted
-    #max_delay = 24   #number of delays wanted
-    SC.change_MF_FC(r, BS, ST, FC_delay, FC_flag, FC_auto)
-    # delay for TS in ms: if TS_delay < 127, if TS_delay between 0xF1 and 0xF9 then 100-900 usec
-    # this one will allow delay bigger than 127 ms as needed for testing purposes
-    #if ((TS_delay > 0xF1) & (TS_delay < 0xFA)):
-    #    TSdelay = (TS_delay & 0x0F) / 10000
-    #else:
-    #    TSdelay = TS_delay / 1000
-    time.sleep(FC_delay/1000)
-    #print ("Step3a: frames received ", len(can_frames[r]), "\n")
-    SC.send_FC_frame(stub, s, ns, SC.can_subscribes[r][4], SC.can_subscribes[r][1], SC.can_subscribes[r][2])
-        #FC = threading.Timer(FC_delay/1000, send_FC_frame, (stub,can_send,can_namespace,FC_flag,BS,ST)).start()
-    #can_subscribes[r][5] += 1    
-    #print ("DelayNo.: ", FC_responses, ", Number of can_frames received: ", len(can_frames[r]))
-    #print ("Step3: frames received ", len(can_frames[r]), "\n")
-        #if len(can_frames[r]) ==  
-    #for i in can_frames[r]:
-        
-    #    print("frame time:", i[0], "\n")
-    #print ("Step3: frames: ", len(can_frames[r]), "\n")
-        
-    #    print("frame time:", i[0], "\n") 
-
-# teststep 9: send flow control with WAIT flag (0x31), BS=0, ST=0
-def step_9(stub, s, r, ns):
-    #global FC_flag
-    #global FC_responses
-    #global BS
-    global BS1
-    #global ST
-    #global FC_delay
-    global testresult
-
-    #can_m_send = b'\x31\x00\x00\x00\x00\x00\x00\x00'
-    #can_mr_extra = ''
-
-    #can_m_send = b''
-    #can_mr_extra = ''
-
-
-    FC_flag = 48    # b'\x30'
-    BS = 0          # b'\x00'
-    ST = 0          # b'\x00'
-    FC_delay = 0
-    FC_auto = True
-
-    stepno = 9
-    purpose = "Change FC to continue (0x30) and BS = 0"
-    timeout = 0.1
-    #min_no_messages = 0
-    #max_no_messages = 0
-    
-    SuTe.print_test_purpose(stepno, purpose)
-    #update_can_messages_2(stub, r)
-    #print(can_frames, "\n")
-    time.sleep( SC.can_subscribes[r][3] / 1000)
-    SC.change_MF_FC(r, BS, ST, FC_delay, FC_flag, FC_auto)
-    #send_FC_frame(stub,s,ns,FC_flag,BS,ST)
-    SC.send_FC_frame(stub, s, ns, SC.can_subscribes[r][4], SC.can_subscribes[r][1], SC.can_subscribes[r][2])
-    time.sleep(timeout)
-    #update_can_messages(stub, r)
-    #print ("Step4: N# frames received ", len(can_frames[r])) 
-    #print ("Step6: frames received ", can_frames[r])
-    print ("Step9: frames received ", len(SC.can_frames[r]), "\n")
-    if len(SC.can_frames[r]) == BS1:
-        print ("number of frames received as expected: ", len(SC.can_frames[r]))
-    else:
-        testresult = False
-        print("number of frames received not expected")  
-
-# teststep 10: verify session
-def step_10(stub, s, r, ns):
-    global testresult
-    
-    stepno = 10
     purpose = "Verify extended session"
     timeout = 5
     min_no_messages = -1
@@ -492,11 +236,11 @@ def step_10(stub, s, r, ns):
     testresult = testresult and SuTe.teststep(stub, can_m_send, can_mr_extra, s, r, ns, stepno, purpose, timeout, min_no_messages, max_no_messages)
     time.sleep(1)
 
-# teststep 11: Change to default session
-def step_11(stub, s, r, ns):
+# teststep 7: Change to default session
+def step_7(stub, s, r, ns):
     global testresult
     
-    stepno = 11
+    stepno = 7
     purpose = "Change to Default session"
     timeout = 1
     min_no_messages = 1
@@ -508,11 +252,11 @@ def step_11(stub, s, r, ns):
     testresult = testresult and SuTe.teststep(stub, can_m_send, can_mr_extra, s, r, ns, stepno, purpose, timeout, min_no_messages, max_no_messages)
     #time.sleep(1)
 
-    # teststep 12: verify session
-def step_12(stub, s, r, ns):
+    # teststep 8: verify session
+def step_8(stub, s, r, ns):
     global testresult
     
-    stepno = 12
+    stepno = 8
     purpose = "Verify Default session"
     timeout = 5
     min_no_messages = -1
@@ -585,43 +329,34 @@ def run():
     step_2(network_stub, can_send, can_receive, can_namespace)
 
     # step3:
-    # action: send 'hard_reset' to BECM
-    # result: BECM acknowledges message
-    #step_3(network_stub, can_send, can_receive, can_namespace)
-    
-    # step4:
-    # action: check current session
-    # result: BECM reports default session
-    
-    #step_3(network_stub, can_send, can_receive, can_namespace)
-
-    #step_4(network_stub, can_send, can_receive, can_namespace)
-
-    #step_5(network_stub, can_send, can_receive, can_namespace)
-
+    # action: send request with FC_delay < timeout
+    # result: whole message received
     step_3(network_stub, can_send, can_receive, can_namespace)
 
-    #step_4_0(network_stub, can_send, can_receive, can_namespace)
-
-    #step_3(network_stub, can_send, can_receive, can_namespace)
-
+    # step4:
+    # action: send request with FC_delay > timeout
+    # result: only first frame received
     step_4(network_stub, can_send, can_receive, can_namespace)
 
+    # step5:
+    # action: restore FC_delay again
+    # result: 
     step_5(network_stub, can_send, can_receive, can_namespace)
 
+    # step6:
+    # action: check current session
+    # result: BECM reports extended session
     step_6(network_stub, can_send, can_receive, can_namespace)
 
+    # step 7:
+    # action: change BECM to default
+    # result: BECM reports mode
     step_7(network_stub, can_send, can_receive, can_namespace)
 
+    # step8:
+    # action: check current session
+    # result: BECM reports default session
     step_8(network_stub, can_send, can_receive, can_namespace)
-
-    step_9(network_stub, can_send, can_receive, can_namespace)
-
-    step_10(network_stub, can_send, can_receive, can_namespace)
-
-    step_11(network_stub, can_send, can_receive, can_namespace)
-
-    step_12(network_stub, can_send, can_receive, can_namespace)
 
     
     ############################################
