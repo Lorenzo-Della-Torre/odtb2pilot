3a4a72fe50e6f7ca7eed8c406e8b09396474eff3
Della Torre, Lorenzo <lorenzo.della.torre@volvocars.com>
2019-05-13 10:45:59 +0200

    Update BSW_REQPROD_74165_Service_diagnosticSessionControl__10__-_transition_from_and_to_programmi....py

--- testscripts/Diagnostics/VCC-UDS_Services/VCC-UDS_Services_Service_10/BSW_REQPROD_74165_Service_diagnosticSessionControl__10__-_transition_from_and_to_programmi....py
+++ testscripts/Diagnostics/VCC-UDS_Services/VCC-UDS_Services_Service_10/BSW_REQPROD_74165_Service_diagnosticSessionControl__10__-_transition_from_and_to_programmi....py
@@ -235,17 +235,6 @@ def run():
     can_receive = "BecmToVcu1Front1DiagResFrame"
     can_namespace = SC.nspace_lookup("Front1CANCfg1")
 
-    # Test PreCondition
-    root = logging.getLogger()
-    root.setLevel(logging.DEBUG)
-    
-    ch = logging.StreamHandler(sys.stdout)
-    ch.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    ch.setFormatter(formatter)
-    root.addHandler(ch)
-    root.info('BEGIN:  %s' % os.path.basename(__file__))
-    
     
     print ("Testcase start: ", datetime.now())
     starttime = time.time()
