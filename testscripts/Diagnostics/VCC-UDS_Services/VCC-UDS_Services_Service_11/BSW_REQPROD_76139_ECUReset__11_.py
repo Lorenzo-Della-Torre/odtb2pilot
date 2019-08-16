602fad5f4be0b90aa9ee562f61c3bd6aa4d14728
hweiler <hans-klaus.weiler@volvocars.com>
2019-08-16 17:13:43 +0200

    additional fixes due to interface change and update of support functions

--- testscripts/Diagnostics/VCC-UDS_Services/VCC-UDS_Services_Service_11/BSW_REQPROD_76139_ECUReset__11_.py
+++ testscripts/Diagnostics/VCC-UDS_Services/VCC-UDS_Services_Service_11/BSW_REQPROD_76139_ECUReset__11_.py
@@ -5,7 +5,8 @@
 # version:  1.0
 # reqprod:  76139 76140
 
-# #inspired by https://grpc.io/docs/tutorials/basic/python.html
+#inspired by https://grpc.io/docs/tutorials/basic/python.html
+
 # Copyright 2015 gRPC authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -20,33 +21,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
- 
-
 """The Python implementation of the gRPC route guide client."""
 
-from __future__ import print_function
 from datetime import datetime
-import threading
-from threading import Thread
-
-import random
 import time
-
-import grpc
-import string
-
 import logging
 import os
 import sys
 
-sys.path.append('generated')
-
-import volvo_grpc_network_api_pb2
-import volvo_grpc_network_api_pb2_grpc
-import volvo_grpc_functional_api_pb2
-import volvo_grpc_functional_api_pb2_grpc
-import common_pb2
 import ODTB_conf
+
+import random
+import grpc
+import string
+sys.path.append('generated')
 
 from support_can import Support_CAN
 SC = Support_CAN()
@@ -56,8 +44,6 @@ SuTe = Support_test_ODTB2()
 
 # Global variable:
 testresult = True
-
-
     
 # precondition for test running:
 #  BECM has to be kept alive: start heartbeat
@@ -65,22 +51,9 @@ def precondition(stub, s, r, ns):
     global testresult
     
     # start heartbeat, repeat every 0.8 second
-    SC._heartbeat = True
-    t = Thread (target=SC.send_heartbeat, args = (stub, "EcmFront1NMFr", "Front1CANCfg0", b'\x20\x40\x00\xFF\x00\x00\x00\x00',0.8))
-    t.daemon = True
-    t.start()
-    # wait for BECM to wake up
-    time.sleep(5)
-    # Register signals
-    
-    #messages = list()
-    #can_send = "Vcu1ToBecmFront1DiagReqFrame"
-    #can_rec = "BecmToVcu1Front1DiagResFrame"
-    #can_nspace = "Front1CANCfg0"
-    
+    SC.start_heartbeat(stub, "EcmFront1NMFr", "Front1CANCfg0", b'\x20\x40\x00\xFF\x00\x00\x00\x00', 0.8)        
 
     # timeout = more than maxtime script takes
-    # needed as thread for registered signals won't stop without timeout
     #timeout = 300   #seconds
     timeout = 60   #seconds
     SC.subscribe_signal(stub, s, r, ns, timeout)
@@ -88,13 +61,13 @@ def precondition(stub, s, r, ns):
     SC.subscribe_signal(stub, r, s, ns, timeout)
 
     # Parameters for FrameControl FC VCU
-    time.sleep(1)
-    BS=0
-    ST=0
-    FC_delay = 0 #no wait
-    FC_flag = 48 #continue send
-    FC_auto = False
-    SC.change_MF_FC(s, BS, ST, FC_delay, FC_flag, FC_auto)
+    #time.sleep(1)
+    #BS=0
+    #ST=0
+    #FC_delay = 0 #no wait
+    #FC_flag = 48 #continue send
+    #FC_auto = False
+    #SC.change_MF_FC(s, BS, ST, FC_delay, FC_flag, FC_auto)
     
     print()
     step_0(stub, s, r, ns)
@@ -421,30 +394,12 @@ def step_18(stub, s, r, ns):
 def run():
     global testresult
 
-    #start logging
-    # to be implemented
-    
     # where to connect to signal_broker
-    channel = grpc.insecure_channel(ODTB_conf.ODTB2_DUT + ':' + ODTB_conf.ODTB2_PORT)
-    
-    functional_stub = volvo_grpc_functional_api_pb2_grpc.FunctionalServiceStub(channel)
-    network_stub = volvo_grpc_network_api_pb2_grpc.NetworkServiceStub(channel)
+    network_stub = SC.connect_to_signalbroker(ODTB_conf.ODTB2_DUT, ODTB_conf.ODTB2_PORT)
 
     can_send = "Vcu1ToBecmFront1DiagReqFrame"
     can_receive = "BecmToVcu1Front1DiagResFrame"
-    can_namespace = "Front1CANCfg0"
-
-    # Test PreCondition
-    root = logging.getLogger()
-    root.setLevel(logging.DEBUG)
-    
-    ch = logging.StreamHandler(sys.stdout)
-    ch.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    ch.setFormatter(formatter)
-    root.addHandler(ch)
-    root.info('BEGIN:  %s' % os.path.basename(__file__))
-    
+    can_namespace = SC.nspace_lookup("Front1CANCfg0")    
     
     print ("Testcase start: ", datetime.now())
     starttime = time.time()
@@ -557,33 +512,16 @@ def run():
     print ("Testcase end: ", datetime.now())
     print ("Time needed for testrun (seconds): ", int(time.time() - starttime))
 
-    
     print ("Do cleanup now...")
-    print ("Stop heartbeat sent")
-    SC._heartbeat = False
+    print ("Stop all periodic signals sent")
+    #SC.stop_heartbeat()
+    SC.stop_periodic_all()
     #time.sleep(5)
 
-    #print ("Signals to unsubscribe")
-    #print ("Number of signals subscribed ", len(SC.can_subscribes))
-    #print ("Can signals subscribed to: ", SC.can_subscribes)
-    for unsubsc in SC.can_subscribes:
-        print ("unsubscribe signal: ", unsubsc)
-        SC.can_subscribes[unsubsc][0].cancel()
-        #print ("can_subscribes obj ", SC.can_subscribes[unsubsc][0])
-
-        print ("waiting for threads to finish")
-    time.sleep(5)
-    
-    print ("active threads remaining: " , threading.active_count())
-    #cleanup
-    #postcondition(network_stub)
-    while threading.active_count() > 1:
-        item =(threading.enumerate())[-1]
-        print ("thread to join ", item)
-        item.join(5)
-        time.sleep(5)
-        print ("active thread after join ", threading.active_count() )
-        print ("thread enumerate ", threading.enumerate())
+    # deregister signals
+    SC.unsubscribe_signals()
+    # if threads should remain: try to stop them 
+    SC.thread_stop()
             
     print ("Test cleanup end: ", datetime.now())
     print ()
