f9f64bb757a4b1d10def732522f0ca0780744ba1
hweiler <hans-klaus.weiler@volvocars.com>
2019-12-10 08:16:20 +0100

    new support functions for SBL and SecAccess

---
+++ Py_testenv/support_SBL.py
@@ -0,0 +1,73 @@
+# project:  ODTB2 testenvironment using SignalBroker
+# author:   hweiler (Hans-Klaus Weiler)
+# date:     2019-05-17
+# version:  1.0
+
+
+#inspired by https://grpc.io/docs/tutorials/basic/python.html
+
+# Copyright 2015 gRPC authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""The Python implementation of the gRPC route guide client."""
+
+#from __future__ import print_function
+from datetime import datetime
+import time
+
+import logging
+import os
+import sys
+
+import threading
+from threading import Thread
+
+import random
+
+import grpc
+import string
+
+
+#import network_api_pb2
+#import network_api_pb2_grpc
+#import functional_api_pb2
+#import functional_api_pb2_grpc
+#import system_api_pb2
+#import system_api_pb2_grpc
+#import common_pb2
+
+
+#class for supporting sending/receiving CAN frames
+class Support_CAN:
+
+# Global variables for use in SBL
+    #can_cf_received = [1554802159.4773512, 'BecmToVcu1Front1DiagResFrame', '30000A000000FCCF']
+    can_cf_received = dict()
+    can_frames = dict()
+    can_messages = dict()
+    can_subscribes = dict()
+    can_periodic = dict()
+    
+
+    _heartbeat = False
+
+    #init array for payload to send
+    can_mf_send = dict()
+    pl_array = []
+    
+    def add_pl_length(self, payload_value):
+        payload_value = bytes([len(payload_value)]) + payload_value
+        #print ("new payload: ", payload_value)
+        return(payload_value)
+
