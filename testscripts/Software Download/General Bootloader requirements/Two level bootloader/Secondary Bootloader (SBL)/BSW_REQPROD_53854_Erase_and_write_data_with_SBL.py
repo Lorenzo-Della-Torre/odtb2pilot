3be024c1aa2282aa70d7ea05396b2abbc7a08884
Della Torre, Lorenzo <lorenzo.della.torre@volvocars.com>
2019-12-16 07:50:29 +0100

    Replace BSW_REQPROD_53854_Erase_and_write_data_with_SBL.py

--- testscripts/Software Download/General Bootloader requirements/Two level bootloader/Secondary Bootloader (SBL)/BSW_REQPROD_53854_Erase_and_write_data_with_SBL.py
+++ testscripts/Software Download/General Bootloader requirements/Two level bootloader/Secondary Bootloader (SBL)/BSW_REQPROD_53854_Erase_and_write_data_with_SBL.py
@@ -3,7 +3,10 @@
 # author:   LDELLATO (Lorenzo Della Torre)
 # date:     2019-11-06
 # version:  1.0
-# #inspired by https://grpc.io/docs/tutorials/basic/python.html
+# reqprod:  53854
+
+#inspired by https://grpc.io/docs/tutorials/basic/python.html
+
 # Copyright 2015 gRPC authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -30,9 +33,13 @@ import struct
 import ODTB_conf
 from support_can import Support_CAN
 from support_test_odtb2 import Support_test_ODTB2
+from support_SBL import Support_SBL_Download
+from support_SecAcc import Support_Security_Access
 
 SC = Support_CAN()
 SUTE = Support_test_ODTB2()
+SSBL = Support_SBL_Download()
+SSA = Support_Security_Access()
 
 def precondition(stub, can_send, can_receive, can_namespace, result):
     """
@@ -94,7 +101,7 @@ def step_1(stub, can_send, can_receive, can_namespace, result):
                                       can_receive, can_namespace, stepno, purpose,
                                       timeout, min_no_messages, max_no_messages)
     
-    result = result and SUTE.PP_Decode_Routine_Control_response(SC.can_frames[can_receive][0][2], 'Type1,Completed')
+    result = result and SUTE.PP_Decode_Routine_Control_response(SC.can_messages[can_receive][0][2], 'Type1,Completed')
     return result
 
 def step_2(stub, can_send, can_receive, can_namespace, result):
@@ -122,50 +129,18 @@ def step_2(stub, can_send, can_receive, can_namespace, result):
 
 def step_3(stub, can_send, can_receive, can_namespace, result):
     """
-    Teststep 3: Security Access Request SID
+    Security Access Request SID
     """
-    global R
     stepno = 3
     purpose = "Security Access Request SID"
-    timeout = 0.05
-    min_no_messages = 1
-    max_no_messages = 1
-
-    can_m_send = b'\x27\x01'
-    can_mr_extra = ''
-    
-    result = result and SUTE.teststep(stub, can_m_send, can_mr_extra, can_send,
-                                      can_receive, can_namespace, stepno, purpose,
-                                      timeout, min_no_messages, max_no_messages)
-
-    R = SUTE.SetSecurityAccessPins(SC.can_messages[can_receive][0][2][6:12])
-    return result
+    result = result and SSA.Activation_Security_Access(stub, can_send, can_receive, can_namespace, stepno, purpose) 
+    return result 
 
 def step_4(stub, can_send, can_receive, can_namespace, result):
     """
-    Teststep 4: Security Access Send Key
+    Teststep 4:Flash Erase in PBL reply with Aborted
     """
-    global R
     stepno = 4
-    purpose = "Security Access Send Key"
-    timeout = 0.05
-    min_no_messages = -1
-    max_no_messages = -1
-
-    can_m_send = b'\x27\x02'+ R
-    can_mr_extra = ''
-    
-    result = result and SUTE.teststep(stub, can_m_send, can_mr_extra, can_send,
-                                      can_receive, can_namespace, stepno, purpose,
-                                      timeout, min_no_messages, max_no_messages)
-    result = result and SUTE.test_message(SC.can_messages[can_receive], '6702') 
-    return result 
-
-def step_5(stub, can_send, can_receive, can_namespace, result):
-    """
-    Teststep 5:Flash Erase in PBL reply with Aborted
-    """
-    stepno = 5
     purpose = "Flash Erase Routine reply Aborted in PBL"
 
     # Parameters for FrameControl FC
@@ -198,182 +173,33 @@ def step_5(stub, can_send, can_receive, can_namespace, result):
     
     return result
 
-def step_6(stub, can_send, can_receive, can_namespace, result):
+def step_5(stub, can_send, can_receive, can_namespace, result):
     """
-    Teststep 6: SBL Download 
+    Teststep 5: SBL Download 
     """
     global call
-    global sw_signature
+    stepno = 5
+    purpose = 'SBL Download'
+    tresult, call = SSBL.SBL_Download(stub, can_send, can_receive, can_namespace, stepno, purpose)
+    result = result and tresult
+    return result
+
+def step_6(stub, can_send, can_receive, can_namespace, result):
+    """
+    Teststep 6: Activate SBL
+    """
+    global call
     stepno = 6
-    PP_StringTobytes = SUTE.PP_StringTobytes
-    unpack = struct.unpack
-    purpose = "SBL Download"
+    purpose = "Activation of SBL"
+    result = result and SSBL.Activate_SBL(stub, can_send, can_receive, can_namespace, stepno, purpose,call)
 
-    # Parameters for FrameControl FC
-    BS=0
-    ST=0
-    FC_delay = 0 #no wait
-    FC_flag = 48 #continue send
-    FC_auto = False
-
-    """
-    Read vbf file for SBL download
-    """
-    if len(sys.argv) !=2:
-        print('Please specify VBF file')
-        quit(-1)
-
-    data = SUTE.main(sys.argv[1])
-    find = data.find
-    header_len = find(b'\x3B\x0D\x0A\x7D') + 4
-
-    if header_len < 100:
-        print ('Unknown format')
-        quit(-1)
-    off1 = find(b'sw_signature_dev = 0x') + 21
-    end = find(b';\r\n}')
-    #print(data[off1:end])
-    sw_signature = bytes.fromhex(str(data[off1 : end])[2:-1])
-    #print(sw_signature)
-    off2 = find(b'call = 0x') + 9
-    call = bytes.fromhex(str(data[off2 : off2 + 8])[2:-1])
-    out_hex = intelhex.IntelHex()
-    offset = header_len
-    
-    block_address = unpack('>L', data[offset: offset + 4])[0]
-    print(block_address)
-    
-
-    """
-    Iteration to Download the SBL by blocks
-    """
-    while offset < len(data):
-        [block_addr, block_len] = unpack('>2L', data[offset: offset + 8])
-        offset += 8
-        block_addr_by = PP_StringTobytes(hex(block_addr),4)
-        block_len_by = PP_StringTobytes(hex(block_len),4)
-
-        block_data = data[offset : offset + block_len]
-        offset += block_len
-        
-        crc = unpack('>H', data[offset : offset + 2])[0]
-        #print(hex(crc))
-        offset += 2
-      
-        crc_res = 'ok ' if SUTE.crc16(block_data) == crc else 'error'
-
-        print ("Block adr: 0x%X length: 0x%X crc %s" % (block_addr, block_len, crc_res))
-
-        timeout = 0.05
-        min_no_messages = -1
-        max_no_messages = -1
-        
-        can_m_send = b'\x34\x10\x44'+ block_addr_by + block_len_by
-        can_mr_extra = ''
-
-        SC.change_MF_FC(can_send, BS, ST, FC_delay, FC_flag, FC_auto)
-    
-        result = result and SUTE.teststep(stub, can_m_send, can_mr_extra, can_send,
-                                          can_receive, can_namespace, stepno, purpose,
-                                          timeout, min_no_messages, max_no_messages)
-        result = result and SUTE.test_message(SC.can_messages[can_receive], '74')
-
-        NBL = PP_StringTobytes(SC.can_frames[can_receive][0][2][6:10],2)
-        NBL = unpack('>H', NBL)[0]
-        print("NBL: ",NBL)
-        
-        """
-        Flash blocks to BECM with transfer data service 0x36
-        """
-        pad = 0
-        for i in range(int(block_len/(NBL-2))+1):
-        
-            pad = (NBL-2)*i
-            i += 1
-            ibyte = bytes([i])
-            timeout = 0.02
-            min_no_messages = -1
-            max_no_messages = -1
-
-            can_m_send = b'\x36' + ibyte + block_data[pad:pad + NBL-2]
-            
-            can_mr_extra = ''
-        
-            SC.change_MF_FC(can_send, BS, ST, FC_delay, FC_flag, FC_auto)
-            
-            result = result and SUTE.teststep(stub, can_m_send, can_mr_extra, can_send,
-                                              can_receive, can_namespace, stepno, purpose,
-                                              timeout, min_no_messages, max_no_messages)
-            result = result and SUTE.test_message(SC.can_messages[can_receive], '76')
-
-        """    
-        Transfer data exit with service 0x37
-        """
-        min_no_messages = 1
-        max_no_messages = 1
-        can_m_send = b'\x37'
-        can_mr_extra = ''    
-        result = result and SUTE.teststep(stub, can_m_send, can_mr_extra, can_send,
-                                          can_receive, can_namespace, stepno, purpose,
-                                          timeout, min_no_messages, max_no_messages)      
     return result
 
 def step_7(stub, can_send, can_receive, can_namespace, result):
     """
-    Teststep 7: Check Memory
+    Teststep 7:Flash Erase of PBL memory address is not allowed
     """
-    global sw_signature
     stepno = 7
-    purpose = "verify RoutineControl start are sent for Type 1"
-    timeout = 1 #wait a second for reply to be send
-    min_no_messages = -1
-    max_no_messages = -1
-
-    # Parameters for FrameControl FC
-    BS=0
-    ST=0
-    FC_delay = 0 #no wait
-    FC_flag = 48 #continue send
-    FC_auto = False
-
-    can_m_send = SC.can_m_send( "RoutineControlRequestSID",b'\x02\x12' + sw_signature, b'\x01')
-    can_mr_extra = ''
-    SC.change_MF_FC(can_send, BS, ST, FC_delay, FC_flag, FC_auto)
-            
-    result = result and SUTE.teststep(stub, can_m_send, can_mr_extra, can_send,
-                                      can_receive, can_namespace, stepno, purpose,
-                                      timeout, min_no_messages, max_no_messages)
-    
-    result = result and SUTE.PP_Decode_Routine_Control_response(SC.can_messages[can_receive][0][2], 'Type1,Completed')
-    print(SC.can_messages[can_receive])
-    return result
-
-def step_8(stub, can_send, can_receive, can_namespace, result):
-    """
-    Teststep 8: Activate SBL
-    """
-    global call
-    stepno = 8
-    purpose = "verify RoutineControl start are sent for Type 1"
-    timeout = 2 #wait a second for reply to be send
-    min_no_messages = -1
-    max_no_messages = -1
-
-    can_m_send = SC.can_m_send( "RoutineControlRequestSID",b'\x03\x01' + call, b'\x01')
-    can_mr_extra = ''
-
-    result = result and SUTE.teststep(stub, can_m_send, can_mr_extra, can_send,
-                                      can_receive, can_namespace, stepno, purpose,
-                                      timeout, min_no_messages, max_no_messages)
-    
-    result = result and SUTE.PP_Decode_Routine_Control_response(SC.can_messages[can_receive][0][2], 'Type1,Completed')
-    return result
-
-def step_9(stub, can_send, can_receive, can_namespace, result):
-    """
-    Teststep 9:Flash Erase of PBL memory address is not allowed
-    """
-    stepno = 9
     purpose = "Flash Erase of PBL memory address is not allowed"
 
     # Parameters for FrameControl FC
@@ -407,12 +233,12 @@ def step_9(stub, can_send, can_receive, can_namespace, result):
     print(SUTE.PP_Decode_7F_response(SC.can_frames[can_receive][0][2]))
     return result
 
-def step_10(stub, can_send, can_receive, can_namespace, result):
+def step_8(stub, can_send, can_receive, can_namespace, result):
     """
-    Teststep 10: Reset
+    Teststep 8: Reset
     """
     
-    stepno = 10
+    stepno = 8
     purpose = "ECU Reset"
     timeout = 1
     min_no_messages = -1
@@ -429,12 +255,12 @@ def step_10(stub, can_send, can_receive, can_namespace, result):
     time.sleep(1)
     return result
 
-def step_11(stub, can_send, can_receive, can_namespace, result):
+def step_9(stub, can_send, can_receive, can_namespace, result):
     """
-    Teststep 11: verify session
+    Teststep 9: verify session
     """
     
-    stepno = 11
+    stepno = 9
     purpose = "Verify Default session"
     timeout = 1
     min_no_messages = 1
@@ -522,16 +348,6 @@ def run():
     # action: verify RoutineControl start is sent for Type 1
     # result: BECM sends positive reply
     test_result = step_9(network_stub, can_send, can_receive, can_namespace, test_result)
-
-    # step 10:
-    # action: verify RoutineControl start is sent for Type 1
-    # result: BECM sends positive reply
-    test_result = step_10(network_stub, can_send, can_receive, can_namespace, test_result)
-    
-    # step 11:
-    # action: 
-    # result: BECM sends positive reply
-    test_result = step_11(network_stub, can_send, can_receive, can_namespace, test_result)
    
     ############################################
     # postCondition
