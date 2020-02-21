d361eec7ab11539773cd4d737e4c5ff73872cabb
Della Torre, Lorenzo <lorenzo.della.torre@volvocars.com>
2020-02-21 08:54:53 +0100

    Replace support_SecAcc.py

--- Py_testenv/support_SecAcc.py
+++ Py_testenv/support_SecAcc.py
@@ -5,7 +5,7 @@
 
 #inspired by https://grpc.io/docs/tutorials/basic/python.html
 
-# Copyright 2015 gRPC authors.
+# Copyright 2015 grPC authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,15 +15,13 @@
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# WITHOUT WArrANTIES Or CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""The Python implementation of the gRPC route guide client."""
+"""The Python implementation of the grPC route guide client."""
 
 import time
-from datetime import datetime
-import binascii
 
 from support_can import Support_CAN
 from support_test_odtb2 import Support_test_ODTB2
@@ -33,35 +31,41 @@ SUTE = Support_test_ODTB2()
 
 #class for supporting Security Access
 class Support_Security_Access:
+    """
+    class for supporting Security Access
+    """
 
     #Algorithm to decode the Security Access Pin
-    def SetSecurityAccessPins(self, Sid):
+    def set_security_access_pins(self, sid):
+        """
+        Algorithm to decode the Security Access Pin
+        """
         #iteration variable
         i = int
 
         #step1: load the challenge bytes, bit by bit, into a 64-bit variable space
         #insert fivefixed bytes and 3 seed
-        li = 'FFFFFFFFFF'        
-        li = li + Sid[4:6] + Sid[2:4] + Sid[0:2]
+        load = 'FFFFFFFFFF'
+        load = load + sid[4:6] + sid[2:4] + sid[0:2]
         # Test Pins
-        #li = '43BB42AA41'
-        #li = li + '8A' + '96' + '4E'
-        li = (bin(int(li,16)))
-        li = li[2:]
+        #load = '43BB42AA41'
+        #load = load + '8A' + '96' + '4E'
+        load = (bin(int(load, 16)))
+        load = load[2:]
         #Extension for Test Pins
-        #li = '0' + li
-        #print(hex(int(li[:8])))
-        li = li[::-1]
+        #load = '0' + load
+        #print(hex(int(load[:8])))
+        load = load[::-1]
 
         #step2: Load C541A9 hex into the 24 bit Initial Value variable space
-        lista = bin(int('C541A9',16))
+        lista = bin(int('C541A9', 16))
         lista = lista[2:]
 
-        #step3: Perform the Shift Right and Xor operations for 64 times
-        for i in li: 
-            
+        #step3: Perform the Shift right and Xor operations for 64 times
+        for i in load:
+
             lista1 = bin(lista[-1] != i)
-            lista1 = lista1[2:] 
+            lista1 = lista1[2:]
             # invert position of first bit with the last
             lista = lista1 + lista[:-1]
             # Xor between last reference list and last Sid arrow
@@ -73,38 +77,40 @@ class Support_Security_Access:
 
             lista11 = bin(lista[11] != lista1)
             lista11 = lista11[2:]
-           
+
             lista18 = bin(lista[18] != lista1)
             lista18 = lista18[2:]
-            
+
             lista20 = bin(lista[20] != lista1)
             lista20 = lista20[2:]
-           
-            lista = lista [:3] + lista3 + lista[4:8] + lista8 + lista[9:11] + lista11 + lista[12:18] + lista18 + lista[19:20] + lista20 + lista[21:24] 
 
-        #step4: Generate R1, R2, R3
-        R1 = hex(int(lista[12:20],2))
-        R1 = hex(int(R1,16) + int("0x200",16))
-        R1 = R1[3:]
-        #print(R1)
-        R2 = hex(int((lista[8:12] + lista[0:4]),2))
-        R2 = hex(int(R2,16) + int("0x200",16))
-        R2 = R2[3:]
-        #print(R2)
-        R3 = hex(int((lista[20:24] + lista[4:8]),2))
-        R3 = hex(int(R3,16) + int("0x200",16))
-        R3 = R3[3:]
-        #print(R3)
-        R = hex(int(('0x' + R1 + R2 + R3),16) + int("0x2000000",16))
-        R = R[3:]
-        print(R)
-        return bytes.fromhex(R)
-    
+            lista = lista [:3] + lista3 + lista[4:8] + lista8 + lista[9:11] +\
+                 lista11 + lista[12:18] + lista18 + lista[19:20] + lista20 + lista[21:24]
+
+        #step4: Generate r_1, r_2, r_3
+        r_1 = hex(int(lista[12:20], 2))
+        r_1 = hex(int(r_1, 16) + int("0x200", 16))
+        r_1 = r_1[3:]
+        #print(r_1)
+        r_2 = hex(int((lista[8:12] + lista[0:4]), 2))
+        r_2 = hex(int(r_2, 16) + int("0x200", 16))
+        r_2 = r_2[3:]
+        #print(r_2)
+        r_3 = hex(int((lista[20:24] + lista[4:8]), 2))
+        r_3 = hex(int(r_3, 16) + int("0x200", 16))
+        r_3 = r_3[3:]
+        #print(r_3)
+        r_0 = hex(int(('0x' + r_1 + r_2 + r_3), 16) + int("0x2000000", 16))
+        r_0 = r_0[3:]
+        print(r_0)
+        return bytes.fromhex(r_0)
+
     #Support function to activate the Security Access
-    def Activation_Security_Access(self, stub, can_send, can_rec, can_nspace, step_no, purpose):
+    def activation_security_access(self, stub, can_send, can_rec, can_nspace, step_no, purpose):
         """
-        Security Access Request SID
+        Support function to activate the Security Access
         """
+        #Security Access request sid
         testresult = True
         timeout = 0.05
         min_no_messages = 1
@@ -112,26 +118,24 @@ class Support_Security_Access:
 
         can_m_send = b'\x27\x01'
         can_mr_extra = ''
-    
+
         testresult = testresult and SUTE.teststep(stub, can_m_send, can_mr_extra, can_send,
-                                      can_rec, can_nspace, step_no, purpose,
-                                      timeout, min_no_messages, max_no_messages)
+                                                  can_rec, can_nspace, step_no, purpose,
+                                                  timeout, min_no_messages, max_no_messages)
 
-        R = self.SetSecurityAccessPins(SC.can_messages[can_rec][0][2][6:12])
+        r_0 = self.set_security_access_pins(SC.can_messages[can_rec][0][2][6:12])
 
-        """
-        Security Access Send Key
-        """
+        #Security Access Send Key
         timeout = 0.05
         min_no_messages = -1
         max_no_messages = -1
 
-        can_m_send = b'\x27\x02'+ R
+        can_m_send = b'\x27\x02'+ r_0
         can_mr_extra = ''
-    
+
         testresult = testresult and SUTE.teststep(stub, can_m_send, can_mr_extra, can_send,
-                                      can_rec, can_nspace, step_no, purpose,
-                                      timeout, min_no_messages, max_no_messages)
-        testresult = testresult and SUTE.test_message(SC.can_messages[can_rec], '6702') 
+                                                  can_rec, can_nspace, step_no, purpose,
+                                                  timeout, min_no_messages, max_no_messages)
+        testresult = testresult and SUTE.test_message(SC.can_messages[can_rec], '6702')
         time.sleep(1)
         return testresult
