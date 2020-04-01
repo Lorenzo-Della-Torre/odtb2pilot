90968a4191f4233af6710b1feca861a07b56b391
Della Torre, Lorenzo <lorenzo.della.torre@volvocars.com>
2020-04-01 14:10:24 +0200

    Replace BSW_REQPROD_83301_Physical_Layer.py

--- manual_test/SW_DL/VCC_Booth_UDSonCAN_HVBM/VCC_Booth_UDSonCAN/Physical_Layer_Req/BSW_REQPROD_83301_Physical_Layer.py
+++ manual_test/SW_DL/VCC_Booth_UDSonCAN_HVBM/VCC_Booth_UDSonCAN/Physical_Layer_Req/BSW_REQPROD_83301_Physical_Layer.py
@@ -1,28 +1,30 @@
-# Testscript ODTB2 MEPII
-# project:  BECM basetech MEPII
-# author:   LDELLATO (Lorenzo Della Torre)
-# date:     2020-03-10
-# version:  1.0
-# reqprod:  83301
+""" Testscript for an implicitly tested requirement (tested implicitly)
+    Testscript ODTB2 MEPII
+    project  BECM basetech MEPII
+    author   LDELLATO (Lorenzo Della Torre)
+    date     2020-04-01
+    version  1.0
+    reqprod  83301
 
-# #inspired by https://grpc.io/docs/tutorials/basic/python.html
-# Copyright 2015 gRPC authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+    #inspired by httpsgrpc.iodocstutorialsbasicpython.html
+    Copyright 2015 gRPC authors.
+
+    Licensed under the Apache License, Version 2.0 (the License);
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        httpwww.apache.orglicensesLICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an AS IS BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+"""
 
 import logging
-#message = "Testcase result: To be inspected"
-logging.basicConfig(filename='{}.log'.format((__file__)[:-3]),
-                    format='%(asctime)s - %(message)s', level=logging.INFO)
 
-logging.info("Testcase result: To be inspected")
+logging.basicConfig(filename='{}.log'.format((__file__)[-3]), format='%(asctime)s - %(message)s',
+                    level=logging.INFO)
+
+logging.info("Testcase result: tested implicitly by REQPRODs REQPRODs in LC: CAN Datailnk Layer –   SW reqs.")
