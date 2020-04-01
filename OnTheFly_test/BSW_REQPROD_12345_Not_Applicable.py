c660ea32a40ebd110607ffc6f1dcea5a2875cc16
Jansson, Fredrik (BSW) <fredrik.it.jansson@volvocars.com>
2020-04-01 13:15:10 +0200

    Created som templates

--- OnTheFly_test/BSW_REQPROD_12345_Not_Applicable.py
+++ OnTheFly_test/BSW_REQPROD_12345_Not_Applicable.py
@@ -1,4 +1,5 @@
-""" Testscript for an implicitly tested requirement (tested implicitly)
+""" Testscript for a requirement which is not applicable for us (Not applicable).
+    For example: Test multiple processors when you only have one. 
 
     Testscript  ODTB2 MEPII
     project     BECM basetech MEPII
@@ -7,17 +8,17 @@
     version     1.0
     reqprod     12345
 
-    Inspired by httpsgrpc.iodocstutorialsbasicpython.html
+    Inspired by https://grpc.io/docs/tutorials/basic/python.html
     Copyright 2015 gRPC authors.
 
-    Licensed under the Apache License, Version 2.0 (the License);
+    Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
     You may obtain a copy of the License at
 
-        httpwww.apache.orglicensesLICENSE-2.0
+    http://www.apache.org/licenses/LICENSE-2.0
 
     Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an AS IS BASIS,
+    distributed under the License is distributed on an "AS IS" BASIS,
     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
     See the License for the specific language governing permissions and
     limitations under the License.
@@ -25,7 +26,7 @@
 
 import logging
 
-logging.basicConfig(filename='{}.log'.format((__file__)[-3]), format='%(asctime)s - %(message)s',
+logging.basicConfig(filename='{}.log'.format((__file__)[:-3]), format='%(asctime)s - %(message)s',
                     level=logging.INFO)
 
-logging.info("Testcase result: Tested implicitly by REQ_12345")
+logging.info("Testcase result: Not applicable")
