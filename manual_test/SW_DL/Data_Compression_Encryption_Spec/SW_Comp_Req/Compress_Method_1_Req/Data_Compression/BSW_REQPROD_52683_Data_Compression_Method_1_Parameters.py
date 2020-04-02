8eec15477775ee89c0589730f8f823ca18d42466
Della Torre, Lorenzo <lorenzo.della.torre@volvocars.com>
2020-04-02 09:06:10 +0200

    Update BSW_REQPROD_52683_Data_Compression_Method_1_Parameters.py

--- manual_test/SW_DL/Data_Compression_Encryption_Spec/SW_Comp_Req/Compress_Method_1_Req/Data_Compression/BSW_REQPROD_52683_Data_Compression_Method_1_Parameters.py
+++ manual_test/SW_DL/Data_Compression_Encryption_Spec/SW_Comp_Req/Compress_Method_1_Req/Data_Compression/BSW_REQPROD_52683_Data_Compression_Method_1_Parameters.py
@@ -5,7 +5,7 @@
     author      LDELLATO (Lorenzo Della Torre)
     date        2020-04-02
     version     1.0
-    reqprod     53683
+    reqprod     52683
 
     Inspired by https://grpc.io/docs/tutorials/basic/python.html
     Copyright 2015 gRPC authors.
