ed9a0552f0dc0fb2ec90291bbd41a1eb8202c70e
Della Torre, Lorenzo <lorenzo.della.torre@volvocars.com>
2020-04-22 13:00:55 +0200

    Update BSW_REQPROD_53913_ECUs_with_multiple_networks_P319.py

--- test_cases/SW_DL/General_BL_Reqs/Additional_BL_Req_multi_Net/BSW_REQPROD_53913_ECUs_with_multiple_networks_P319.py
+++ test_cases/SW_DL/General_BL_Reqs/Additional_BL_Req_multi_Net/BSW_REQPROD_53913_ECUs_with_multiple_networks_P319.py
@@ -28,7 +28,7 @@ from datetime import datetime
 import sys
 import logging
 
-import Py_testenv.ODTB_conf
+import Py_testenv.ODTB_conf as ODTB_config
 from Py_testenv.support_can import Support_CAN
 from Py_testenv.support_test_odtb2 import Support_test_ODTB2
 from Py_testenv.support_SBL import Support_SBL
@@ -224,7 +224,7 @@ def run():
     # to be implemented
 
     # where to connect to signal_broker
-    network_stub = SC.connect_to_signalbroker(ODTB_conf.ODTB2_DUT, ODTB_conf.ODTB2_PORT)
+    network_stub = SC.connect_to_signalbroker(ODTB_config.ODTB2_DUT, ODTB_config.ODTB2_PORT)
 
     can_send = "Vcu1ToBecmFront1DiagReqFrame"
     can_receive = "BecmToVcu1Front1DiagResFrame"
