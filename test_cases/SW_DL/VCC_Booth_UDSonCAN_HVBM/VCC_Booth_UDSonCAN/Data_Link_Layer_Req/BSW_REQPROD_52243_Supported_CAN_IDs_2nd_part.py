ffaa239689492c31ac4aeae718dfba860ad4c1c7
Della Torre, Lorenzo <lorenzo.della.torre@volvocars.com>
2020-05-07 15:34:48 +0200

    Upload New File

---
+++ test_cases/SW_DL/VCC_Booth_UDSonCAN_HVBM/VCC_Booth_UDSonCAN/Data_Link_Layer_Req/BSW_REQPROD_52243_Supported_CAN_IDs_2nd_part.py
@@ -0,0 +1,32 @@
+""" Testscript for a requirement which needs to be tested by code inspection (To be inspected)
+
+    Testscript  ODTB2 MEPII
+    project     BECM basetech MEPII
+    author      LDELLATO (Lorenzo Della Torre)
+    date        2020-05-07
+    version     1.0
+    reqprod     52243
+
+    Inspired by https://grpc.io/docs/tutorials/basic/python.html
+    Copyright 2015 gRPC authors.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+"""
+
+import logging
+import sys
+
+logging.basicConfig(format='%(asctime)s - %(message)s',
+                    stream=sys.stdout, level=logging.DEBUG)
+
+logging.info("Testcase result: To be inspected")
