43b805d3cf7c0b07d4fb8fc5250ffe6c206d5a74
Weiler <HWEILER@volvocars.com>
2020-05-19 18:29:21 +0200

    Changes in type to fit Python3.7

--- Py_testenv/support_can.py
+++ Py_testenv/support_can.py
@@ -31,8 +31,8 @@ from threading import Thread
 import sys
 #from typing import TypedDict, NewType
 from typing import Dict, NewType
-import yaml
 import re
+import yaml
 #import random
 import grpc
 #import string
@@ -767,10 +767,6 @@ class Support_CAN:
 
 
 #change parameters of FC and how FC frame is used
-    #def change_MF_FC(self, sig,\
-    #                 block_size, separation_time,\
-    #                 frame_control_delay, frame_control_flag,\
-    #                 frame_control_auto):
     def change_MF_FC(self, sig, can_mf_param: CanMFParam):
         """
         change_MF_FC
