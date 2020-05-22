aeacf58191944071c7711c435b23f1c4a58e85e2
Weiler <HWEILER@volvocars.com>
2020-05-22 20:51:31 +0200

    REQ_400704 : Update to read VBF from default catalog if no param given

--- test_cases/SW_DL/General_BL_Reqs/SW_Authentication/Prog_Seq_Autenticity_Verif/Download_Activate_SBL/BSW_REQPROD_400704_SBL_Condition_for_activation_1st_Part.py
+++ test_cases/SW_DL/General_BL_Reqs/SW_Authentication/Prog_Seq_Autenticity_Verif/Download_Activate_SBL/BSW_REQPROD_400704_SBL_Condition_for_activation_1st_Part.py
@@ -28,22 +28,27 @@ from datetime import datetime
 import sys
 import logging
 
-import Py_testenv.ODTB_conf as ODTB_config
-from Py_testenv.support_can import Support_CAN
-from Py_testenv.support_test_odtb2 import Support_test_ODTB2
-from Py_testenv.support_SBL import Support_SBL
-from Py_testenv.support_SecAcc import Support_Security_Access
+import glob
+
+import ODTB_conf as ODTB_config
+from support_can import Support_CAN
+from support_test_odtb2 import Support_test_ODTB2
+from support_SBL import Support_SBL
+from support_SecAcc import Support_Security_Access
 
 SC = Support_CAN()
 SUTE = Support_test_ODTB2()
 SSBL = Support_SBL()
 SSA = Support_Security_Access()
-
+    
 def precondition(stub, can_send, can_receive, can_namespace, result):
     """
     Precondition for test running:
     BECM has to be kept alive: start heartbeat
     """
+    # read VBF param when testscript is s started, if empty take default param
+    SSBL.get_vbf_files()
+    
     # start heartbeat, repeat every 0.8 second
     SC.start_heartbeat(stub, "MvcmFront1NMFr", "Front1CANCfg0",
                        b'\x00\x40\xFF\xFF\xFF\xFF\xFF\xFF', 0.4)
@@ -67,17 +72,32 @@ def step_0(stub, can_send, can_receive, can_namespace, result):
     Teststep 0: Complete ECU Part/Serial Number(s)
     """
     stepno = 0
-    purpose = "Complete ECU Part/Serial Number(s)"
-    timeout = 1
-    min_no_messages = -1
-    max_no_messages = -1
+    #purpose = "Complete ECU Part/Serial Number(s)"
+    #timeout = 1
+    #min_no_messages = -1
+    #max_no_messages = -1
 
-    can_m_send = SC.can_m_send("ReadDataByIdentifier", b'\xED\xA0', "")
-    can_mr_extra = ''
+    #can_m_send = SC.can_m_send("ReadDataByIdentifier", b'\xED\xA0', "")
+    #can_mr_extra = ''
 
-    result = result and SUTE.teststep(stub, can_m_send, can_mr_extra, can_send,
-                                      can_receive, can_namespace, stepno, purpose,
-                                      timeout, min_no_messages, max_no_messages)
+    ts_param = {"stub" : stub,\
+                "m_send" : SC.can_m_send("ReadDataByIdentifier", b'\xED\xA0', ""),\
+                "mr_extra" : '',\
+                "can_send" : can_send,\
+                "can_rec"  : can_receive,\
+                "can_nspace" : can_namespace\
+               }
+    extra_param = {"purpose" : "Complete ECU Part/Serial Number(s)",\
+                   "timeout" : 1,\
+                   "min_no_messages" : -1,\
+                   "max_no_messages" : -1
+                  }
+
+    result = SUTE.teststep(ts_param,\
+                           stepno, extra_param)
+    #result = result and SUTE.teststep(stub, can_m_send, can_mr_extra, can_send,
+    #                                  can_receive, can_namespace, stepno, purpose,
+    #                                  timeout, min_no_messages, max_no_messages)
     logging.info('%s', SUTE.PP_CombinedDID_EDA0(SC.can_messages[can_receive][0][2], title=''))
     return result
 
@@ -86,17 +106,32 @@ def step_1(stub, can_send, can_receive, can_namespace, result):
     Teststep 1: verify RoutineControlRequest is sent for Type 1
     """
     stepno = 1
-    purpose = "verify RoutineControl start are sent for Check Programming Preconditions"
-    timeout = 1 #wait a second for reply to be send
-    min_no_messages = -1
-    max_no_messages = -1
+    ts_param = {"stub" : stub,\
+                "m_send" : SC.can_m_send("RoutineControlRequestSID", b'\x02\x06', b'\x01'),\
+                "mr_extra" : '',\
+                "can_send" : can_send,\
+                "can_rec"  : can_receive,\
+                "can_nspace" : can_namespace\
+               }
+    extra_param = {"purpose" : "verify RoutineControl start are sent for Check Programming Preconditions",\
+                   "timeout" : 1,\
+                   "min_no_messages" : -1,\
+                   "max_no_messages" : -1
+                  }
 
-    can_m_send = SC.can_m_send("RoutineControlRequestSID", b'\x02\x06', b'\x01')
-    can_mr_extra = ''
+    result = SUTE.teststep(ts_param,\
+                           stepno, extra_param)
+    #purpose = "verify RoutineControl start are sent for Check Programming Preconditions"
+    #timeout = 1 #wait a second for reply to be send
+    #min_no_messages = -1
+    #max_no_messages = -1
 
-    result = result and SUTE.teststep(stub, can_m_send, can_mr_extra, can_send,
-                                      can_receive, can_namespace, stepno, purpose,
-                                      timeout, min_no_messages, max_no_messages)
+    #can_m_send = SC.can_m_send("RoutineControlRequestSID", b'\x02\x06', b'\x01')
+    #can_mr_extra = ''
+
+    #result = result and SUTE.teststep(stub, can_m_send, can_mr_extra, can_send,
+    #                                  can_receive, can_namespace, stepno, purpose,
+    #                                  timeout, min_no_messages, max_no_messages)
 
     result = result and SUTE.PP_Decode_Routine_Control_response(SC.can_messages[can_receive][0][2],
                                                                 'Type1,Completed')
@@ -107,21 +142,38 @@ def step_2(stub, can_send, can_receive, can_namespace, result):
     Teststep 2: Change to Programming session
     """
     stepno = 2
-    purpose = "Change to Programming session(01) from default"
-    timeout = 1
-    min_no_messages = -1
-    max_no_messages = -1
+    ts_param = {"stub" : stub,\
+                "m_send" : SC.can_m_send("DiagnosticSessionControl", b'\x02', ""),\
+                "mr_extra" : '',\
+                "can_send" : can_send,\
+                "can_rec"  : can_receive,\
+                "can_nspace" : can_namespace\
+               }
+    extra_param = {"purpose" : "Change to Programming session(01) from default",\
+                   "timeout" : 1,\
+                   "min_no_messages" : -1,\
+                   "max_no_messages" : -1
+                  }
 
-    can_m_send = SC.can_m_send("DiagnosticSessionControl", b'\x02', "")
-    can_mr_extra = ''
+    result = SUTE.teststep(ts_param,\
+                           stepno, extra_param)
+    result = SUTE.teststep(ts_param,\
+                           stepno, extra_param)
+    #purpose = "Change to Programming session(01) from default"
+    #timeout = 1
+    #min_no_messages = -1
+    #max_no_messages = -1
 
-    result = result and SUTE.teststep(stub, can_m_send, can_mr_extra, can_send,
-                                      can_receive, can_namespace, stepno, purpose,
-                                      timeout, min_no_messages, max_no_messages)
+    #can_m_send = SC.can_m_send("DiagnosticSessionControl", b'\x02', "")
+    #can_mr_extra = ''
 
-    result = result and SUTE.teststep(stub, can_m_send, can_mr_extra, can_send,
-                                      can_receive, can_namespace, stepno, purpose,
-                                      timeout, min_no_messages, max_no_messages)
+    #result = result and SUTE.teststep(stub, can_m_send, can_mr_extra, can_send,
+    #                                  can_receive, can_namespace, stepno, purpose,
+    #                                  timeout, min_no_messages, max_no_messages)
+
+    #result = result and SUTE.teststep(stub, can_m_send, can_mr_extra, can_send,
+    #                                  can_receive, can_namespace, stepno, purpose,
+    #                                  timeout, min_no_messages, max_no_messages)
     return result
 
 def step_3(stub, can_send, can_receive, can_namespace, result):
@@ -140,25 +192,41 @@ def step_4(stub, can_send, can_receive, can_namespace, result):
     """
     stepno = 4
     purpose = "Download of SBL"
-    tresult, call = SSBL.sbl_download_no_check(stub, can_send,
-                                               can_receive, can_namespace, stepno, purpose)
+    tresult, call = SSBL.sbl_download_no_check(stub, SSBL.get_sbl_filename(),\
+                                               can_send, can_receive, can_namespace,\
+                                               stepno, purpose)
     result = result and tresult
     return call, result
 
-def step_5(stub, can_send, can_receive, can_namespace, call):
+def step_5(stub, can_send, can_receive, can_namespace, call, result):
     """
     Teststep 5: SBL activation with correct call
     """
     stepno = 5
-    purpose = "SBL activation with correct call"
-    timeout = 4 #wait a second for reply to be send
-    min_no_messages = -1
-    max_no_messages = -1
-    can_m_send = SC.can_m_send("RoutineControlRequestSID", b'\x03\x01' + call, b'\x01')
-    can_mr_extra = ''
-    result = SUTE.teststep(stub, can_m_send, can_mr_extra, can_send,
-                           can_receive, can_namespace, stepno, purpose,
-                           timeout, min_no_messages, max_no_messages)
+    ts_param = {"stub" : stub,\
+                "m_send" : SC.can_m_send("RoutineControlRequestSID", b'\x03\x01' + call, b'\x01'),\
+                "mr_extra" : '',\
+                "can_send" : can_send,\
+                "can_rec"  : can_receive,\
+                "can_nspace" : can_namespace\
+               }
+    extra_param = {"purpose" : "SBL activation with correct call",\
+                   "timeout" : 4,\
+                   "min_no_messages" : -1,\
+                   "max_no_messages" : -1
+                  }
+
+    result = SUTE.teststep(ts_param,\
+                           stepno, extra_param)
+    #purpose = "SBL activation with correct call"
+    #timeout = 4 #wait a second for reply to be send
+    #min_no_messages = -1
+    #max_no_messages = -1
+    #can_m_send = SC.can_m_send("RoutineControlRequestSID", b'\x03\x01' + call, b'\x01')
+    #can_mr_extra = ''
+    #result = result and SUTE.teststep(stub, can_m_send, can_mr_extra, can_send,
+    #                                  can_receive, can_namespace, stepno, purpose,
+    #                                  timeout, min_no_messages, max_no_messages)
 
     result = result and SUTE.test_message(SC.can_messages[can_receive], teststring='7F3131')
     logging.info(SUTE.PP_Decode_7F_response(SC.can_frames[can_receive][0][2]))
@@ -169,17 +237,32 @@ def step_6(stub, can_send, can_receive, can_namespace, result):
     Teststep 6: Reset
     """
     stepno = 6
-    purpose = "ECU Reset"
-    timeout = 1
-    min_no_messages = -1
-    max_no_messages = -1
+    ts_param = {"stub" : stub,\
+                "m_send" : b'\x11\x01',\
+                "mr_extra" : '',\
+                "can_send" : can_send,\
+                "can_rec"  : can_receive,\
+                "can_nspace" : can_namespace\
+               }
+    extra_param = {"purpose" : "ECU Reset",\
+                   "timeout" : 1,\
+                   "min_no_messages" : -1,\
+                   "max_no_messages" : -1
+                  }
 
-    can_m_send = b'\x11\x01'
-    can_mr_extra = ''
+    result = SUTE.teststep(ts_param,\
+                           stepno, extra_param)
+    #purpose = "ECU Reset"
+    #timeout = 1
+    #min_no_messages = -1
+    #max_no_messages = -1
 
-    result = result and SUTE.teststep(stub, can_m_send, can_mr_extra, can_send,
-                                      can_receive, can_namespace, stepno, purpose,
-                                      timeout, min_no_messages, max_no_messages)
+    #can_m_send = b'\x11\x01'
+    #can_mr_extra = ''
+
+    #result = result and SUTE.teststep(stub, can_m_send, can_mr_extra, can_send,
+    #                                  can_receive, can_namespace, stepno, purpose,
+    #                                  timeout, min_no_messages, max_no_messages)
 
     result = result and SUTE.test_message(SC.can_messages[can_receive], teststring='025101')
     time.sleep(1)
@@ -190,17 +273,32 @@ def step_7(stub, can_send, can_receive, can_namespace, result):
     Teststep 7: verify session
     """
     stepno = 7
-    purpose = "Verify Default session"
-    timeout = 1
-    min_no_messages = 1
-    max_no_messages = 1
+    ts_param = {"stub" : stub,\
+                "m_send" : SC.can_m_send("ReadDataByIdentifier", b'\xF1\x86', ""),\
+                "mr_extra" : '',\
+                "can_send" : can_send,\
+                "can_rec"  : can_receive,\
+                "can_nspace" : can_namespace\
+               }
+    extra_param = {"purpose" : "Verify Default session",\
+                   "timeout" : 1,\
+                   "min_no_messages" : 1,\
+                   "max_no_messages" : 1
+                  }
 
-    can_m_send = SC.can_m_send("ReadDataByIdentifier", b'\xF1\x86', "")
-    can_mr_extra = b'\x01'
+    result = SUTE.teststep(ts_param,\
+                           stepno, extra_param)
+    #purpose = "Verify Default session"
+    #timeout = 1
+    #min_no_messages = 1
+    #max_no_messages = 1
 
-    result = result and SUTE.teststep(stub, can_m_send, can_mr_extra, can_send,
-                                      can_receive, can_namespace, stepno, purpose,
-                                      timeout, min_no_messages, max_no_messages)
+    #can_m_send = SC.can_m_send("ReadDataByIdentifier", b'\xF1\x86', "")
+    #can_mr_extra = b'\x01'
+
+    #result = result and SUTE.teststep(stub, can_m_send, can_mr_extra, can_send,
+    #                                  can_receive, can_namespace, stepno, purpose,
+    #                                  timeout, min_no_messages, max_no_messages)
     time.sleep(1)
     return result
 
@@ -255,7 +353,7 @@ def run():
     # step 5:
     # action:
     # result:
-    result = result and step_5(network_stub, can_send, can_receive, can_namespace, call)
+    result = step_5(network_stub, can_send, can_receive, can_namespace, call, result)
 
     # step 6:
     # action:
