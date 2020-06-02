0a35cb91a061c1bca3aa8638bf45534da6b5d3b6
Weiler, Hans-Klaus <hans-klaus.weiler@volvocars.com>
2020-06-02 13:16:01 +0200

    Support service  new 20200602

---
+++ Py_testenv/support_service22.py
@@ -0,0 +1,93 @@
+# project:  ODTB2 testenvironment using SignalBroker
+# author:   HWEILER (Hans-Klaus Weiler)
+# date:     2020-05-29
+# version:  1.0
+
+# Initial version:
+# version 1.0:
+#   teststep    Common teststeps moved into support for dedicated service
+#   pep8        coding is changed to confirm to pep8 (some code left, though)
+
+# inspired by https://grpc.io/docs/tutorials/basic/python.html
+
+# Copyright 2015 gRPC authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""The Python implementation of the gRPC route guide client."""
+
+import logging
+
+from support_can import Support_CAN, CanParam, CanPayload, CanTestExtra
+from support_test_odtb2 import Support_test_ODTB2
+
+
+SC = Support_CAN()
+SUTE = Support_test_ODTB2()
+
+class SupportService22:
+    """
+    class for supporting Service#22
+    """
+
+
+    #@classmethod
+    @staticmethod
+    def read_did_eda0(can_p: CanParam):
+        """
+        Read composite DID EDA0: Complete ECU Part/Serial Number(s)
+        """
+        stepno = 220
+        cpay: CanPayload = {"m_send" : SC.can_m_send("ReadDataByIdentifier", b'\xED\xA0', ""),\
+                            "mr_extra" : ''
+                           }
+        etp: CanTestExtra = {"purpose" : "Service22: Complete ECU Part/Serial Number(s)",\
+                             "timeout" : 1,\
+                             "min_no_messages" : -1,\
+                             "max_no_messages" : -1
+                            }
+
+    #can_m_send = SC.can_m_send("ReadDataByIdentifier", b'\xED\xA0', "")
+    #can_mr_extra = ''
+
+        result = SUTE.teststep(can_p, cpay, stepno, etp)
+        if not len(SC.can_messages[can_p["rec"]]) == 0:
+            logging.info('%s',\
+                         SUTE.PP_CombinedDID_EDA0(SC.can_messages[can_p["rec"]][0][2],\
+                                                    title='')
+                        )
+        else:
+            logging.info('%s', "No messages received for request Read DID EDA0")
+            result = False
+        return result
+
+    #@classmethod
+    @staticmethod
+    #def read_did_f186(self, stub, can_send, can_receive, can_namespace, dsession=b''):
+    def read_did_f186(can_p: CanParam, dsession=b''):
+        """
+        Read DID F186: Active Diagnostic Session
+        """
+        stepno = 221
+        cpay: CanPayload = {"m_send" : SC.can_m_send("ReadDataByIdentifier", b'\xF1\x86', ""),\
+                            "mr_extra" : dsession
+                           }
+        etp: CanTestExtra = {"purpose" : "Service22: Active Diagnostic Session",\
+                             "timeout" : 1,\
+                             "min_no_messages" : 1,\
+                             "max_no_messages" : 1
+                            }
+
+        result = SUTE.teststep(can_p, cpay, stepno, etp)
+        #time.sleep(1)
+        return result
---
+++ Py_testenv/support_service34.py
@@ -0,0 +1,83 @@
+# project:  ODTB2 testenvironment using SignalBroker
+# author:   HWEILER (Hans-Klaus Weiler)
+# date:     2020-06-01
+# version:  1.0
+
+# Initial version:
+# version 1.0:
+#   teststep    Common teststeps moved into support for dedicated service
+#   pep8        coding is changed to confirm to pep8 (some code left, though)
+
+# inspired by https://grpc.io/docs/tutorials/basic/python.html
+
+# Copyright 2015 gRPC authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""The Python implementation of the gRPC route guide client."""
+
+#import logging
+#from typing import Dict
+
+from support_can import Support_CAN, CanMFParam, CanParam, CanPayload, CanTestExtra
+from support_test_odtb2 import Support_test_ODTB2
+
+
+SC = Support_CAN()
+SUTE = Support_test_ODTB2()
+
+class SupportService34:
+    """
+    class for supporting Service#34
+    """
+
+
+    #@classmethod
+    #Support function for Request Download
+    @staticmethod
+    def request_block_download(can_p: CanParam, stepno, purpose,
+                               block_addr_by, block_len_by,
+                               data_format):
+        """
+        Support function for Request Download
+        """
+        #testresult = True
+        # Parameters for FrameControl FC
+        #seed = can_p["send"]
+
+        can_mf_param: CanMFParam = {
+            'block_size' : 0,
+            'separation_time' : 0,
+            'frame_control_delay' : 0, #no wait
+            'frame_control_flag' : 48, #continue send
+            'frame_control_auto' : False
+            }
+        SC.change_MF_FC(can_p["send"], can_mf_param)
+
+        cpay: CanPayload = {"m_send" : b'\x34' + data_format + b'\x44'+\
+                                       block_addr_by + block_len_by,\
+                            "mr_extra" : ''
+                           }
+        etp: CanTestExtra = {"purpose" : purpose,\
+                             "timeout" : 0.05,\
+                             "min_no_messages" : -1,\
+                             "max_no_messages" : -1
+                            }
+        testresult = SUTE.teststep(can_p, cpay, stepno, etp)
+        testresult = testresult and SUTE.test_message(SC.can_messages[can_p["rec"]], '74')
+        nbl = SUTE.PP_StringTobytes(SC.can_frames[can_p["rec"]][0][2][6:10], 4)
+        #if self._debug:
+        #    print("NBL: {}".format(nbl))
+        #nbl = int.from_bytes(SC.can_frames[can_p["rec"]][0][2][6:10])
+        nbl = int.from_bytes(nbl, 'big')
+        return testresult, nbl
