8b5382f85e51820bf014ac7bc100762bf68222ed
Weiler <HWEILER@volvocars.com>
2020-06-11 13:12:52 +0200

    update support functions - some small issues fixed

--- Py_testenv/support_precondition.py
+++ Py_testenv/support_precondition.py
@@ -31,20 +31,20 @@ import logging
 from support_can import SupportCAN, CanParam, PerParam
 from support_test_odtb2 import SupportTestODTB2
 from support_service22 import SupportService22
+from support_service3e import SupportService3e
 
 
 SC = SupportCAN()
 SUTE = SupportTestODTB2()
 SE22 = SupportService22()
+SE3E = SupportService3e()
 
 class SupportPrecondition:
     """
     class for supporting Service#11
     """
 
-    #@classmethod
     @staticmethod
-    #def precondition(self, stub, can_send, can_receive, can_namespace, timeout=300):
     def precondition(can_p: CanParam, timeout=300):
         """
         Precondition for test running:
@@ -64,15 +64,8 @@ class SupportPrecondition:
         # start heartbeat, repeat every x second
         SC.start_heartbeat(can_p["netstub"], hb_param)
 
-        per_param: PerParam = {
-            "name" : "Networkeptalive",
-            "send" : True,
-            "id" : "Vcu1ToAllFuncFront1DiagReqFrame",
-            "nspace" : can_p["namespace"].name,
-            "frame" : b'\x02\x3E\x80\x00\x00\x00\x00\x00',
-            "intervall" : 1.02
-            }
-        SC.start_periodic(can_p["netstub"], per_param)
+        #Start testerpresent without reply
+        SE3E.start_periodic_tp_zero_suppress_prmib(can_p)
 
         ##record signal we send as well
         #SC.subscribe_signal(stub, can_receive, can_send, can_namespace, timeout)
@@ -138,6 +131,46 @@ class SupportPrecondition:
                             "send": can_p["receive"],
                             "receive": can_p["send"],
                             "namespace": can_p["namespace"],
+                           }
+        SC.subscribe_signal(can_p2, timeout)
+
+        result = SE22.read_did_eda0(can_p)
+        logging.info("Precondition testok: %s\n", result)
+        return result
+        
+        
+    @staticmethod
+    def precondition_spa2(can_p: CanParam, timeout=300):
+        """
+        Precondition for test running:
+        BECM has to be kept alive: start heartbeat
+        """
+
+        # start heartbeat, repeat every 0.8 second
+        hb_param: PerParam = {
+            "name" : "Heartbeat",
+            "send" : True,
+            "id" : "HvbmdpNmFrame", #SPA1: MvcmFront1NMFr",
+            "nspace" : can_p["namespace"].name,
+            "frame" : b'\x00\x40\xFF\xFF\xFF\xFF\xFF\xFF',
+            "intervall" : 0.4
+            }
+
+        # start heartbeat, repeat every x second
+        SC.start_heartbeat(can_p["netstub"], hb_param)
+
+        #Start testerpresent without reply
+        SE3E.start_periodic_tp_zero_suppress_prmib(can_p)
+
+        ##record signal we send as well
+        #SC.subscribe_signal(stub, can_receive, can_send, can_namespace, timeout)
+        SC.subscribe_signal(can_p, timeout)
+        print("precondition can_p2", can_p)
+        #record signal we send as well
+        can_p2: CanParam = {"netstub": can_p["netstub"],
+                            "send": can_p["receive"],
+                            "receive": can_p["send"],
+                            "namespace": can_p["namespace"]
                            }
         SC.subscribe_signal(can_p2, timeout)
 
