2b879b4bd7a0cc5f22f5d82cd711a3670e9f1373
Della Torre, Lorenzo <lorenzo.della.torre@volvocars.com>
2020-06-23 14:40:11 +0200

    Replace BSW_REQPROD_60003_Transport_and_Network_layer.py

--- manual_test/Communication/DoCAN/Req_from_Transport_Network_Layer/BSW_REQPROD_60003_Transport_and_Network_layer.py
+++ manual_test/Communication/DoCAN/Req_from_Transport_Network_Layer/BSW_REQPROD_60003_Transport_and_Network_layer.py
@@ -28,5 +28,16 @@ import logging
 logging.basicConfig(filename='{}.log'.format((__file__)[-3]), format='%(asctime)s - %(message)s',
                     level=logging.INFO)
 
-logging.info("Testcase result: Tested implicitly by REQPRODs in in LC : " \
-             "VCC DoCAN - SW reqs.")
+logging.info("Testcase result: Tested implicitly by REQPRODs in in LC : "
+             "VCC DoCAN - SW reqs.:"
+             "Requirements from section Addressing formats"
+             "Requirements from section BlockSize (BS) parameter definition"
+             "Requirements from section CAN frame data and payload configuration"
+             "Requirements from section Max number of FC. Wait frame transmission (N_WFTmax)"
+             "Requirements from section Separation( STmin) parameter definition"
+             "Requirements from section Soparation time between sigle frames"
+             "Requirements from section Timing parameters"
+             "Requirements from section Transport and Network Layer"
+             "Requirements from section Unexpected arrival of N_PDU"
+             "Requirements from section Use data frame by frame"
+             )
