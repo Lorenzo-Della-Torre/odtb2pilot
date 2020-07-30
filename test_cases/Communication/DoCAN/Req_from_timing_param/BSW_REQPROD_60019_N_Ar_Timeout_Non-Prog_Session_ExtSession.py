7c26657f2ab35f72db890c8095be3e39dc4ff88a
Weiler <HWEILER@volvocars.com>
2020-07-30 12:46:37 +0200

    BSW_REQPROD_60019 update to match SPA/MEP2

--- test_cases/Communication/DoCAN/Req_from_timing_param/BSW_REQPROD_60019_N_Ar_Timeout_Non-Prog_Session_ExtSession.py
+++ test_cases/Communication/DoCAN/Req_from_timing_param/BSW_REQPROD_60019_N_Ar_Timeout_Non-Prog_Session_ExtSession.py
@@ -5,6 +5,12 @@
 # version:  1.1
 # reqprod:  60019
 
+# author:   HWEILER (Hans-Klaus Weiler)
+# date:     2020-07-30
+# version:  1.2
+# changes:  update to use YML parameter
+#           step2: compare if whole MF message received (frames differ between projects)
+
 # #inspired by https://grpc.io/docs/tutorials/basic/python.html
 # Copyright 2015 gRPC authors.
 #
@@ -26,17 +32,20 @@ import time
 from datetime import datetime
 import sys
 import logging
+import inspect
 
-import ODTB_conf
+import odtb_conf
 from support_can import SupportCAN, CanParam, CanMFParam, CanPayload, CanTestExtra
 from support_test_odtb2 import SupportTestODTB2
 from support_carcom import SupportCARCOM
+from support_file_io import SupportFileIO
 
 from support_precondition import SupportPrecondition
 from support_postcondition import SupportPostcondition
 from support_service22 import SupportService22
 from support_service10 import SupportService10
 
+SIO = SupportFileIO
 SC = SupportCAN()
 SUTE = SupportTestODTB2()
 SC_CARCOM = SupportCARCOM()
@@ -45,11 +54,10 @@ POST = SupportPostcondition()
 SE10 = SupportService10()
 SE22 = SupportService22()
 
-def step_2(can_par):
+def step_2(can_p):
     """
     Teststep 2: request EDA0 - with FC delay < timeout 1000 ms
     """
-    n_frame = 11
     can_mf: CanMFParam = {"block_size" : 0,\
                           "separation_time" : 0,\
                           "frame_control_delay" : 950,\
@@ -66,86 +74,86 @@ def step_2(can_par):
                          "min_no_messages" : -1,\
                          "max_no_messages" : -1\
                          }
-    SC.change_mf_fc(can_par["receive"], can_mf)
-    result = SUTE.teststep(can_par, cpay, etp)
+    SC.change_mf_fc(can_p["receive"], can_mf)
+    result = SUTE.teststep(can_p, cpay, etp)
 
-    result = (len(SC.can_frames[can_par["receive"]]) == n_frame)
-    if len(SC.can_frames[can_par["receive"]]) == n_frame:
-        logging.info("Timeout due to FC delay: ")
-        logging.info("number of frames received as expected: %s",\
-                     len(SC.can_frames[can_par["receive"]]))
-    else:
-        logging.info("FAIL: Wrong number of frames received. Expeced %s Received: %s",\
-                     n_frame, len(SC.can_frames[can_par["receive"]]))
+    logging.info("Messages received: %s", SC.can_messages[can_p["receive"]])
+    if not len(SC.can_messages[can_p["receive"]]) == 1:
+        logging.info("Len Mess received: %s", len(SC.can_messages[can_p["receive"]]))
+
+    logging.info("Step %s: Result teststep: %s \n", etp["step_no"], result)
     return result
 
-def step_3(can_par):
+def step_3(can_p):
     """
     Teststep 3: request EDA0 - with FC delay > timeout 1000 ms
     """
     n_frame = 1
-    can_mf: CanMFParam = {"block_size" : 0,\
-                          "separation_time" : 0,\
-                          "frame_control_delay" : 1050,\
-                          "frame_control_flag" : 48,\
+    can_mf: CanMFParam = {"block_size" : 0,
+                          "separation_time" : 0,
+                          "frame_control_delay" : 1050,
+                          "frame_control_flag" : 48,
                           "frame_control_auto" : True
                           }
 
     cpay: CanPayload = {"payload" : SC_CARCOM.can_m_send("ReadDataByIdentifier", b'\xED\xA0', b''),\
                         "extra" : ''
                         }
-    etp: CanTestExtra = {"step_no" : 3,\
-                         "purpose" : "request EDA0 - with FC delay > timeout 1000 ms",\
-                         "timeout" : 2,\
-                         "min_no_messages" : -1,\
-                         "max_no_messages" : -1\
+    etp: CanTestExtra = {"step_no" : 3,
+                         "purpose" : "request EDA0 - with FC delay > timeout 1000 ms",
+                         "timeout" : 2,
+                         "min_no_messages" : -1,
+                         "max_no_messages" : -1
                          }
-    SC.change_mf_fc(can_par["receive"], can_mf)
-    result = SUTE.teststep(can_par, cpay, etp)
+    SC.change_mf_fc(can_p["receive"], can_mf)
+    result = SUTE.teststep(can_p, cpay, etp)
 
-    result = (len(SC.can_frames[can_par["receive"]]) == n_frame)
-    if len(SC.can_frames[can_par["receive"]]) == n_frame:
+    result = (len(SC.can_frames[can_p["receive"]]) == n_frame)
+    if len(SC.can_frames[can_p["receive"]]) == n_frame:
         logging.info("Timeout due to FC delay: ")
         logging.info("number of frames received as expected: %s",\
-                     len(SC.can_frames[can_par["receive"]]))
+                     len(SC.can_frames[can_p["receive"]]))
     else:
         logging.info("FAIL: Wrong number of frames received. Expeced %s Received: %s",\
-                     n_frame, len(SC.can_frames[can_par["receive"]]))
+                     n_frame, len(SC.can_frames[can_p["receive"]]))
     return result
 
-def step_4(can_par):
+def step_4(can_p):
     """
     Teststep 4: set back frame_control_delay to default
     """
     stepno = 4
     purpose = "set back frame_control_delay to default"
 
-    can_mf: CanMFParam = {"block_size" : 0,\
-                          "separation_time" : 0,\
-                          "frame_control_delay" : 0,\
-                          "frame_control_flag" : 48,\
+    can_mf: CanMFParam = {"block_size" : 0,
+                          "separation_time" : 0,
+                          "frame_control_delay" : 0,
+                          "frame_control_flag" : 48,
                           "frame_control_auto" : True
                           }
 
     SUTE.print_test_purpose(stepno, purpose)
-    SC.change_mf_fc(can_par["receive"], can_mf)
+    SC.change_mf_fc(can_p["receive"], can_mf)
 
 def run():
     """
     Run - Call other functions from here
     """
-    logging.basicConfig(format=' %(message)s', stream=sys.stdout, level=logging.DEBUG)
+    logging.basicConfig(format=' %(message)s', stream=sys.stdout, level=logging.INFO)
 
     # start logging
     # to be implemented
 
     # where to connect to signal_broker
-    can_par: CanParam = {
-        "netstub" : SC.connect_to_signalbroker(ODTB_conf.ODTB2_DUT, ODTB_conf.ODTB2_PORT),\
-        "send" : "Vcu1ToBecmFront1DiagReqFrame",\
-        "receive" : "BecmToVcu1Front1DiagResFrame",\
-        "namespace" : SC.nspace_lookup("Front1CANCfg0")
+    can_p: CanParam = {
+        'netstub': SC.connect_to_signalbroker(odtb_conf.ODTB2_DUT, odtb_conf.ODTB2_PORT),
+        'send': "Vcu1ToBecmFront1DiagReqFrame",
+        'receive': "BecmToVcu1Front1DiagResFrame",
+        'namespace': SC.nspace_lookup("Front1CANCfg0")
         }
+    #Read YML parameter for current function (get it from stack)
+    logging.debug("Read YML for %s", str(inspect.stack()[0][3]))
+    SIO.extract_parameter_yml(str(inspect.stack()[0][3]), can_p)
 
     logging.info("Testcase start: %s", datetime.now())
     starttime = time.time()
@@ -155,7 +163,7 @@ def run():
     # precondition
     ############################################
     timeout = 30
-    result = PREC.precondition(can_par, timeout)
+    result = PREC.precondition(can_p, timeout)
 
     if result:
     ############################################
@@ -164,33 +172,33 @@ def run():
     # step1:
     # action: # Change to extended session
     # result: BECM reports mode
-        result = result and SE10.diagnostic_session_control_mode3(can_par, 1)
+        result = result and SE10.diagnostic_session_control_mode3(can_p, 1)
 
     # step2:
     # action: send request with FC_delay < timeout
     # result: whole message received
-        result = result and step_2(can_par)
+        result = result and step_2(can_p)
 
     # step3:
     # action: send request with FC_delay > timeout
     # result: only first frame received
-        result = result and step_3(can_par)
+        result = result and step_3(can_p)
 
     # step4:
     # action: restore FC_delay again
     # result:
-        step_4(can_par)
+        step_4(can_p)
 
     # step5:
     # action: verify current session
     # result: BECM reports extended session
-        result = result and SE22.read_did_f186(can_par, dsession=b'\x03')#, 5)
+        result = result and SE22.read_did_f186(can_p, dsession=b'\x03')#, 5)
 
     ############################################
     # postCondition
     ############################################
 
-    POST.postcondition(can_par, starttime, result)
+    POST.postcondition(can_p, starttime, result)
 
 if __name__ == '__main__':
     run()
