1a8b79cda5b00dbc6d0d9859133fdc26fdde0320
Weiler <HWEILER@volvocars.com>
2020-08-06 15:54:42 +0200

    S19 REQ 74180 YML update 20200806

--- test_cases/Diagnostics/VCC-UDS_Services/VCC-UDS_Services_Service_19/Req_from_effect_ECU_operations/BSW_REQPROD_74180_Service_ReadDTCInformation__19__affecting_ECU_functionality.py
+++ test_cases/Diagnostics/VCC-UDS_Services/VCC-UDS_Services_Service_19/Req_from_effect_ECU_operations/BSW_REQPROD_74180_Service_ReadDTCInformation__19__affecting_ECU_functionality.py
@@ -26,8 +26,9 @@ import time
 from datetime import datetime
 import sys
 import logging
+import inspect
 
-import ODTB_conf
+import odtb_conf
 from support_can import SupportCAN, CanParam, CanTestExtra, CanPayload
 from support_test_odtb2 import SupportTestODTB2
 from support_carcom import SupportCARCOM
@@ -45,69 +46,66 @@ PREC = SupportPrecondition()
 POST = SupportPostcondition()
 SE11 = SupportService11()
 
-def step_1(can_par):
+def step_1(can_p):
     """
     Teststep 1: register another signal
     """
-    stepno = 1
-    etp: CanTestExtra = SIO.extract_parameter_yml(
-        "step_{}".format(stepno),
-        step_no=1,
-        purpose="register another signal",
-        timeout=15,
-        min_no_messages=-1,
-        max_no_messages=-1
-        )
-    can_par_ex: CanParam = SIO.extract_parameter_yml(
-        "main",
-        netstub=SC.connect_to_signalbroker(ODTB_conf.ODTB2_DUT, ODTB_conf.ODTB2_PORT),
-        send="ECMFront1Fr02",
-        receive="BECMFront1Fr02",
-        namespace=SC.nspace_lookup("Front1CANCfg0")
-        )
+    etp: CanTestExtra = {
+        "step_no": 1,
+        "purpose": "register another signal",
+        "timeout": 15,
+        "min_no_messages": -1,
+        "max_no_messages": -1
+        }
+    SIO.extract_parameter_yml(str(inspect.stack()[0][3]), etp)
+    can_p_ex: CanParam = {
+        "netstub": can_p["netstub"],
+        "send": "ECMFront1Fr02",
+        "receive": "BECMFront1Fr02",
+        "namespace": can_p["namespace"]
+        }
+    SIO.extract_parameter_yml(str(inspect.stack()[0][3]), can_p_ex)
 
-    SC.subscribe_signal(can_par_ex, etp["timeout"])
+    SC.subscribe_signal(can_p_ex, etp["timeout"])
     time.sleep(1)
     SC.clear_all_can_messages()
     logging.info("all can messages cleared")
     SC.clear_all_can_frames()
-    SC.update_can_messages(can_par["receive"])
+    SC.update_can_messages(can_p["receive"])
     logging.info("all can messages updated")
     time.sleep(1)
     logging.info("Step%s: messages received %s", etp["step_no"],
-                 len(SC.can_messages[can_par_ex["receive"]]))
+                 len(SC.can_messages[can_p_ex["receive"]]))
     logging.info("Step%s: messages: %s \n", etp["step_no"],
-                 SC.can_messages[can_par_ex["receive"]])
+                 SC.can_messages[can_p_ex["receive"]])
     logging.info("Step%s: frames received %s", etp["step_no"],
-                 len(SC.can_frames[can_par_ex["receive"]]))
+                 len(SC.can_frames[can_p_ex["receive"]]))
     logging.info("Step%s: frames: %s \n", etp["step_no"],
-                 SC.can_frames[can_par_ex["receive"]])
+                 SC.can_frames[can_p_ex["receive"]])
 
-    frame_step_2 = len(SC.can_frames[can_par_ex["receive"]])
+    frame_step_2 = len(SC.can_frames[can_p_ex["receive"]])
     #min number of non-diagnostic frames received allowed
-    result = (len(SC.can_frames[can_par_ex["receive"]]) > 10)
+    result = (len(SC.can_frames[can_p_ex["receive"]]) > 10)
 
     logging.info("Step%s teststatus: %s \n", etp["step_no"], result)
-    return result, can_par_ex, frame_step_2
+    return result, can_p_ex, frame_step_2
 
-def step_2(can_par, can_par_ex, frame_step_2):
+def step_2(can_p, can_p_ex, frame_step_2):
     """
     Teststep 2: verify ReadDTCInfoSnapshotIdentification reply positively
     """
-    stepno = 2
-    cpay: CanPayload = SIO.extract_parameter_yml(
-        "step_{}".format(stepno),
-        payload=SC_CARCOM.can_m_send("ReadDTCInfoSnapshotIdentification", b'', b''),
-        extra=''
-        )
-    etp: CanTestExtra = SIO.extract_parameter_yml(
-        "step_{}".format(stepno),
-        step_no=2,
-        purpose="verify ReadDTCInfoSnapshotIdentification reply positively",
-        timeout=1,
-        min_no_messages=-1,
-        max_no_messages=-1
-        )
+    cpay: CanPayload = {
+        "payload": SC_CARCOM.can_m_send("ReadDTCInfoSnapshotIdentification", b'', b''),
+        "extra": ''
+        }
+    SIO.extract_parameter_yml(str(inspect.stack()[0][3]), cpay)
+    etp: CanTestExtra = {
+        "step_no": 2,
+        "purpose": "verify ReadDTCInfoSnapshotIdentification reply positively",
+        "timeout": 1,
+        "min_no_messages": -1,
+        "max_no_messages": -1
+        }
     waiting_time = 2 #seconds
     max_diff = 50 #max difference allowed for number of frame non-diagnostic received
     number_of_frames_received = 0
@@ -115,19 +113,19 @@ def step_2(can_par, can_par_ex, frame_step_2):
     logging.info("all can messages cleared")
     SC.clear_all_can_frames()
     now = int(time.time())
-    SC.update_can_messages(can_par["receive"])
+    SC.update_can_messages(can_p["receive"])
     while now + waiting_time > int(time.time()):
-        result = SUTE.teststep(can_par, cpay, etp)
-        result = result and SUTE.test_message(SC.can_messages[can_par["receive"]],
+        result = SUTE.teststep(can_p, cpay, etp)
+        result = result and SUTE.test_message(SC.can_messages[can_p["receive"]],
                                               teststring='5903')
-    number_of_frames_received = len(SC.can_frames[can_par_ex["receive"]])
-    logging.info("Step %s frames received: %s", stepno, number_of_frames_received)
+    number_of_frames_received = len(SC.can_frames[can_p_ex["receive"]])
+    logging.info("Step %s frames received: %s", etp["step_no"], number_of_frames_received)
     result = result and ((number_of_frames_received + max_diff) > frame_step_2 > \
         (number_of_frames_received - max_diff))
-    logging.info("Step %s teststatus: %s \n", stepno, result)
+    logging.info("Step %s teststatus: %s \n", etp["step_no"], result)
     return result
 
-def step_3(can_par, can_par_ex, frame_step_2):
+def step_3(can_p, can_p_ex, frame_step_2):
     """
     Teststep 3: Verify subscribed signal is still sent
     """
@@ -138,19 +136,19 @@ def step_3(can_par, can_par_ex, frame_step_2):
     SC.clear_all_can_messages()
     logging.info("all can messages cleared")
     SC.clear_all_can_frames()
-    SC.update_can_messages(can_par["receive"])
+    SC.update_can_messages(can_p["receive"])
     logging.info("all can messages updated")
     time.sleep(1)
     logging.info("Step%s: messages received %s", stepno,\
-                 len(SC.can_messages[can_par_ex["receive"]]))
+                 len(SC.can_messages[can_p_ex["receive"]]))
     logging.info("Step%s: messages: %s \n", stepno,\
-                 SC.can_messages[can_par_ex["receive"]])
+                 SC.can_messages[can_p_ex["receive"]])
     logging.info("Step%s: frames received %s", stepno,\
-                 len(SC.can_frames[can_par_ex["receive"]]))
+                 len(SC.can_frames[can_p_ex["receive"]]))
     logging.info("Step%s: frames: %s \n", stepno,\
-                 SC.can_frames[can_par_ex["receive"]])
+                 SC.can_frames[can_p_ex["receive"]])
 
-    number_of_frames_received = len(SC.can_frames[can_par_ex["receive"]])
+    number_of_frames_received = len(SC.can_frames[can_p_ex["receive"]])
     result = ((number_of_frames_received + 15) > frame_step_2 > \
         (number_of_frames_received - 15))
 
@@ -162,19 +160,17 @@ def run():
     """
     Run - Call other functions from here
     """
-    logging.basicConfig(format=' %(message)s', stream=sys.stdout, level=logging.DEBUG)
-
-    # start logging
-    # to be implemented
+    #logging.basicConfig(format=' %(message)s', stream=sys.stdout, level=logging.DEBUG)
+    logging.basicConfig(format=' %(message)s', stream=sys.stdout, level=logging.INFO)
 
     # where to connect to signal_broker
-    can_par: CanParam = SIO.extract_parameter_yml(
-        "main",
-        netstub=SC.connect_to_signalbroker(ODTB_conf.ODTB2_DUT, ODTB_conf.ODTB2_PORT),
-        send="Vcu1ToBecmFront1DiagReqFrame",
-        receive="BecmToVcu1Front1DiagResFrame",
-        namespace=SC.nspace_lookup("Front1CANCfg0")
-        )
+    can_p: CanParam = {
+        "netstub" : SC.connect_to_signalbroker(odtb_conf.ODTB2_DUT, odtb_conf.ODTB2_PORT),
+        "send" : "Vcu1ToBecmFront1DiagReqFrame",
+        "receive" : "BecmToVcu1Front1DiagResFrame",
+        "namespace" : SC.nspace_lookup("Front1CANCfg0")
+    }
+    SIO.extract_parameter_yml(str(inspect.stack()[0][3]), can_p)
 
     logging.info("Testcase start: %s", datetime.now())
     starttime = time.time()
@@ -184,7 +180,7 @@ def run():
     # precondition
     ############################################
     timeout = 30
-    result = PREC.precondition(can_par, timeout)
+    result = PREC.precondition(can_p, timeout)
 
     if result:
     ############################################
@@ -193,24 +189,24 @@ def run():
     # step 1:
     # action: Register not diagnostic message
     # result: BECM send requested signals
-        resultt, can_par_ex, frame_step_2 = step_1(can_par)
+        resultt, can_p_ex, frame_step_2 = step_1(can_p)
         result = result and resultt
 
     # step2:
-    # action:
+    # action: verify ReadDTCInfoSnapshotIdentification reply positiv
     # result: BECM reports confirmed message
-        result = result and step_2(can_par, can_par_ex, frame_step_2)
+        result = result and step_2(can_p, can_p_ex, frame_step_2)
 
     # step3:
     # action: Verify signal is still sent
     # result: BECM send requested signals
-        result = result and step_3(can_par, can_par_ex, frame_step_2)
+        result = result and step_3(can_p, can_p_ex, frame_step_2)
 
     ############################################
     # postCondition
     ############################################
 
-    result = POST.postcondition(can_par, starttime, result)
+    POST.postcondition(can_p, starttime, result)
 
 if __name__ == '__main__':
     run()
