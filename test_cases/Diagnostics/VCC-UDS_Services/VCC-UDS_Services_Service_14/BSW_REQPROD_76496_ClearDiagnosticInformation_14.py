90cf883977b769d8daef67d8cf2d2dbb6ace6e05
Weiler <HWEILER@volvocars.com>
2020-08-06 15:14:22 +0200

    BSW_REQPROD_76496_update_YML_20200806

--- test_cases/Diagnostics/VCC-UDS_Services/VCC-UDS_Services_Service_14/BSW_REQPROD_76496_ClearDiagnosticInformation_14.py
+++ test_cases/Diagnostics/VCC-UDS_Services/VCC-UDS_Services_Service_14/BSW_REQPROD_76496_ClearDiagnosticInformation_14.py
@@ -5,6 +5,11 @@
 # version:  1.0
 # reqprod:  76496
 
+# author:   HWEILER (Hans-Klaus Weiler)
+# date:     2020-08-06
+# version:  1.1
+# changes:  Update for YML
+
 #inspired by https://grpc.io/docs/tutorials/basic/python.html
 
 # Copyright 2015 gRPC authors.
@@ -27,8 +32,9 @@ import time
 from datetime import datetime
 import sys
 import logging
+import inspect
 
-import ODTB_conf
+import odtb_conf
 from support_can import SupportCAN, CanParam, CanPayload, CanTestExtra
 from support_test_odtb2 import SupportTestODTB2
 from support_carcom import SupportCARCOM
@@ -44,48 +50,44 @@ SC_CARCOM = SupportCARCOM()
 PREC = SupportPrecondition()
 POST = SupportPostcondition()
 
-def step_1(can_par):
+def step_1(can_p):
     """
     Teststep 1: verify ClearDiagnosticInformation reply positively
     """
-    stepno = 1
-    cpay: CanPayload = SIO.extract_parameter_yml(
-        "step_{}".format(stepno),
-        payload=SC_CARCOM.can_m_send("ClearDiagnosticInformation", b'\x0B\x4A\x00', b''),
-        extra=''
-        )
-    etp: CanTestExtra = SIO.extract_parameter_yml(
-        "step_{}".format(stepno),
-        step_no=1,
-        purpose="verify ClearDiagnosticInformation reply positively",
-        timeout=1,
-        min_no_messages=-1,
-        max_no_messages=-1
-        )
+    cpay: CanPayload = {
+        "payload": SC_CARCOM.can_m_send("ClearDiagnosticInformation", b'\x0B\x4A\x00', b''),
+        "extra": ''
+        }
+    SIO.extract_parameter_yml(str(inspect.stack()[0][3]), cpay)
+    etp: CanTestExtra = {
+        "step_no": 1,
+        "purpose": "verify ClearDiagnosticInformation reply positively",
+        "timeout": 1,
+        "min_no_messages": -1,
+        "max_no_messages": -1
+        }
+    SIO.extract_parameter_yml(str(inspect.stack()[0][3]), etp)
 
-    result = SUTE.teststep(can_par, cpay, etp)
-    result = result and SUTE.test_message(SC.can_messages[can_par["receive"]],
+    result = SUTE.teststep(can_p, cpay, etp)
+    result = result and SUTE.test_message(SC.can_messages[can_p["receive"]],
                                           teststring='54')
-
     return result
 
 def run():
     """
     Run - Call other functions from here
     """
-    logging.basicConfig(format=' %(message)s', stream=sys.stdout, level=logging.DEBUG)
-
-    # start logging
-    # to be implemented
+    #logging.basicConfig(format=' %(message)s', stream=sys.stdout, level=logging.DEBUG)
+    logging.basicConfig(format=' %(message)s', stream=sys.stdout, level=logging.INFO)
 
     # where to connect to signal_broker
-    can_par: CanParam = SIO.extract_parameter_yml(
-        "main",
-        netstub=SC.connect_to_signalbroker(ODTB_conf.ODTB2_DUT, ODTB_conf.ODTB2_PORT),
-        send="Vcu1ToBecmFront1DiagReqFrame",
-        receive="BecmToVcu1Front1DiagResFrame",
-        namespace=SC.nspace_lookup("Front1CANCfg0")
-        )
+    can_p: CanParam = {
+        "netstub" : SC.connect_to_signalbroker(odtb_conf.ODTB2_DUT, odtb_conf.ODTB2_PORT),
+        "send" : "Vcu1ToBecmFront1DiagReqFrame",
+        "receive" : "BecmToVcu1Front1DiagResFrame",
+        "namespace" : SC.nspace_lookup("Front1CANCfg0")
+    }
+    SIO.extract_parameter_yml(str(inspect.stack()[0][3]), can_p)
 
     logging.info("Testcase start: %s", datetime.now())
     starttime = time.time()
@@ -95,7 +97,7 @@ def run():
     # precondition
     ############################################
     timeout = 30
-    result = PREC.precondition(can_par, timeout)
+    result = PREC.precondition(can_p, timeout)
 
     if result:
     ############################################
@@ -105,13 +107,13 @@ def run():
     # step1:
     # action:
     # result: BECM sends positive reply
-        result = result and step_1(can_par)
+        result = result and step_1(can_p)
 
     ############################################
     # postCondition
     ############################################
 
-    POST.postcondition(can_par, starttime, result)
+    POST.postcondition(can_p, starttime, result)
 
 if __name__ == '__main__':
     run()
--- test_cases/Diagnostics/VCC-UDS_Services/VCC-UDS_Services_Service_14/BSW_REQPROD_76496_ClearDiagnosticInformation_14_Ext.py
+++ test_cases/Diagnostics/VCC-UDS_Services/VCC-UDS_Services_Service_14/BSW_REQPROD_76496_ClearDiagnosticInformation_14_Ext.py
@@ -5,6 +5,11 @@
 # version:  1.0
 # reqprod:  76496
 
+# author:   HWEILER (Hans-Klaus Weiler)
+# date:     2020-08-06
+# version:  1.1
+# changes:  Update for YML, fixed wrong session change
+
 #inspired by https://grpc.io/docs/tutorials/basic/python.html
 
 # Copyright 2015 gRPC authors.
@@ -27,8 +32,9 @@ import time
 from datetime import datetime
 import sys
 import logging
+import inspect
 
-import ODTB_conf
+import odtb_conf
 from support_can import SupportCAN, CanParam, CanPayload, CanTestExtra
 from support_test_odtb2 import SupportTestODTB2
 from support_carcom import SupportCARCOM
@@ -48,27 +54,26 @@ POST = SupportPostcondition()
 SE10 = SupportService10()
 SE22 = SupportService22()
 
-def step_2(can_par):
+def step_2(can_p):
     """
     Teststep 2: verify ClearDiagnosticInformation reply positively
     """
-    stepno = 2
-    cpay: CanPayload = SIO.extract_parameter_yml(
-        "step_{}".format(stepno),
-        payload=SC_CARCOM.can_m_send("ClearDiagnosticInformation", b'\x0B\x4A\x00', b''),
-        extra=''
-        )
-    etp: CanTestExtra = SIO.extract_parameter_yml(
-        "step_{}".format(stepno),
-        step_no=2,
-        purpose="verify ClearDiagnosticInformation reply positively",
-        timeout=1,
-        min_no_messages=-1,
-        max_no_messages=-1
-        )
+    cpay: CanPayload = {
+        "payload": SC_CARCOM.can_m_send("ClearDiagnosticInformation", b'\x0B\x4A\x00', b''),
+        "extra": ''
+        }
+    SIO.extract_parameter_yml(str(inspect.stack()[0][3]), cpay)
+    etp: CanTestExtra = {
+        "step_no": 2,
+        "purpose": "verify ClearDiagnosticInformation reply positively",
+        "timeout": 1,
+        "min_no_messages": -1,
+        "max_no_messages": -1
+        }
+    SIO.extract_parameter_yml(str(inspect.stack()[0][3]), etp)
 
-    result = SUTE.teststep(can_par, cpay, etp)
-    result = result and SUTE.test_message(SC.can_messages[can_par["receive"]],
+    result = SUTE.teststep(can_p, cpay, etp)
+    result = result and SUTE.test_message(SC.can_messages[can_p["receive"]],
                                           teststring='54')
 
     return result
@@ -77,19 +82,17 @@ def run():
     """
     Run - Call other functions from here
     """
-    logging.basicConfig(format=' %(message)s', stream=sys.stdout, level=logging.DEBUG)
-
-    # start logging
-    # to be implemented
+    #logging.basicConfig(format=' %(message)s', stream=sys.stdout, level=logging.DEBUG)
+    logging.basicConfig(format=' %(message)s', stream=sys.stdout, level=logging.INFO)
 
     # where to connect to signal_broker
-    can_par: CanParam = SIO.extract_parameter_yml(
-        "main",
-        netstub=SC.connect_to_signalbroker(ODTB_conf.ODTB2_DUT, ODTB_conf.ODTB2_PORT),
-        send="Vcu1ToBecmFront1DiagReqFrame",
-        receive="BecmToVcu1Front1DiagResFrame",
-        namespace=SC.nspace_lookup("Front1CANCfg0")
-        )
+    can_p: CanParam = {
+        "netstub" : SC.connect_to_signalbroker(odtb_conf.ODTB2_DUT, odtb_conf.ODTB2_PORT),
+        "send" : "Vcu1ToBecmFront1DiagReqFrame",
+        "receive" : "BecmToVcu1Front1DiagResFrame",
+        "namespace" : SC.nspace_lookup("Front1CANCfg0")
+    }
+    SIO.extract_parameter_yml(str(inspect.stack()[0][3]), can_p)
 
     logging.info("Testcase start: %s", datetime.now())
     starttime = time.time()
@@ -99,32 +102,32 @@ def run():
     # precondition
     ############################################
     timeout = 30
-    result = PREC.precondition(can_par, timeout)
+    result = PREC.precondition(can_p, timeout)
 
     if result:
     ############################################
     # teststeps
     ############################################
     # step1:
-    # action: # Change to extended session
+    # action: Change to extended session
     # result: BECM reports mode
-        result = result and SE10.diagnostic_session_control_mode2(can_par, 1)
+        result = result and SE10.diagnostic_session_control_mode3(can_p, 1)
 
     # step2:
     # action: send ReadDTCInfoSnapshotIdentification signal in Extended mode
     # result: BECM sends positive reply
-        result = result and step_2(can_par)
+        result = result and step_2(can_p)
 
     # step3:
     # action: verify current session
     # result: BECM reports extended session
-        result = result and SE22.read_did_f186(can_par, dsession=b'\x02')#, 3)
+        result = result and SE22.read_did_f186(can_p, dsession=b'\x03', stepno=3)
 
     ############################################
     # postCondition
     ############################################
 
-    POST.postcondition(can_par, starttime, result)
+    POST.postcondition(can_p, starttime, result)
 
 if __name__ == '__main__':
     run()
