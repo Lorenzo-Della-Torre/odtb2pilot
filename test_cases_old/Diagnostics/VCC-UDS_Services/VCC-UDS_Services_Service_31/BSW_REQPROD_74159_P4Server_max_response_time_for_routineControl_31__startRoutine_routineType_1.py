28498355bd01652ce69a86d0645d4317af9c89de
Weiler <HWEILER@volvocars.com>
2020-08-24 12:54:25 +0200

    S31 REQ 74159 update YML 20200824

--- test_cases_old/Diagnostics/VCC-UDS_Services/VCC-UDS_Services_Service_31/BSW_REQPROD_74159_P4Server_max_response_time_for_routineControl_31__startRoutine_routineType_1.py
+++ test_cases_old/Diagnostics/VCC-UDS_Services/VCC-UDS_Services_Service_31/BSW_REQPROD_74159_P4Server_max_response_time_for_routineControl_31__startRoutine_routineType_1.py
@@ -5,7 +5,12 @@
 # version:  1.0
 # reqprod:  74159
 
-# #inspired by https://grpc.io/docs/tutorials/basic/python.html
+# author:   HWEILER (Hans-Klaus Weiler)
+# date:     2020-08-241
+# version:  1.1
+# changes:  update for YML support
+
+# inspired by https://grpc.io/docs/tutorials/basic/python.html
 # Copyright 2015 gRPC authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -20,182 +25,165 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
- 
-
 """The Python implementation of the gRPC route guide client."""
 
-from datetime import datetime
 import time
-import logging
-import os
+from datetime import datetime
 import sys
+import logging
+import inspect
 
-import ODTB_conf
+import odtb_conf
+from support_can import SupportCAN, CanParam, CanTestExtra, CanPayload
+from support_test_odtb2 import SupportTestODTB2
+from support_carcom import SupportCARCOM
+from support_file_io import SupportFileIO
+from support_precondition import SupportPrecondition
+from support_postcondition import SupportPostcondition
+from support_service22 import SupportService22
+from support_service10 import SupportService10
+SIO = SupportFileIO
+SC = SupportCAN()
+SUTE = SupportTestODTB2()
+SC_CARCOM = SupportCARCOM()
+PREC = SupportPrecondition()
+POST = SupportPostcondition()
+SE10 = SupportService10()
+SE22 = SupportService22()
 
-from support_can import Support_CAN
-SC = Support_CAN()
+JITTER_TESTENV = 10
 
-from support_test_odtb2 import Support_test_ODTB2
-SuTe = Support_test_ODTB2()
+def step_time_measure(can_p, stepno, p2_server_max):
+    """
+    Teststep time_measure:
+    Verify (time receive message – time sending request) less than P2_server_max
+    """
 
-# Global variable:
-testresult = True
+    logging.info("Step %s: Collecting data for calculating time:", stepno)
+    t_1 = SC.can_frames[can_p["send"]][0][0]
+    logging.info("Step %s: Timestamp request sent: %s", stepno, t_1)
+    t_2 = SC.can_frames[can_p["receive"]][0][0]
+    logging.info("Step %s: Timestamp request sent: %s", stepno, t_2)
+    #fetch P2 server max from the received message
+    #p2_server_max = int(SC.can_messages[can_p["receive"]][0][2][8:10], 16)
+    #logging.info("Step %s: P2_server_max: %s",
+    #             stepno,
+    #             int(SC.can_messages[can_p["receive"]][0][2][8:10], 16))
+
+    purpose = "Verify (time receive message – time sending request) less than P2_server_max"
+    SUTE.print_test_purpose(stepno, purpose)
+    result = ((p2_server_max + JITTER_TESTENV)/1000 > (t_2 - t_1))
+    logging.info("Step%s: t2: %s (sec)", stepno, t_2)
+    logging.info("Step%s: t1: %s (sec)", stepno, t_1)
+    logging.info("Step%s: t2-t1: %s (sec)", stepno, (t_2 - t_1))
+    logging.info("P2_server_max: %s (msec)", p2_server_max)
+    logging.info("JITTER_TESTENV: %s (msec)", JITTER_TESTENV)
+    logging.info("P2_server_max + JITTER_TESTENV: %s (msec)", p2_server_max + JITTER_TESTENV)
+    #logging.info("(p2-jitter) / 1000: %s", (p2_server_max + JITTER_TESTENV)/1000)
+
+    #logging.info("T difference(s): %s", (p2_server_max + JITTER_TESTENV)/1000 - (t_2 - t_1))
+    logging.info("Step %s teststatus:%s \n", stepno, result)
+    return result
 
 
-    
-# precondition for test running:
-#  BECM has to be kept alive: start heartbeat
-def precondition(stub, s, r, ns):
-    global testresult
-        
-    # start heartbeat, repeat every 0.8 second
-    SC.start_heartbeat(stub, "EcmFront1NMFr", "Front1CANCfg0", b'\x20\x40\x00\xFF\x00\x00\x00\x00', 0.8)
-    
-    timeout = 40   #seconds
-    SC.subscribe_signal(stub, s, r, ns, timeout)
-    #record signal we send as well
-    SC.subscribe_signal(stub, r, s, ns, timeout)
-    
-    print()
-    step_0(stub, s, r, ns)
-    
-    print ("precondition testok:", testresult, "\n")
+def step_1(can_p):
+    """
+    Teststep 1: Request session change to Mode1, get P2_server_max
+    """
+    cpay: CanPayload = {
+        'payload': SC_CARCOM.can_m_send("DiagnosticSessionControl", b'\x01', b''),
+        'extra': ''
+        }
+    etp: CanTestExtra = {
+        'step_no': 1,
+        'purpose': "get P2_server_max",
+        'timeout': 1,
+        'min_no_messages': 1,
+        'max_no_messages': 1
+        }
+    result = SUTE.teststep(can_p, cpay, etp)
+    p2_server_max = int(SC.can_messages[can_p["receive"]][0][2][8:10], 16)
+    return result, p2_server_max
 
-    
-# teststep 0: Complete ECU Part/Serial Number(s)
-def step_0(stub, s, r, ns):
-    global testresult
-    
-    stepno = 0
-    purpose = "Complete ECU Part/Serial Number(s)"
-    timeout = 5
-    min_no_messages = -1
-    max_no_messages = -1
-    
-    can_m_send = SC.can_m_send( "ReadDataByIdentifier", b'\xED\xA0', "")
-    can_mr_extra = ''
-
-    testresult = testresult and SuTe.teststep(stub, can_m_send, can_mr_extra, s, r, ns, stepno, purpose, timeout, min_no_messages, max_no_messages)
-    print(SuTe.PP_CombinedDID_EDA0(SC.can_messages[r][0][2], title=''))
-
-# teststep 1: get P2_max
-def step_1(stub, s, r, ns):
-    global testresult
-    global P2_server_max
-    
-    stepno = 1
-    purpose = "Get P2 server max value"
-    timeout = 1
-    min_no_messages = 1
-    max_no_messages = 1
-
-    can_m_send = SC.can_m_send( "DiagnosticSessionControl", b'\x01', "")
-    can_mr_extra = ''
-    
-    SuTe.teststep(stub, can_m_send, can_mr_extra, s, r, ns, stepno, purpose, timeout, min_no_messages, max_no_messages)
-    
-    P2_server_max = int(SC.can_messages[r][0][2][8:10], 16)
-
-    print(P2_server_max)
 
 # teststep 2: verify RoutineControl start reply positively and Type1 is stopped
-def step_2(stub, s, r, ns):
-    global testresult
-    global T1, T2
-    stepno = 2
-    purpose = "verify RoutineControl start reply positively and Type1 is stopped"
-    timeout = 1 #wait a second for reply to be send
-    min_no_messages = 1
-    max_no_messages = 1
-    
-    can_m_send = SC.can_m_send( "RoutineControlRequestSID",b'\x02\x06', b'\x01')
-    can_mr_extra = ''
-   
-    T1=time.time()
-    testresult = testresult and SuTe.teststep(stub, can_m_send, can_mr_extra, s, r, ns, stepno, purpose, timeout, min_no_messages, max_no_messages)
-    T2 = SC.can_messages[r][0][0]
-    testresult = testresult and SuTe.PP_Decode_Routine_Control_response(SC.can_frames[r][0][2], "Type1,Completed")
+def step_2(can_p):
+    """
+    Teststep 2: verify RoutineControl start reply positively and Type1 is stopped
+    """
+    cpay: CanPayload = {
+        "payload": SC_CARCOM.can_m_send("RoutineControlRequestSID",
+                                        b'\x02\x06',
+                                        b'\x01'),
+        "extra": ''
+        }
+    SIO.extract_parameter_yml(str(inspect.stack()[0][3]), cpay)
+    etp: CanTestExtra = {
+        "step_no": 2,
+        "purpose": "verify RoutineControl start reply positively and Type1 is stopped",
+        "timeout": 1,
+        "min_no_messages": 1,
+        "max_no_messages": 1
+        }
+    SIO.extract_parameter_yml(str(inspect.stack()[0][3]), etp)
+    result = SUTE.teststep(can_p, cpay, etp)
+    result = result and\
+             SUTE.pp_decode_routine_control_response(SC.can_frames[can_p["receive"]][0][2],
+                                                     'Type1,Completed')
+    return result
 
-def step_3():
-    global testresult
-    global T1, T2
-    global P2_server_max
-    stepno = 3
-    purpose = "Verify (time receive message – time sending request) < P4_server_max"
-    SuTe.print_test_purpose(stepno, purpose)
-    jitter_testenv = 10
-    if (P2_server_max + jitter_testenv) > (T2 - T1):
-        testresult 
-    else:
-        testresult = False
-    print ("T difference(s):", (P2_server_max + 10)/1000 - (T2 - T1))
-    print ("Step ", stepno, " teststatus:", testresult, "\n")
-    
+
 def run():
-    global testresult
+    """
+    Run - Call other functions from here
+    """
 
-    #start logging
-    # to be implemented
-    
+    logging.basicConfig(format=' %(message)s', stream=sys.stdout, level=logging.INFO)
+
     # where to connect to signal_broker
-    network_stub = SC.connect_to_signalbroker(ODTB_conf.ODTB2_DUT, ODTB_conf.ODTB2_PORT)
+    can_p: CanParam = {
+        "netstub" : SC.connect_to_signalbroker(odtb_conf.ODTB2_DUT, odtb_conf.ODTB2_PORT),
+        "send" : "Vcu1ToBecmFront1DiagReqFrame",
+        "receive" : "BecmToVcu1Front1DiagResFrame",
+        "namespace" : SC.nspace_lookup("Front1CANCfg0")
+    }
+    SIO.extract_parameter_yml(str(inspect.stack()[0][3]), can_p)
 
-    can_send = "Vcu1ToBecmFront1DiagReqFrame"
-    can_receive = "BecmToVcu1Front1DiagResFrame"
-    can_namespace = SC.nspace_lookup("Front1CANCfg0")
-    
-    print ("Testcase start: ", datetime.now())
+    logging.info("Testcase start: %s", datetime.now())
     starttime = time.time()
-    print ("time ", time.time())
-    print()
+    logging.info("Time: %s \n", time.time())
+
     ############################################
     # precondition
     ############################################
-    precondition(network_stub, can_send, can_receive, can_namespace)
-    
+    timeout = 40
+    result = PREC.precondition(can_p, timeout)
+
+    if result:
     ############################################
     # teststeps
     ############################################
     # step 1:
     # action:Change to default session
     # result: positive reply with Parameters P2_server_max and P2*_server_max
-    step_1(network_stub, can_send, can_receive, can_namespace)
+        result, p2_server_max = result and step_1(can_p)
 
     # step2:
     # action: send start RoutineControl signal in default mode
     # result: BECM sends positive reply
-    step_2(network_stub, can_send, can_receive, can_namespace)
+        result = result and step_2(can_p)
 
     # step 3:
-    # action: Verify (time receive message – time sending request) < P4_server_max 
+    # action: Verify (time receive message – time sending request) < P4_server_max
     # result: positive result
-    step_3()
+        result = result and step_time_measure(can_p, stepno=3, p2_server_max=p2_server_max)
+
 
     ############################################
     # postCondition
     ############################################
-            
-    print()
-    print ("time ", time.time())
-    print ("Testcase end: ", datetime.now())
-    print ("Time needed for testrun (seconds): ", int(time.time() - starttime))
 
-    print ("Do cleanup now...")
-    print ("Stop heartbeat sent")
-    SC.stop_heartbeat()
-    #time.sleep(5)
-    # deregister signals
-    SC.unsubscribe_signals()
-    # if threads should remain: try to stop them 
-    SC.thread_stop()
-            
-    print ("Test cleanup end: ", datetime.now())
-    print ()
-    if testresult:
-        print ("Testcase result: PASSED")
-    else:
-        print ("Testcase result: FAILED")
+    POST.postcondition(can_p, starttime, result)
 
-    
 if __name__ == '__main__':
     run()
