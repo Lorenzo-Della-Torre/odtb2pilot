2926bddbbc9f6836f46a87b14233019a4a3093e2
Weiler, Hans-Klaus <hans-klaus.weiler@volvocars.com>
2020-09-03 10:28:03 +0200

    S31 REQPROD 76601 YML update 20200826

--- test_cases_old/Diagnostics/VCC-UDS_Services/VCC-UDS_Services_Service_31/BSW_REQPROD_76601_RoutineControl__31__default_Session.py
+++ test_cases_old/Diagnostics/VCC-UDS_Services/VCC-UDS_Services_Service_31/BSW_REQPROD_76601_RoutineControl__31__default_Session.py
@@ -4,8 +4,13 @@
 # date:     2019-05-31
 # version:  1.0
 # reqprod:  76601
-#inspired by https://grpc.io/docs/tutorials/basic/python.html
 
+# author:   HWEILER (Hans-Klaus Weiler)
+# date:     2020-08-26
+# version:  1.1
+# changes:  update for YML support
+
+# inspired by https://grpc.io/docs/tutorials/basic/python.html
 # Copyright 2015 gRPC authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -22,188 +27,161 @@
 
 """The Python implementation of the gRPC route guide client."""
 
-from datetime import datetime
 import time
-import logging
-import os
+from datetime import datetime
+
 import sys
+import logging
+import inspect
 
-import ODTB_conf
+import odtb_conf
+from support_can import SupportCAN, CanParam, CanTestExtra, CanPayload
+from support_test_odtb2 import SupportTestODTB2
+from support_carcom import SupportCARCOM
+from support_file_io import SupportFileIO
+from support_precondition import SupportPrecondition
+from support_postcondition import SupportPostcondition
+from support_service22 import SupportService22
+from support_service10 import SupportService10
+SIO = SupportFileIO
+SC = SupportCAN()
+SUTE = SupportTestODTB2()
+SC_CARCOM = SupportCARCOM()
+PREC = SupportPrecondition()
+POST = SupportPostcondition()
+SE10 = SupportService10()
+SE22 = SupportService22()
 
-from support_can import Support_CAN
-SC = Support_CAN()
 
-from support_test_odtb2 import Support_test_ODTB2
-SuTe = Support_test_ODTB2()
+def step_1(can_p):
+    """
+    Teststep 1: verify RoutineControlRequest is sent for Type 1
+    """
+    cpay: CanPayload = {
+        "payload": SC_CARCOM.can_m_send("RoutineControlRequestSID",
+                                        b'\x02\x06',
+                                        b'\x01'),
+        "extra": ''
+        }
+    SIO.extract_parameter_yml(str(inspect.stack()[0][3]), cpay)
+    etp: CanTestExtra = {
+        "step_no": 1,
+        "purpose": "verify RoutineControl start are sent for Type 1",
+        "timeout": 1,
+        "min_no_messages": 1,
+        "max_no_messages": 1
+        }
+    SIO.extract_parameter_yml(str(inspect.stack()[0][3]), etp)
 
-# Global variable:
-testresult = True
+    result = SUTE.teststep(can_p, cpay, etp)
+    result = result and\
+             SUTE.pp_decode_routine_control_response(SC.can_frames[can_p["receive"]][0][2],
+                                                     'Type1,Completed')
+    logging.info("Step %s teststatus:%s \n", etp["step_no"], result)
+    return result
 
 
-    
-# precondition for test running:
-#  BECM has to be kept alive: start heartbeat
-def precondition(stub, s, r, ns):
-    global testresult
-        
-    # start heartbeat, repeat every 0.8 second
-    SC.start_heartbeat(stub, "EcmFront1NMFr", "Front1CANCfg0", b'\x20\x40\x00\xFF\x00\x00\x00\x00', 0.8)
-    
-    timeout = 40   #seconds
-    SC.subscribe_signal(stub, s, r, ns, timeout)
-    #record signal we send as well
-    SC.subscribe_signal(stub, r, s, ns, timeout)
-    
-    print()
-    step_0(stub, s, r, ns)
-    
-    print ("precondition testok:", testresult, "\n")
+def step_2(can_p):
+    """
+    Teststep 2: verify RoutineControlRequest is not applicable for Type 2
+    """
+    cpay: CanPayload = {
+        "payload": SC_CARCOM.can_m_send("RoutineControlRequestSID",
+                                        b'\x40\x50',
+                                        b'\x01'),
+        "extra": ''
+        }
+    SIO.extract_parameter_yml(str(inspect.stack()[0][3]), cpay)
+    etp: CanTestExtra = {
+        "step_no": 2,
+        "purpose": "verify RoutineControl is not applicable for Type 2",
+        "timeout": 1,
+        "min_no_messages": -1,
+        "max_no_messages": -1
+        }
+    SIO.extract_parameter_yml(str(inspect.stack()[0][3]), etp)
 
-    
-# teststep 0: Complete ECU Part/Serial Number(s)
-def step_0(stub, s, r, ns):
-    global testresult
-    
-    stepno = 0
-    purpose = "Complete ECU Part/Serial Number(s)"
-    timeout = 5
-    min_no_messages = -1
-    max_no_messages = -1
-    
-    can_m_send = SC.can_m_send( "ReadDataByIdentifier", b'\xED\xA0', "")
-    can_mr_extra = ''
+    result = SUTE.teststep(can_p, cpay, etp)
+    result = result and SUTE.test_message(SC.can_messages[can_p["receive"]], teststring='7F3131')
+    logging.info(SUTE.pp_decode_7f_response(SC.can_frames[can_p["receive"]][0][2]))
+    time.sleep(1)
+    return result
 
-    testresult = testresult and SuTe.teststep(stub, can_m_send, can_mr_extra, s, r, ns, stepno, purpose, timeout, min_no_messages, max_no_messages)
-    print(SuTe.PP_CombinedDID_EDA0(SC.can_messages[r][0][2], title=''))
 
-# teststep 1: verify RoutineControlRequest is sent for Type 1
-def step_1(stub, s, r, ns):
-    global testresult
-    
-    stepno = 1
-    purpose = "verify RoutineControl start are sent for Type 1"
-    timeout = 1 #wait a second for reply to be send
-    min_no_messages = 1
-    max_no_messages = 1
+def step_3(can_p):
+    """
+    Teststep 3: verify RoutineControlRequest is not applicable for Type 3
+    """
+    cpay: CanPayload = {
+        "payload": SC_CARCOM.can_m_send("RoutineControlRequestSID",
+                                        b'\x40\x00',
+                                        b'\x01'),
+        "extra": ''
+        }
+    SIO.extract_parameter_yml(str(inspect.stack()[0][3]), cpay)
+    etp: CanTestExtra = {
+        "step_no": 3,
+        "purpose": "verify RoutineControl is not applicable for Type 3",
+        "timeout": 1,
+        "min_no_messages": -1,
+        "max_no_messages": -1
+        }
+    SIO.extract_parameter_yml(str(inspect.stack()[0][3]), etp)
 
-    #SC.can_m_send( "Read counters", b'\x0B\x45\x00') #Request current session
-    can_m_send = SC.can_m_send( "RoutineControlRequestSID",b'\x02\x06', b'\x01')
-    can_mr_extra = ''
-    #print(SC.can_m_send( "Read counters", b'\x0B\x45\x00'))
-
-    testresult = testresult and SuTe.teststep(stub, can_m_send, can_mr_extra, s, r, ns, stepno, purpose, timeout, min_no_messages, max_no_messages)
-    
-    print(SuTe.PP_Decode_Routine_Control_response(SC.can_frames[r][0][2]))
-
-# teststep 2: verify RoutineControlRequest is not applicable for Type 2
-def step_2(stub, s, r, ns):
-    global testresult
-    
-    stepno = 2
-    purpose = "verify RoutineControl is not applicable for Type 2"
-    timeout = 1 #wait a second for reply to be send
-    min_no_messages = -1
-    max_no_messages = -1
-
-    #SC.can_m_send( "Read counters", b'\x0B\x45\x00') #Request current session
-    can_m_send = SC.can_m_send( "RoutineControlRequestSID",b'\x40\x50', b'\x01')
-    can_mr_extra = ''
-    #print(SC.can_m_send( "Read counters", b'\x0B\x45\x00'))
-
-    testresult = testresult and SuTe.teststep(stub, can_m_send, can_mr_extra, s, r, ns, stepno, purpose, timeout, min_no_messages, max_no_messages)
-
-    testresult = testresult and SuTe.test_message(SC.can_messages[r], teststring='7F3131')
-
-    print(SuTe.PP_Decode_7F_response(SC.can_frames[r][0][2]))
-    
-    time.sleep(1)  
-
-# teststep 3: verify RoutineControlRequest is not applicable for Type 3
-def step_3(stub, s, r, ns):
-    global testresult
-    
-    stepno = 3
-    purpose = "verify RoutineControl is not applicable for Type 3"
-    timeout = 1 #wait a second for reply to be send
-    min_no_messages = -1
-    max_no_messages = -1
-
-    
-    can_m_send = SC.can_m_send( "RoutineControlRequestSID",b'\x40\x00', b'\x01')
-    can_mr_extra = ''
-    
-
-    testresult = testresult and SuTe.teststep(stub, can_m_send, can_mr_extra, s, r, ns, stepno, purpose, timeout, min_no_messages, max_no_messages)
-    
-    testresult = testresult and SuTe.test_message(SC.can_messages[r], teststring='7F3131') 
-
-    print(SuTe.PP_Decode_7F_response(SC.can_frames[r][0][2]))
+    result = SUTE.teststep(can_p, cpay, etp)
+    result = result and SUTE.test_message(SC.can_messages[can_p["receive"]], teststring='7F3131')
+    logging.info(SUTE.pp_decode_7f_response(SC.can_frames[can_p["receive"]][0][2]))
+    return result
 
 def run():
-    global testresult
-    
+    """
+    Run - Call other functions from here
+    """
+
+    logging.basicConfig(format=' %(message)s', stream=sys.stdout, level=logging.INFO)
+
     # where to connect to signal_broker
-    network_stub = SC.connect_to_signalbroker(ODTB_conf.ODTB2_DUT, ODTB_conf.ODTB2_PORT)
+    can_p: CanParam = {
+        "netstub" : SC.connect_to_signalbroker(odtb_conf.ODTB2_DUT, odtb_conf.ODTB2_PORT),
+        "send" : "Vcu1ToBecmFront1DiagReqFrame",
+        "receive" : "BecmToVcu1Front1DiagResFrame",
+        "namespace" : SC.nspace_lookup("Front1CANCfg0")
+    }
+    SIO.extract_parameter_yml(str(inspect.stack()[0][3]), can_p)
 
-    can_send = "Vcu1ToBecmFront1DiagReqFrame"
-    can_receive = "BecmToVcu1Front1DiagResFrame"
-    can_namespace = SC.nspace_lookup("Front1CANCfg0")
-
-    print ("Testcase start: ", datetime.now())
+    logging.info("Testcase start: %s", datetime.now())
     starttime = time.time()
-    print ("time ", time.time())
-    print()
+    logging.info("Time: %s \n", time.time())
     ############################################
     # precondition
     ############################################
-    precondition(network_stub, can_send, can_receive, can_namespace)
-    
+    timeout = 40
+    result = PREC.precondition(can_p, timeout)
+
+    if result:
     ############################################
     # teststeps
     ############################################
     # step 1:
     # action: verify RoutineControl start is sent for Type 1
     # result: BECM sends positive reply
-    step_1(network_stub, can_send, can_receive, can_namespace)
-    
+        result = result and step_1(can_p)
+
     # step2:
     # action: send start RoutineControl signal for Type 2
     # result: BECM sends NRC (requestOutOfRange)
-    step_2(network_stub, can_send, can_receive, can_namespace)
+        result = result and step_2(can_p)
 
     # step3:
     # action: send start RoutineControl signal for Type 3
     # result: BECM sends NRC (requestOutOfRange)
-    step_3(network_stub, can_send, can_receive, can_namespace)
+        result = result and step_3(can_p)
 
-   
     ############################################
     # postCondition
     ############################################
-            
-    print()
-    print ("time ", time.time())
-    print ("Testcase end: ", datetime.now())
-    print ("Time needed for testrun (seconds): ", int(time.time() - starttime))
 
-    print ("Do cleanup now...")
-    print ("Stop all periodic signals sent")
-    #SC.stop_heartbeat()
-    SC.stop_periodic_all()
-    #time.sleep(5)
+    POST.postcondition(can_p, starttime, result)
 
-    # deregister signals
-    SC.unsubscribe_signals()
-    # if threads should remain: try to stop them 
-    SC.thread_stop()
-            
-    print ("Test cleanup end: ", datetime.now())
-    print()
-    if testresult:
-        print ("Testcase result: PASSED")
-    else:
-        print ("Testcase result: FAILED")
-
-    
 if __name__ == '__main__':
     run()
