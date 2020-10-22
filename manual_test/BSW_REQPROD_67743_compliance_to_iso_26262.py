be8065a7ee25bf5f77dbbca3cd1ebd2c928d3f3b
fjansso8 <fredrik.it.jansson@volvocars.com>
2020-10-22 21:25:11 +0200

    11 NA scripts. I put them in the manual test root since we anyway will go for flat structure.

---
+++ manual_test/BSW_REQPROD_67743_compliance_to_iso_26262.py
@@ -0,0 +1,32 @@
+""" Testscript for a requirement which is not applicable for us (Not applicable).
+    For example: Test multiple processors when you only have one.
+    Testscript ODTB2 MEPII
+    project  BECM basetech MEPII
+    author   FJANSSO8 (Fredrik Jansson)
+    date     2020-10-22
+    version  1.0
+    reqprod  67743
+
+    #inspired by httpsgrpc.iodocstutorialsbasicpython.html
+    Copyright 2015 gRPC authors.
+
+    Licensed under the Apache License, Version 2.0 (the License);
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        httpwww.apache.orglicensesLICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an AS IS BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+"""
+
+import logging
+import sys
+
+logging.basicConfig(format='%(asctime)s - %(message)s',
+                    stream=sys.stdout, level=logging.DEBUG)
+
+logging.info("Testcase result: Not applicable")
