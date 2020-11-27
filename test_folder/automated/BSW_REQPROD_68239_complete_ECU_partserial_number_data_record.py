b3715e8c407f1990ad0e5805b74837f51ad8fd8d
Anton Svensson <anton.svensson.2@volvocars.com>
2020-11-27 14:46:11 +0100

    Pylint fix Reorder imports.
    ARTTBCC-5262

--- test_folder/automated/BSW_REQPROD_68239_complete_ECU_partserial_number_data_record.py
+++ test_folder/automated/BSW_REQPROD_68239_complete_ECU_partserial_number_data_record.py
@@ -29,6 +29,8 @@ import sys
 import logging
 import inspect
 
+import odtb_conf
+
 from supportfunctions.support_can import SupportCAN, CanParam,CanPayload, CanTestExtra
 from supportfunctions.support_test_odtb2 import SupportTestODTB2
 from supportfunctions.support_carcom import SupportCARCOM
@@ -38,7 +40,6 @@ from supportfunctions.support_precondition import SupportPrecondition
 from supportfunctions.support_postcondition import SupportPostcondition
 from supportfunctions.support_service22 import SupportService22
 from supportfunctions.support_service10 import SupportService10
-import odtb_conf
 
 SIO = SupportFileIO
 SC = SupportCAN()
