6d8b7e54c3d98ff588d8e9c6705e9cc950a785ea
Daniel Hjelm <daniel.hjelm@volvocars.com>
2021-01-13 17:00:24 +0100

    add dvm creator

---
+++ dvm.py
@@ -0,0 +1,173 @@
+#!/usr/bin/env python
+
+import importlib.util
+import sys
+from pathlib import Path
+import yaml
+
+from docx import Document
+from docx.shared import Inches
+from docx.shared import Pt
+from docx.shared import Mm
+from docx.enum.style import WD_STYLE_TYPE
+from docx.enum.table import WD_ROW_HEIGHT_RULE
+from docx.enum.table import WD_CELL_VERTICAL_ALIGNMENT
+from docx.enum.section import WD_ORIENTATION
+
+def create_dvm(test_file_py):
+    spec = importlib.util.spec_from_file_location("req_test", test_file_py)
+    req_test = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(req_test)
+    reqdata = yaml.safe_load(req_test.__doc__)
+    reqdata.setdefault('reqprod', '')
+    reqdata.setdefault('version', '')
+    reqdata.setdefault('title', '')
+    reqdata.setdefault('purpose', '')
+    reqdata.setdefault('description', '')
+    reqdata.setdefault('precondition', 'Default session')
+    reqdata.setdefault('postcondition', 'Default session')
+    reqdata.setdefault('details', '')
+
+
+    steps = []
+    for step_name in dir(req_test):
+        if step_name.startswith("step_"):
+            step = getattr(req_test, step_name)
+            d = yaml.safe_load(step.__doc__)
+            d.setdefault('action', '')
+            d.setdefault('expected_result', '')
+            d.setdefault('result', '')
+            d.setdefault('comment', '')
+            steps.append(d)
+
+    dvm = Document()
+
+    section = dvm.sections[-1]
+    section.orientation = WD_ORIENTATION.LANDSCAPE
+
+    section = dvm.sections[0]
+    section.page_height = Mm(210)
+    section.page_width = Mm(297)
+    section.left_margin = Mm(25.4)
+    section.right_margin = Mm(25.4)
+    section.top_margin = Mm(25.4)
+    section.bottom_margin = Mm(25.4)
+    section.header_distance = Mm(12.7)
+    section.footer_distance = Mm(12.7)
+
+    styles = dvm.styles
+    heading = styles.add_style('Heading', WD_STYLE_TYPE.PARAGRAPH)
+    heading.font.name = 'Calibri Light'
+    heading.font.size = Pt(28)
+
+    body = styles.add_style('Body', WD_STYLE_TYPE.PARAGRAPH)
+    body.font.name = 'Calibri'
+    body.font.size = Pt(11)
+    body.font.bold = True
+
+    table_body = styles.add_style('Table Body', WD_STYLE_TYPE.TABLE)
+    table_body.font.name = 'Calibri'
+    table_body.font.size = Pt(11)
+
+    border_table_body = styles['Table Grid']
+    border_table_body.font.name = 'Calibri'
+    border_table_body.font.size = Pt(11)
+
+    dvm_heading = f'REQPROD {reqdata["reqprod"]} / MAIN ; {reqdata["version"]}'
+
+    p = dvm.add_paragraph(dvm_heading)
+    p.style = dvm.styles['Heading']
+
+    p = dvm.add_paragraph('Tprocedure:')
+    p.style = dvm.styles['Heading']
+
+    records = (
+        ('General:', dvm_heading),
+        ('Title:', reqdata['title']),
+        ('Purpose:', reqdata['purpose']),
+        ('Description:', reqdata['description'])
+    )
+
+    table = dvm.add_table(rows=0, cols=2)
+    table.style = table_body
+
+    for title, content in records:
+        row = table.add_row()
+        p = row.cells[0].paragraphs[0]
+        p.style = body
+        p.text = title
+        row.cells[0].vertical_alignment = WD_CELL_VERTICAL_ALIGNMENT.TOP
+        row.cells[1].text = content
+
+    table.columns[0].width = Mm(25)
+    table.columns[1].width = Mm(218.4)
+
+    #  dvm.add_page_break()
+
+    table = dvm.add_table(rows=1, cols=2)
+    table.style = table_body
+    row = table.rows[0]
+    p = row.cells[0].paragraphs[0]
+    p.style = body
+    p.text = "Precondition:"
+    p = row.cells[1].paragraphs[0]
+    p.text = reqdata["precondition"]
+    table.columns[0].width = Mm(25)
+    table.columns[1].width = Mm(218.4)
+
+    p = dvm.add_paragraph("Test execution:")
+    p.style = body
+
+    table = dvm.add_table(rows=1, cols=5)
+    table.style = 'Table Grid'
+    hdr_cells = table.rows[0].cells
+    hdr_cells[0].text = 'Test Step'
+    hdr_cells[1].text = 'Action (client)'
+    hdr_cells[2].text = 'Expected Result (ECU)'
+    hdr_cells[3].text = 'Result'
+    hdr_cells[4].text = 'Comment'
+
+    for step, d in enumerate(steps, 1):
+        row = table.add_row()
+        row.cells[0].text = str(step)
+        row.cells[1].text = d["action"]
+        row.cells[2].text = d["expected_result"]
+        row.cells[3].text = d["result"]
+        row.cells[4].text = d["comment"]
+
+    table.columns[0].width = Mm(19)
+    table.columns[1].width = Mm(71.3)
+    table.columns[2].width = Mm(32.5)
+    table.columns[3].width = Mm(14.8)
+    table.columns[4].width = Mm(101.2)
+
+    records = (
+        ('Postcondition:', reqdata['postcondition']),
+        ('Details:', reqdata['details']),
+        ('Path:', '-'),
+    )
+
+    table = dvm.add_table(rows=0, cols=2)
+    table.style = table_body
+
+    for title, content in records:
+        row = table.add_row()
+        p = row.cells[0].paragraphs[0]
+        p.style = body
+        p.text = title
+        row.cells[0].vertical_alignment = WD_CELL_VERTICAL_ALIGNMENT.TOP
+        row.cells[1].text = content
+
+    table.columns[0].width = Mm(25)
+    table.columns[1].width = Mm(218.4)
+    docx_filename = Path(test_file_py).with_suffix('.docx').name
+    docx_filename = docx_filename.replace("BSW_REQPROD_", "REQ_")
+    print("Generated file: ", docx_filename)
+
+    dvm.save(docx_filename)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+           sys.exit("Usage: dvm.py <test_file.py>")
+    create_dvm(sys.argv[1])
