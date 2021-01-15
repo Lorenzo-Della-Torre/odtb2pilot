cfe6237a1ff9b3f03061bea6dc899cb4c8caf9f8
Daniel Hjelm <daniel.hjelm@volvocars.com>
2021-01-15 09:35:06 +0100

    removing old and duplicated swdl scripts

--- autotest/SWDL_A_sample.sh
+++
@@ -1,2 +0,0 @@
-python3 BSW_SWDL.py ./VBF_A_sample/sbl_32263410_AA.vbf ./VBF_A_sample/ess_32263223_AA_6M.vbf ./VBF_A_sample/32263219XC.vbf ./VBF_A_sample/32263220XC.vbf ./VBF_A_sample/32263221XC.vbf ./VBF_A_sample/32263222XA.vbf
-
