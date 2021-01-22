7990fdc577f8eecea749f82fafd8f9441e137672
Gustav Andersson <gustav.andersson@sigmatechnology.se>
2021-01-22 13:36:24 +0100

    Created script: Implicitly tested

---
+++ test_folder/automated/BSW_REQPROD_56150_P2Server_max_Prg_Session.py
@@ -0,0 +1,38 @@
+"""
+testscript: ODTB2 MEPII
+project:    BECM basetech MEPII
+author:     GANDER10 (Gustav Andersson)
+date:       2020-01-22
+version:    1.0
+reqprod:    56150
+
+title:
+    P2Server_max - ProgrammingSession ; 1
+
+purpose:
+    P2Server_max is the maximum time for the server
+    to start with the response message after the
+    reception of a request message.
+
+description:
+    The maximum time for P2Server in programming session is 25ms.
+
+details:
+    Implicitly tested by:
+        REQPROD 76172 ReadMemoryByAddress (Service 23).
+"""
+
+import logging
+import sys
+
+def run():
+    """
+    Run - Call other functions from here.
+    """
+    logging.basicConfig(format=' %(message)s', stream=sys.stdout, level=logging.INFO)
+
+    logging.info("Testcase result: Implicitly tested by "\
+    "REQPROD 76172 (ReadMemoryByAddress)")
+
+if __name__ == '__main__':
+    run()
