9bb46e0ae7cae787856453b415395f6177aa0d15
Hjelm, Daniel <daniel.hjelm@volvocars.com>
2021-02-01 08:51:38 +0100

    Req 76441 - implicit

---
+++ test_folder/automated/BSW_REQPROD_76441_ReadMemoryByAddress_MS.py
@@ -0,0 +1,33 @@
+"""
+version: 1
+reqprod: 76441
+
+title:
+
+    ReadMemoryByAddress (23) - memorySize (MS)
+
+purpose:
+
+    Compliance with VOLVO CAR CORPORATION tools.
+
+description:
+
+    The ECU shall support the service readMemoryByAddress with the data
+    parameter memorySize in all sessions where the ECU supports the service
+    readMemoryByAddress. The implementer shall define the values of the
+    memorySize.
+
+details: >
+
+    This testcase has been tested by REQPROD 76173 where 10 bytes of data is
+    set as the memorySize. The data is being read from the memory address
+    0x70000000 in each session using the folling payload: 0x232470000000000A.
+    The last 0x0A corresponds to the 10 bytes of data that is requested.
+ """
+
+import logging
+import sys
+
+logging.basicConfig(format=' %(message)s', stream=sys.stdout, level=logging.INFO)
+
+logging.info("Testcase result: Tested implicitly by REQPROD 76173")
