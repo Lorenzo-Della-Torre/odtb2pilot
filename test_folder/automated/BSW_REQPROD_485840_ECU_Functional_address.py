ac0573d7134f01eeed8cfbd5dc9b80c6eed990ad
Gustav Andersson <gustav.andersson@sigmatechnology.se>
2021-02-25 15:35:24 +0100

    Created script - Implicitly tested

---
+++ test_folder/automated/BSW_REQPROD_485840_ECU_Functional_address.py
@@ -0,0 +1,43 @@
+"""
+testscript: ODTB2 MEPII
+project:    BECM basetech MEPII
+author:     GANDER10 (Gustav Andersson)
+date:       2021-02-25
+version:    0
+reqprod:    485840
+
+title:
+    ECU functional address ; 0
+
+purpose:
+    To dedicate a functional logical address for the ECU
+
+description:
+    Rationale:
+        To make it possible for the tester to send a functional request to all ECUs in the vehicle
+        at the same time. Functional addressed requests are used both in the manufacturing and
+        aftersales process.
+
+    Req:
+        The ECU shall implement the functional address specified in the table below.
+
+            - ECU Functional address | 0x1FFF
+
+details:
+    Implicitly tested by:
+        REQPROD 60112 (Supporting functional requsts)
+"""
+
+import logging
+import sys
+
+def run():
+    """
+    Run - Call other functions from here.
+    """
+    logging.basicConfig(format=' %(message)s', stream=sys.stdout, level=logging.INFO)
+
+    logging.info("Testcase result: Implicitly tested by REQPROD 60112")
+
+if __name__ == '__main__':
+    run()
