2150bb2c7323b17aa266ae99fed86707b0f3369c
Daniel Hjelm <daniel.hjelm@volvocars.com>
2021-04-27 17:06:16 +0200

    fix 128838 and shorten the filename

--- test_folder/automated/e_128838_MAIN_1_ecu_ability_to_receive_complete_message.py
+++
@@ -1,6 +0,0 @@
-'''Import script - Inherited from older version of requirement'''
-
-from e_128838_MAIN_0_ecu_ability_to_receive_complete_message import run
-
-if __name__ == '__main__':
-    run()
---
+++ test_folder/automated/e_128838_MAIN_1_ecu_complete_msg.py
@@ -0,0 +1,8 @@
+'''Import script - Inherited from older version of requirement'''
+
+import logging
+import sys
+
+logging.basicConfig(format=' %(message)s', stream=sys.stdout, level=logging.INFO)
+
+logging.info("Testcase result: Tested implicitly by REQPROD 60017 (tested implicitly)")
