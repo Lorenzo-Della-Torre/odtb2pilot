84254b51e86c9aa01d899b417bf5cb75a0f2c847
Daniel Hjelm <daniel.hjelm@volvocars.com>
2021-05-03 22:37:15 +0200

    automatically configure run environment
    lazy loading of runner and nightly

--- manage.py
+++ manage.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """ manage.py: Management commands for the ODTB project """
 
-import os
 import sys
 import logging
+from os import environ
+from os.path import dirname
+from os.path import join
 from argparse import ArgumentParser
 from pathlib import Path
 
@@ -11,12 +13,17 @@ from supportfunctions.support_sddb import parse_sddb_file
 from supportfunctions.support_sddb import get_platform_dir
 from supportfunctions.support_sddb import get_sddb_file
 from supportfunctions.dvm import create_dvm
-from supportfunctions.testrunner import runner
-from supportfunctions.testrunner import nightly
 
-sys.path.append(os.path.dirname(__file__))
-sys.path.append(os.path.join(os.path.dirname(__file__), "test_folder/automated"))
-sys.path.append(os.path.join(os.path.dirname(__file__), "test_folder/manual"))
+def config_environ(platform):
+    """automatically set pythonpath and environment variables"""
+    sys.path.append(dirname(__file__))
+    sys.path.append(join(dirname(__file__), "test_folder/automated"))
+    sys.path.append(join(dirname(__file__), "test_folder/manual"))
+    odtb_proj = f"MEP2_{platform}"
+    odtb_proj_parm = join(dirname(__file__), f"projects/{odtb_proj}")
+    environ["ODTBPROJ"] = odtb_proj
+    environ["ODTBPROJPARAM"] = odtb_proj_parm
+    sys.path.append(odtb_proj_parm)
 
 def check_install():
     """ Make sure that the installation is setup and configured properly """
@@ -81,7 +88,6 @@ def check_install():
     logging.info("PBL diagnostic part number: %s", did.pbl_diag_part_num)
     logging.info("SBL diagnostic part number: %s", did.sbl_diag_part_num)
     logging.info("App diagnostic part number: %s", did.app_diag_part_num)
-
     logging.info("Everything seems fine.")
 
 
@@ -90,9 +96,13 @@ if __name__ == "__main__":
         description="Management commands for the odtb project"
     )
     parser.add_argument(
-        '-l', '--log-level', dest="loglevel", default="info",
+        '-l', dest="loglevel", default="info",
         choices=["notset", "debug", "info", "warning", "error", "critical"],
         help="set logging level")
+
+    parser.add_argument(
+        '-p', dest="platform", default="spa2",
+        choices=["spa1", "spa2", "hlcm", "ed_ifha"], help="set platform")
 
     subparsers = parser.add_subparsers(dest="command")
     subparsers.add_parser(
@@ -134,6 +144,8 @@ if __name__ == "__main__":
         format='%(levelname)s %(message)s', stream=sys.stdout,
         level=getattr(logging, args.loglevel.upper()))
 
+    config_environ(args.platform.upper())
+
     if not args.command:
         parser.print_help()
 
@@ -145,6 +157,8 @@ if __name__ == "__main__":
     elif args.command == 'dvm':
         create_dvm(args.test_script)
     elif args.command == 'run':
+        from supportfunctions.testrunner import runner
         runner(args)
     elif args.command == 'nightly':
+        from supportfunctions.testrunner import nightly
         nightly(args)
