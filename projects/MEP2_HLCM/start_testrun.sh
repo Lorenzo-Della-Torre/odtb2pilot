38ba77424d936dc07eb422a7a7f9796eefa51d36
Daniel Hjelm <daniel.hjelm@volvocars.com>
2021-06-06 05:28:59 +0200

    updated all start_testrun scripts in the repo
    we should really aim to remove these scripts, but let's keep them
    updated for now. Removes all tabs from the files. Partly because it was
    inconsistently used in the files, and we might just as well stick with
    spaces in all files since this is a python project.

--- projects/MEP2_HLCM/start_testrun.sh
+++ projects/MEP2_HLCM/start_testrun.sh
@@ -3,37 +3,37 @@
 SERVICE="python3"
 if pgrep -x "$SERVICE" >/dev/null
 then
-	echo "Can't start testrun: $SERVICE is already running"
+  echo "Can't start testrun: $SERVICE is already running"
 else
-	echo "Start automated testrun"
+  echo "Start automated testrun"
 
-	### token and pass created for tht repo
-	export TESTREPO=~/Repos/odtb2pilot
-	export ODTBPROJ=MEP2_HLCM
-	export ODTBPROJPARAM=$TESTREPO/projects/$ODTBPROJ
-	export PYTHONPATH=$TESTREPO:$ODTBPROJPARAM:.
-	echo TESTREPO: $TESTREPO
-	echo ODTBPROJ: $ODTBPROJ
-	echo ODTBPROJPARAM: $ODTBPROJPARAM
-	echo PYTHONPATH: $PYTHONPATH
+  ### token and pass created for tht repo
+  export TESTREPO=~/Repos/odtb2pilot
+  export ODTBPROJ=MEP2_HLCM
+  export ODTBPROJPARAM=$TESTREPO/projects/$ODTBPROJ
+  export PYTHONPATH=$TESTREPO:$ODTBPROJPARAM:.
+  echo TESTREPO: $TESTREPO
+  echo ODTBPROJ: $ODTBPROJ
+  echo ODTBPROJPARAM: $ODTBPROJPARAM
+  echo PYTHONPATH: $PYTHONPATH
 
-        cd $TESTREPO
-	git pull
-	cd ~/testrun
+  cd $TESTREPO
+  git pull
+  cd ~/testrun
 
-	# Execute test scripts
-        $TESTREPO/projects/project_template/automated_testrun/auto_testscript.sh
+  # Execute test scripts
+  $TESTREPO/projects/project_template/automated_testrun/auto_testscript_swrs_rev_5_analytics.sh
 
-	# Generate did report
-        $TESTREPO/projects/project_template/automated_testrun/generate_did_report.sh
+  # Generate did report
+  $TESTREPO/manage.py did_report
 
-	# Generate test stats plot graph
-        $TESTREPO/projects/project_template/automated_testrun/generate_plot_graph.sh
+  # Generate test stats plot graph
+  $TESTREPO/projects/project_template/automated_testrun/generate_plot_graph.sh
 
-	# Generate log report
-        $TESTREPO/projects/project_template/automated_testrun/generate_log_report.sh
-	
-	echo
-	echo "All scripts executed. All html reports generated"
+  # Generate log report
+  $TESTREPO/projects/project_template/automated_testrun/generate_log_report.sh
+
+  echo
+  echo "All scripts executed. All html reports generated"
 fi
 
