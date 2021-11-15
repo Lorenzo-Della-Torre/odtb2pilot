1eff42e738db393458934ace7a33203730c68743
Weiler <HWEILER@volvocars.com>
2021-11-15 17:53:28 +0100

    init doesn't work without self but causing pylint error

--- supportfunctions/support_can.py
+++ supportfunctions/support_can.py
@@ -55,15 +55,16 @@ class CanParam(Dict): # pylint: disable=too-few-public-methods,inherit-non-class
         All CAN send/receive parameters
     """
     def __init__(self):
-        super(CanParam).__init__(
+        super(CanParam, self).__init__(
             {'netstub': '',
-             'sytem_stub': '',
+             'system_stub': '',
              'send': '',
              'receive': '',
              'namespace': '',
              'padding': True,
              'framelength_max': 8,
              'protocol': 'can'})
+
 
 
 class CanTestExtra(Dict): # pylint: disable=too-few-public-methods,inherit-non-class
