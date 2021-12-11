05d6160ee08f190648f4d3aaae8535d231edb040
pi13 <pi13@example.com>
2021-12-11 20:14:21 +0000

    add copyright to source files

--- test_folder/automated/e_76500_MAIN_2_19_reportdtcbystatusmask_02_82_ext.py
+++ test_folder/automated/e_76500_MAIN_2_19_reportdtcbystatusmask_02_82_ext.py
@@ -1,3 +1,19 @@
+"""
+/*********************************************************************************/
+
+
+
+Copyright © 2021 Volvo Car Corporation. All rights reserved.
+
+
+
+NOTICE: This file contains material that is confidential and confidential to Volvo Cars and/or other developers. No license is granted under any intellectual or industrial property rights of Volvo Cars except as may be provided in an agreement with Volvo Cars. Any unauthorized copying or distribution of content from this file is prohibited.
+
+
+
+/*********************************************************************************/
+"""
+
 '''Import script - Inherited from older version of requirement'''
 
 from e_76500_MAIN_1_19_reportdtcbystatusmask_02_82_ext import run
--- test_folder/manual/e_397293_main_1_number_of_logical_blocks.py
+++ test_folder/manual/e_397293_main_1_number_of_logical_blocks.py
@@ -1,4 +1,20 @@
 """
+/*********************************************************************************/
+
+
+
+Copyright © 2021 Volvo Car Corporation. All rights reserved.
+
+
+
+NOTICE: This file contains material that is confidential and confidential to Volvo Cars and/or other developers. No license is granted under any intellectual or industrial property rights of Volvo Cars except as may be provided in an agreement with Volvo Cars. Any unauthorized copying or distribution of content from this file is prohibited.
+
+
+
+/*********************************************************************************/
+"""
+
+"""
 title:   Number of logical blocks
 reqprod: 397293
 version: 1
