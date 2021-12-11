b95b6f403a79b5ca891c1841498ef5ae0fb86c08
Weiler, Hans-Klaus <hans-klaus.weiler@volvocars.com>
2021-12-11 21:43:14 +0100

    Revert "Merge branch 'Add_copyright_notice' into 'master'"
    This reverts merge request !887

--- test_folder/automated/e_67864_MAIN_3_id_options_system_supplier_specific_data_records.py
+++ test_folder/automated/e_67864_MAIN_3_id_options_system_supplier_specific_data_records.py
@@ -1,19 +1,3 @@
-"""
-/*********************************************************************************/
-
-
-
-Copyright © 2021 Volvo Car Corporation. All rights reserved.
-
-
-
-NOTICE: This file contains material that is confidential and confidential to Volvo Cars and/or other developers. No license is granted under any intellectual or industrial property rights of Volvo Cars except as may be provided in an agreement with Volvo Cars. Any unauthorized copying or distribution of content from this file is prohibited.
-
-
-
-/*********************************************************************************/
-"""
-
 '''Import script - Inherited from older version of requirement'''
 
 from e_67864_MAIN_2_id_options_system_supplier_specific_data_records import run
--- test_folder/automated/e_76139_MAIN_3_ecureset__11_.py
+++ test_folder/automated/e_76139_MAIN_3_ecureset__11_.py
@@ -1,20 +1,4 @@
 """
-/*********************************************************************************/
-
-
-
-Copyright © 2021 Volvo Car Corporation. All rights reserved.
-
-
-
-NOTICE: This file contains material that is confidential and confidential to Volvo Cars and/or other developers. No license is granted under any intellectual or industrial property rights of Volvo Cars except as may be provided in an agreement with Volvo Cars. Any unauthorized copying or distribution of content from this file is prohibited.
-
-
-
-/*********************************************************************************/
-"""
-
-"""
 Testscript Hilding MEPII
 project:  BECM basetech MEPII
 author:   LDELLATO (Lorenzo Della Torre)
