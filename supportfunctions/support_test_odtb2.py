29dbd44a6b8e17dcbc6795180711fda3cb58a456
pi13 <pi13@example.com>
2021-12-11 21:16:48 +0000

    add copyright to source files

--- supportfunctions/support_test_odtb2.py
+++ supportfunctions/support_test_odtb2.py
@@ -1,4 +1,5 @@
 """
+The Python implementation of the gRPC route guide client.
 
 /*********************************************************************************/
 
@@ -17,7 +18,6 @@ Any unauthorized copying or distribution of content from this file is prohibited
 
 
 /*********************************************************************************/
-"""
 
 # project:  Hilding testenvironment using SignalBroker
 # author:   FJANSSO8 (Fredrik Jansson)
@@ -46,7 +46,7 @@ Any unauthorized copying or distribution of content from this file is prohibited
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""The Python implementation of the gRPC route guide client."""
+"""
 
 from __future__ import print_function
 import logging
