6ff134ea46c701c654b12614c143e1ca49d93ef5
pi13 <pi13@example.com>
2021-12-12 00:06:31 +0000

    add copyright to source files

--- test_folder/automated/e_418372_MAIN_2_ecu_sw_part_num_f12c.py
+++ test_folder/automated/e_418372_MAIN_2_ecu_sw_part_num_f12c.py
@@ -17,9 +17,9 @@ Any unauthorized copying or distribution of content from this file is prohibited
 
 
 /*********************************************************************************/
-"""
 
-'''Import script - Inherited from older version of requirement'''
+Import script - Inherited from older version of requirement
+"""
 
 from e_418372_MAIN_1_ecu_sw_part_num_f12c import run
 
--- test_folder/automated/e_68264_MAIN_1_dtc_defined_by_gmrdb.py
+++ test_folder/automated/e_68264_MAIN_1_dtc_defined_by_gmrdb.py
@@ -17,9 +17,7 @@ Any unauthorized copying or distribution of content from this file is prohibited
 
 
 /*********************************************************************************/
-"""
 
-"""
 Testscript Hilding MEPII
 project:  BECM basetech MEPII
 author:   DHJELM (Daniel Hjelm)
