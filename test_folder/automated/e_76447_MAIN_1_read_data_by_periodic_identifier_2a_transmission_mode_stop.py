5adf23bb38643f41dcb07e426dc6e63bdfd37b45
Lingaiah <KLINGAIA@volvocars.com>
2022-03-10 12:41:26 +0000

    Addd req 76447

--- test_folder/automated/e_76447_MAIN_1_read_data_by_periodic_identifier_2a_transmission_mode_stop.py
+++ test_folder/automated/e_76447_MAIN_1_read_data_by_periodic_identifier_2a_transmission_mode_stop.py
@@ -19,7 +19,7 @@ Any unauthorized copying or distribution of content from this file is prohibited
 
 reqprod: 76447
 version: 1
-title: ReadDataByPeriodicIdentifier (2A) - transmissionMode stop (04)
+title: ReadDataByPeriodicIdentifier (2A) - transmissionMode slow (04)
 purpose:
     Shall be possible to stop reading periodic identifiers without for example changing session 
     since if a session change is done a lot of other functionality might reset as well.
@@ -27,27 +27,21 @@ purpose:
 description: >
     The ECU shall support the service readDataByPeriodicIdentifier with the data parameter 
     transmissionMode set to stop in all sessions where the ECU supports the service 
-    readDataByPeriodicIdentifier
+    readDataByPeriodicIdentifier.
 
 details: >
-    Checking response for ReadDataByPeriodicIdentifier(0x2A) in extendedDiagnosticSession and
-    the data parameter transmissionMode set to stop and it should not support defaultSession
-    and programmingSession.
-
+    1. Checking response for ReadDataByPeriodicIdentifier(0x2A) in extendedDiagnosticSession with
+       response code 0x6A and it should not support defaultSession and programmingSession. 
+    2. Verify transmission mode parameter slow in ReadDataByPeriodicIdentifier(0x2A) service.
 """
 
 import logging
-
 from hilding.dut import Dut
 from hilding.dut import DutTestError
-from hilding.conf import Conf
-from supportfunctions.support_service27 import SupportService27
 from supportfunctions.support_carcom import SupportCARCOM
 from supportfunctions.support_file_io import SupportFileIO
 
-CNF = Conf()
 SC_CARCOM = SupportCARCOM()
-SE27 = SupportService27()
 SIO = SupportFileIO()
 
 
@@ -67,7 +61,6 @@ def compare_positive_response(response, parameters, session):
                      "ReadDataByPeriodicIdentifier(0x2A) in %s session as expected",
                      parameters['periodic_did'], session)
         result = True
-
     else:
         logging.error("Test Failed: Expected positive response %s, received %s in %s session",
                       parameters['periodic_did'], response, session)
@@ -95,18 +88,17 @@ def compare_negative_response(response, session):
                       " ReadDataByPeriodicIdentifier(0x2A) request, received %s in %s session",
                       response, session)
         result = False
+
     return result
 
 
 def request_read_data_periodic_identifier(dut: Dut, periodic_did):
     """
-    Request ReadDataByPeriodicIdentifier(0x2A)  with the data parameter transmissionMode set to
+    Request ReadDataByPeriodicIdentifier(0x2A) with the data parameter transmissionMode set to
     stop and get the ECU response
-
     Args:
         dut(class object): Dut instance
         periodic_did(str): Periodic identifier did
-
     Returns: ECU response of ReadDataByPeriodicIdentifier request
     """
 
@@ -119,14 +111,14 @@ def request_read_data_periodic_identifier(dut: Dut, periodic_did):
 
 def step_1(dut: Dut):
     """
-    action: Set to extended mode and verify ReadDataByPeriodicIdentifier(0x2A) response
+    action: Set to extended mode and verify ReadDataByPeriodicIdentifier(0x2A) response  
             with transmission mode parameter set to stop.
 
-    expected_result: ECU should send positive response within 200ms
+    expected_result: ECU should send positive response
     """
 
     dut.uds.set_mode(3)
-    # Read maximum response time and periodic did from yml file
+    # Read periodic did from yml file
     parameters_dict = {'periodic_did': ''}
     parameters = SIO.parameter_adopt_teststep(parameters_dict)
     if not all(list(parameters.values())):
@@ -145,6 +137,7 @@ def step_2(dut: Dut, periodic_did):
     """
     action: Set to default session and verify ReadDataByPeriodicIdentifier(0x2A) negative response
             with transmission mode parameter set to stop.
+
     expected_result: ECU should not support ReadDataByPeriodicIdentifier(0x2A) in default session
     """
 
@@ -175,7 +168,7 @@ def step_3(dut: Dut, periodic_did):
 
 def run():
     """
-    Verify transmission mode stop parameter in ReadDataByPeriodicIdentifier(0x2A) service
+    Verify transmissionMode parameter stop 0x04 in ReadDataByPeriodicIdentifier(0x2A) service
     """
     dut = Dut()
 
@@ -186,18 +179,18 @@ def run():
         dut.precondition(timeout=60)
         result_step, parameters = dut.step(step_1, purpose='Verify '
                                            'ReadDataByPeriodicIdentifier(0x2A) response '
-                                           'in extended session with the data parameter '
-                                           'transmissionMode set to stop (04) ')
+                                           'in extended session with the transmissionMode'
+                                           'parameter set to stop')
         if result_step:
             result_step = dut.step(step_2, parameters['periodic_did'], purpose='verify '
                                    'ReadDataByPeriodicIdentifier(0x2A) negative response '
-                                   'in default session with the data parameter '
-                                   'transmissionMode set to stop (04) ')
+                                   'in default session with the transmissionMode '
+                                   'parameter set to stop')
         if result_step:
             result_step = dut.step(step_3, parameters['periodic_did'], purpose='verify '
                                    'ReadDataByPeriodicIdentifier(0x2A) negative response '
-                                   'in programming session with the data parameter '
-                                   'transmissionMode set to stop (04) ')
+                                   'in programming session with the transmissionMode '
+                                   'parameter set to stop')
 
         result = result_step
     except DutTestError as error:
