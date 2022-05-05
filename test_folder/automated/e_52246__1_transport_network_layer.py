cc41a2a9bb539a2f8de647399dd09da0935969cc
ABHALE <ABHALE@volvocar.com>
2022-05-05 12:28:17 +0000

    rewrote Req-488703 Transport_Network Layer

--- test_folder/automated/e_52246__1_transport_network_layer.py
+++ test_folder/automated/e_52246__1_transport_network_layer.py
@@ -1,10 +1,9 @@
 """
-
 /*********************************************************************************/
 
 
 
-Copyright © 2021 Volvo Car Corporation. All rights reserved.
+Copyright © 2022 Volvo Car Corporation. All rights reserved.
 
 
 
@@ -18,169 +17,105 @@ Any unauthorized copying or distribution of content from this file is prohibited
 
 /*********************************************************************************/
 
-# Testscript Hilding MEPII
-# project:  BECM basetech MEPII
-# author:   LDELLATO (Lorenzo Della Torre)
-# date:     2020-03-12
-# version:  1.0
-# reqprod:  52246
 
-# author:   LDELLATO (Lorenzo Della Torre)
-# date:     2020-09-25
-# version:  1.1
-# reqprod:  52246
+reqprod: 52246
+version: 1
+title: : Transport_Network Layer
 
-# #inspired by https://grpc.io/docs/tutorials/basic/python.html
-# Copyright 2015 gRPC authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+purpose: >
+    To define the transport/network layer used by the bootloader.
 
-The Python implementation of the gRPC route guide client.
+description: >
+    The CAN bootloader shall implement the transport/network layer defined in LC VCC DoCAN.
+
+details: >
+    Verify the transport/network layer used by the bootloader.
+    Steps:
+    1. Verify programming preconditions
+    2. Verify ECU in Primary Bootloader Session
 """
 
-import time
-from datetime import datetime
-import sys
 import logging
-import inspect
-
-import odtb_conf
-from supportfunctions.support_can import SupportCAN, CanParam, CanTestExtra, CanPayload
-from supportfunctions.support_test_odtb2 import SupportTestODTB2
-from supportfunctions.support_carcom import SupportCARCOM
-from supportfunctions.support_file_io import SupportFileIO
-from supportfunctions.support_SBL import SupportSBL
-from supportfunctions.support_sec_acc import SupportSecurityAccess
-from supportfunctions.support_rpi_gpio import SupportRpiGpio
-
-from supportfunctions.support_precondition import SupportPrecondition
-from supportfunctions.support_postcondition import SupportPostcondition
-from supportfunctions.support_service10 import SupportService10
-from supportfunctions.support_service11 import SupportService11
+from hilding.dut import Dut
+from hilding.dut import DutTestError
 from supportfunctions.support_service22 import SupportService22
-from supportfunctions.support_service27 import SupportService27
 from supportfunctions.support_service31 import SupportService31
-from hilding.conf import Conf
+from supportfunctions.support_service27 import SupportService27
 
-SIO = SupportFileIO
-SC = SupportCAN()
-S_CARCOM = SupportCARCOM()
-SUTE = SupportTestODTB2()
-SSBL = SupportSBL()
-SSA = SupportSecurityAccess()
-SGPIO = SupportRpiGpio()
-
-PREC = SupportPrecondition()
-POST = SupportPostcondition()
-SE10 = SupportService10()
-SE11 = SupportService11()
 SE22 = SupportService22()
-SE27 = SupportService27()
 SE31 = SupportService31()
-conf = Conf()
+SE27 = SupportService27()
 
-def step_4(can_p):
-    '''
-    Teststep 4: verify service 22 is implemented in PBL
-    '''
 
-    cpay: CanPayload = {"payload" : S_CARCOM.can_m_send("ReadDataByIdentifier", b'\xF1\x2A', b''),
-                        "extra" : ''
-                       }
+def step_1(dut: Dut):
+    """
+    action: Verify programming preconditions
+    expected_result: True when routine control request and security access are
+                     successful in programming session
+    """
+    result = SE31.routinecontrol_requestsid_prog_precond(dut, stepno=1)
 
-    SIO.extract_parameter_yml(str(inspect.stack()[0][3]), cpay)
+    if not result:
+        logging.error("Test Failed: Routine control request failed")
+        return False
 
-    etp: CanTestExtra = {"step_no": 4,
-                         "purpose" : "verify service 22 is implemented in PBL",
-                         "timeout" : 1,
-                         "min_no_messages" : -1,
-                         "max_no_messages" : -1
-                        }
+    # Set ECU to Programming Session
+    dut.uds.set_mode(2)
 
-    SIO.extract_parameter_yml(str(inspect.stack()[0][3]), etp)
+    # Security access to ECU
+    security_access = SE27.activate_security_access_fixedkey(dut, dut.conf.default_rig_config,
+                                                            step_no=272, purpose="SecurityAccess")
+    if not security_access:
+        logging.error("Test Failed: security access denied in programming session")
+        return False
 
-    result = SUTE.teststep(can_p, cpay, etp)
-    result = result and SUTE.test_message(SC.can_messages[can_p["receive"]],
-                                          teststring='62F12A')
+    logging.info("Security access successful in programming session")
+    return True
 
-    return result
+
+def step_2(dut: Dut):
+    """
+    action: Verify ECU in Primary Bootloader Session and then reset
+            ECU to get in default session
+    expected_result: True when ECU is in default session
+    """
+    result = SE22.verify_pbl_session(dut)
+    if not result:
+        logging.error("Test Failed: ECU is not in Primary Bootloader Session")
+        return False
+
+    # Reset ECU(1101)
+    reset_result = dut.uds.ecu_reset_1101()
+
+    result = reset_result and SE22.read_did_f186(dut, dsession=b'\x01')
+    if not result:
+        logging.error("Test Failed: ECU not in default session")
+        return False
+
+    logging.info("ECU is in default session")
+    return True
 
 
 def run():
     """
-    Run - Call other functions from here
+    Verify the transport/network layer used by the bootloader.
     """
-    logging.basicConfig(format=' %(message)s', stream=sys.stdout, level=logging.INFO)
+    dut = Dut()
+    start_time = dut.start()
+    result = False
+    result_step = False
+    try:
+        dut.precondition(timeout=30)
+        result_step = dut.step(step_1, purpose="Verify programming preconditions")
+        if result_step:
+            result_step = dut.step(step_2, purpose="Verify ECU in Primary Bootloader"
+                                   " Session and then reset ECU to get in default session")
+        result = result_step
+    except DutTestError as error:
+        logging.error("Test failed: %s", error)
+    finally:
+        dut.postcondition(start_time, result)
 
-    # where to connect to signal_broker
-    can_p: CanParam = {
-        "netstub" : SC.connect_to_signalbroker(odtb_conf.ODTB2_DUT, odtb_conf.ODTB2_PORT),
-        "send" : "Vcu1ToBecmFront1DiagReqFrame",
-        "receive" : "BecmToVcu1Front1DiagResFrame",
-        "namespace" : SC.nspace_lookup("Front1CANCfg0")
-    }
-    SIO.extract_parameter_yml(str(inspect.stack()[0][3]), can_p)
-    logging.info("Testcase start: %s", datetime.now())
-    starttime = time.time()
-    logging.info("Time: %s \n", time.time())
-    ############################################
-    # precondition
-    ############################################
-    timeout = 60
-    result = PREC.precondition(can_p, timeout)
-
-    if result:
-    ############################################
-    # teststeps
-    ############################################
-
-        # step 1:
-        # action: Verify programming preconditions
-        # result: ECU sends positive reply
-        result = result and SE31.routinecontrol_requestsid_prog_precond(can_p, stepno=1)
-        time.sleep(1)
-
-        # step 2:
-        # action: Change to programming session
-        # result: ECU sends positive reply
-        result = result and SE10.diagnostic_session_control_mode2(can_p, stepno=2)
-
-        # step 3:
-        # action: Security Access Request SID
-        # result: ECU sends positive reply
-        result = result and SE27.activate_security_access_fixedkey(can_p, conf.default_rig_config)
-
-        # step 4:
-        # action: verify service 22 is implemented in PBL
-        # result: ECU sends positive reply
-        result = result and step_4(can_p)
-
-        # step 5:
-        # action: Hard Reset
-        # result: ECU sends positive reply
-        result = result and SE11.ecu_hardreset(can_p, stepno=5)
-        time.sleep(1)
-
-        # step 6:
-        # action: verify ECU in default session
-        # result: ECU sends positive reply
-        result = result and SE22.read_did_f186(can_p, b'\x01', stepno=6)
-
-    ############################################
-    # postCondition
-    ############################################
-
-    POST.postcondition(can_p, starttime, result)
 
 if __name__ == '__main__':
     run()
