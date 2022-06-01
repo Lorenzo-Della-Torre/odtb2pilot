293ae71f9611dc5d5a4969ea98c224617ef0adb5
Wankhede <AWANKHED@volvocars.com>
2022-06-01 12:06:12 +0000

    Adding REQID-67761, Vehicle manufacturer specific data records defined in GMRDB (bug fix script)

--- test_folder/automated/e_67761_MAIN_3_manufacturer_dr.py
+++ test_folder/automated/e_67761_MAIN_3_manufacturer_dr.py
@@ -4,7 +4,7 @@
 
 
 
-Copyright © 2021 Volvo Car Corporation. All rights reserved.
+Copyright © 2022 Volvo Car Corporation. All rights reserved.
 
 
 
@@ -18,127 +18,176 @@ Any unauthorized copying or distribution of content from this file is prohibited
 
 /*********************************************************************************/
 
-# Testscript Hilding MEPII
-# project:  BECM basetech MEPII
-# author:   J-ADSJO
-# date:     2020-11-09
-# version:  1.1
-# reqprod:  67761
-# title:    Vehicle manufacture specific data records defined in GMRDB
-# mode:     Default
+reqprod: 67761
+version: 3
+title: Vehicle manufacturer specific data records defined in GMRDB
+purpose: >
+    Volvo car corporation defines mandatory data records in GMRDB
 
-#inspired by https://grpc.io/docs/tutorials/basic/python.html
+description: >
+    Data records with data identifiers in the ranges as specified in the table below and shall be
+    implemented exactly as they are defined in Carcom - Global Master Reference Database.
+    ----------------------------------------------------------------------
+    Description	                                      Identifier range
+    ----------------------------------------------------------------------
+    Vehicle manufacturer specific data records	        0100 - D8FF
+                                                        DE00 - E2FF
+                                                        E600 - ED1F
+                                                        ED80 - ED9F
+                                                        F010 - F0FF
+    ----------------------------------------------------------------------
+    It shall be possible to read the data record by using the diagnostic service specified in
+    Ref[LC : Volvo Car Corporation - UDS Services -Service 0x22 (ReadDataByIdentifier) Reqs]
 
-# Copyright 2015 gRPC authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-The Python implementation of the gRPC route guide client.
+details: >
+    Verify all DIDs in application DIDs in sddb within the given range in default session and
+    extended session
 """
 
-import time
-from datetime import datetime
-import sys
 import logging
-import inspect
-import odtb_conf
-
-from build.did import app_did_dict
-from build.did import app_diag_part_num
-
-from supportfunctions.support_can import SupportCAN, CanParam, CanPayload, CanTestExtra
+from hilding.dut import Dut
+from hilding.dut import DutTestError
 from supportfunctions.support_test_odtb2 import SupportTestODTB2
-from supportfunctions.support_carcom import SupportCARCOM
-from supportfunctions.support_file_io import SupportFileIO
-from supportfunctions.support_service10 import SupportService10
+from supportfunctions.support_can import SupportCAN
 from supportfunctions.support_service22 import SupportService22
-from supportfunctions.support_precondition import SupportPrecondition
-from supportfunctions.support_postcondition import SupportPostcondition
+from supportfunctions.support_file_io import SupportFileIO
 
-SIO = SupportFileIO
 SC = SupportCAN()
 SUTE = SupportTestODTB2()
-SC_CARCOM = SupportCARCOM()
-PREC = SupportPrecondition()
-POST = SupportPostcondition()
-SE10 = SupportService10()
+SIO = SupportFileIO()
 SE22 = SupportService22()
 
-# For each each DID, wait this time for the response. If you wait to short time you might not get
-# the full message (payload). The unit is seconds. 2s should cover even the biggest payloads.
-RESPONSE_TIMEOUT = 2
 
-# Test this amount of DIDs. Example: If equal to 10, the program will only test the first 10 DIDs.
-# This is to speed up during testing.
-# 500 should cover all DIDs
-MAX_NO_OF_DIDS = 500
-
-# Reserve this time for the full script (seconds)
-SCRIPT_TIMEOUT = MAX_NO_OF_DIDS * RESPONSE_TIMEOUT + 15
-
-def step_2(can_p):
+def read_did_f120(dut, app_diag_part_num):
     """
-    Read DID F120: Application Diagnostic Database Part Number
+    Read application diagnostic database part number and compare it with value from sddb
+    Args:
+        dut(Dut): Dut instance
+        app_diag_part_num(str): Application Diagnostic Database Part Number
+    Returns:
+        (bool): True on successfully verified response with value in sddb
     """
-    stepno = 2
-    etp: CanTestExtra = {
-        "step_no": stepno,
-        "purpose" : "Service22: Application Diagnostic Database Part Number",
-        "timeout" : 1,
-        "min_no_messages" : -1,
-        "max_no_messages" : -1}
-    SIO.extract_parameter_yml(str(inspect.stack()[0][3]), etp)
+    dut.uds.read_data_by_id_22(bytes.fromhex('F120'))
 
-    cpay: CanPayload = {
-        "payload" : SC_CARCOM.can_m_send("ReadDataByIdentifier",
-                                        b'\xF1\x20',
-                                        b''),
-        "extra": b''
-    }
-    SIO.extract_parameter_yml(str(inspect.stack()[0][3]), cpay)
-
-    result = SUTE.teststep(can_p, cpay, etp)
-    part_number = SUTE.pp_partnumber(SC.can_messages[can_p["receive"]][0][2][10:])
+    part_number = SUTE.pp_partnumber(SC.can_messages[dut["receive"]][0][2][10:])
     part_number = part_number.replace(" ", "_")
-    logging.info("Step 2: %s", part_number)
-    return result, part_number
+    if part_number == app_diag_part_num :
+        logging.info("successfully verified response with value in sddb")
+        return True
+
+    logging.error("Test Failed: Unable to verify response with value in sddb")
+    return False
 
 
-def step_4(can_p):
-    '''
-    Test service #22:
-    Verify diagnostic service complies to SDDB
-    Request all APP DIDs in SDDB for ECU
-    '''
-    stepno = 4
+def prepare_lookup_dids(parameter):
+    """
+    Prepare lookup DIDs for the specified ranges
+    Args:
+        parameter(dict): did_list_range
+    Returns:
+        did_list(list): List of lookup DIDs within the range
+    """
+
+    did_list = []
+
+    no_of_range=len(parameter['did_list_range'])
+    for i in range(no_of_range):
+        start_did = int(parameter['did_list_range'][i][0], 16)
+        end_did = int(parameter['did_list_range'][i][1], 16)
+        for did in range(start_did, end_did):
+            did_list.append(hex(did)[2:].upper())
+
+    return did_list
+
+
+def filter_dids(lookup_did_list, app_did_dict):
+    """
+    Filtered DIDs in the lookup_did_list which are present in sddb
+    Args:
+        lookup_did_list(list): Lookup DIDs list within the range
+        app_did_dict(dict): Dict of DIDs
+    Returns:
+        filtered_dids_list(list): List of filtered DIDs
+    """
+    filtered_dids_list = []
+
+    for did in lookup_did_list:
+        if did in app_did_dict:
+            filtered_dids_list.append(did)
+    if len(filtered_dids_list) == 0:
+        logging.error("No valid DIDs found in sddb DIDs list")
+
+    return filtered_dids_list
+
+
+def verify_did_response(did_response_list):
+    """
+    Verify response for all DIDs in filtered_dids_list
+    Args:
+        did_response_list(list): list of DID responses
+    Returns:
+        (bool): True when ECU gives expected response for all DIDs
+    """
+    results=[]
+
+    for result in did_response_list:
+        if result.err_msg:
+            logging.error('Received error for DID %s: %s', result.did, result.err_msg)
+
+        did_result = result.c_did and result.c_sid and result.c_size and not result.err_msg
+        results.append(did_result)
+
+    if len(results) != 0 and all(results):
+        logging.info("ECU gives expected response for all DIDs")
+        return True
+
+    logging.error("Test Failed: Received unexpected response from the ECU for some DID requests")
+    return False
+
+
+def print_logs_for_failed_did(info_entry):
+    """
+    Print logs for failed DIDs
+    Args:
+        info_entry(dut): An instance of Infoentry
+    """
+    logging.info('----------------------')
+    logging.info('Testing DID %s failed.', info_entry.did)
+    logging.info('----------------------')
+    logging.info('DID correct: %s', info_entry.c_did)
+    logging.info('SID correct: %s', info_entry.c_sid)
+    logging.info('Size correct: %s', info_entry.c_size)
+    logging.info('Error message: %s', info_entry.err_msg)
+    logging.info('---------------------------------------')
+
+
+def verify_diagnostic_service(dut, app_did_dict, parameter):
+    """
+    Read and verify response for all DIDs in default and extended session by service 22
+    Args:
+        dut(Dut): Dut instance
+        app_did_dict(dict): Dict of DIDs
+        parameter(dict): max_no_of_dids, response_timeout
+    Returns:
+        (bool): True when ECU gives expected response for all DIDs
+    """
+    max_no_of_dids = parameter['max_no_of_dids']
+    response_timeout = parameter['response_timeout']
     pass_or_fail_counter_dict = {"Passed": 0, "Failed": 0, "conditionsNotCorrect (22)": 0,
                                  "requestOutOfRange (31)": 0}
-    result_list = list()
+    did_response_list = list()
     did_counter = 0
-    stepresult = len(app_did_dict) > 0
-    logging.info("Step %s: DID:s in dictionary: %s", stepno, len(app_did_dict))
+
+    lookup_did_list = prepare_lookup_dids(parameter)
+    filtered_dids_list = filter_dids(lookup_did_list, app_did_dict)
 
     for did_id, did_info in app_did_dict.items():
 
-        if ((0x0100 <= int(did_id, base=16) <= 0xDBFF) or
-            (0xDE00 <= int(did_id, base=16) <= 0xE2FF) or
-            (0xE600 <= int(did_id, base=16) <= 0xED1F) or
-            (0xED80 <= int(did_id, base=16) <= 0xED9F) or
-            (0xF010 <= int(did_id, base=16) <= 0xF0FF)):
+        if did_id in filtered_dids_list:
 
             did_counter += 1
-            if did_counter > MAX_NO_OF_DIDS:
-                logging.info("MAX_NO_OF_DIDS reached: %s", MAX_NO_OF_DIDS)
+            if did_counter > max_no_of_dids:
+                logging.info("max_no_of_dids reached: %s", max_no_of_dids)
                 break
             logging.info('DID counter: %s', str(did_counter))
 
@@ -146,108 +195,115 @@ def step_4(can_p):
             logging.info(did_info)
 
             # Using Service 22 to request a particular DID, returning the result in a dictionary
-            did_dict_from_service_22 = SE22.get_did_info(can_p, did_id, RESPONSE_TIMEOUT)
+            did_dict_from_service_22 = SE22.get_did_info(dut, did_id, response_timeout)
 
             # Copy info to the did_info dictionary from the did_dict
             did_info = SE22.adding_info(did_dict_from_service_22, did_info)
-
             # Summarizing the result
             info_entry, pass_or_fail_counter_dict = SE22.summarize_result(
                 did_info, pass_or_fail_counter_dict, did_id)
+            if not(info_entry.c_did and info_entry.c_sid and info_entry.c_size):
+                print_logs_for_failed_did(info_entry)
 
             # Add the results
-            result_list.append(info_entry)
+            did_response_list.append(info_entry)
 
-            # If any of the tests failed. Quit immediately unless debugging.
-            if not(info_entry.c_did and info_entry.c_sid and info_entry.c_size):
-                logging.info('----------------------')
-                logging.info('Testing DID %s failed.', info_entry.did)
-                logging.info('----------------------')
-                logging.info('DID correct: %s', info_entry.c_did)
-                logging.info('SID correct: %s', info_entry.c_sid)
-                logging.info('Size correct: %s', info_entry.c_size)
-                logging.info('Error message: %s', info_entry.err_msg)
-                logging.info('---------------------------------------')
-                if not logging.getLogger("master").isEnabledFor(logging.DEBUG):
-                    return False
+    result = verify_did_response(did_response_list)
+    return result
 
-    logging.info("Step %s: DID:s checked: %s", stepno, did_counter)
-    logging.info(pass_or_fail_counter_dict)
 
-    for result in result_list:
-        logging.debug('DID: %s, c_did: %s, c_sid: %s, c_size: %s, err_msg: %s',
-                      result.did, result.c_did, result.c_sid, result.c_size,
-                      result.err_msg)
-        stepresult = stepresult and result.c_did and result.c_sid and result.c_size \
-                                and not result.err_msg
+def step_1(dut: Dut):
+    """
+    action: Read sddb file and get dict of DIDs present in app_did_dict and app_diag_part_num
+    expected_result: True when successfully extracted app_did_dict and app_diag_part_num
+                     from sddb file
+    """
+    sddb_file = dut.conf.rig.sddb_dids
 
-    logging.info("Step %s: Result teststep: %s\n", stepno, stepresult)
-    return stepresult
+    if 'app_did_dict' and 'app_diag_part_num' in sddb_file.keys():
+        logging.info("Successfully extracted dict of DIDs present in app_did_dict and "
+                     "app_diag_part_num")
+        return True, sddb_file['app_did_dict'], sddb_file['app_diag_part_num']
+
+    logging.error("Test Failed: Unable to extract dict of DIDs present in app_did_dict and "
+                  "app_diag_part_num")
+    return False, None, None
+
+
+def step_2(dut, app_diag_part_num, app_did_dict, parameters):
+    """
+    action: Verify all DIDs in app_did_dict within given range in default session
+    expected result: True when ECU gives expected response for all DIDs
+    """
+    # Check active diagnostic session
+    result = SE22.read_did_f186(dut, b'\x01')
+    if not result:
+        logging.error("Test Failed: ECU is not in default session ")
+        return False
+
+    logging.info("ECU is in default session as expected")
+
+    result = read_did_f120(dut, app_diag_part_num)
+    if result:
+        result = verify_diagnostic_service(dut, app_did_dict, parameters)
+    return result
+
+
+def step_3(dut, app_diag_part_num, app_did_dict, parameters):
+    """
+    action: Verify all DIDs in app_did_dict within given range in default session
+    expected result: True when ECU gives expected response for all DIDs
+    """
+    # Set session in extended
+    dut.uds.set_mode(3)
+
+    result = read_did_f120(dut, app_diag_part_num)
+    if result:
+        result = verify_diagnostic_service(dut, app_did_dict, parameters)
+
+    dut.uds.set_mode(1)
+    return result
 
 
 def run():
     """
-    Run - Call other functions from here
+    Verify all DIDs in app_did_dict within the given range in default session and extended session
     """
-    logging.basicConfig(format=' %(message)s', stream=sys.stdout, level=logging.INFO)
+    dut = Dut()
 
-    # Where to connect to signal_broker
-    # Should get the values from an yml-file instead
-    can_p: CanParam = {
-        "netstub" : SC.connect_to_signalbroker(odtb_conf.ODTB2_DUT, odtb_conf.ODTB2_PORT),
-        "send" : "Vcu1ToBecmFront1DiagReqFrame",
-        "receive" : "BecmToVcu1Front1DiagResFrame",
-        "namespace" : SC.nspace_lookup("Front1CANCfg0")
-    }
-    SIO.extract_parameter_yml(str(inspect.stack()[0][3]), can_p)
+    start_time = dut.start()
+    result = False
+    result_step = False
 
-    logging.info("Testcase start: %s", datetime.now())
-    starttime = time.time()
-    logging.info("Time: %s \n", time.time())
+    parameters_dict = {'parameters_dict': ''}
+    try:
+        parameters = SIO.parameter_adopt_teststep(parameters_dict)
 
-    ############################################
-    # precondition
-    ############################################
-    result = PREC.precondition(can_p, SCRIPT_TIMEOUT)
+        if not all(list(parameters.values())):
+            raise DutTestError("yml parameters not found")
 
-    #result = result and len(app_did_dict) > 0
-    logging.info("Result: %s", result)
-    if result:
+        script_timeout = parameters['parameters_dict']['script_timeout']
 
-        ############################################
-        # teststeps
-        ############################################
-        # step 1:
-        # action: verify current session
-        # result: default session reported
-        result = result and SE22.read_did_f186(can_p, dsession=b'\x01', stepno=1)
+        dut.precondition(timeout = script_timeout)
 
-        # step 2:
-        # action: Request DID F120
-        # result: ECU responds with Application Diagnostic Database Part Number
-        result_2, app_diag_part_num_response = step_2(can_p)
-        result = result and result_2
+        result_step, app_did_dict, app_diag_part_num = dut.step(step_1, purpose="Read sddb file "
+                                                                "and get dict of DIDs present "
+                                                                "in app_did_dict")
+        if result_step:
+            result_step = dut.step(step_2, app_diag_part_num, app_did_dict,
+                                   parameters['parameters_dict'], purpose= "Verify all DIDs in "
+                                   "app_did_dict within given range in default session")
+        if result_step:
+            result_step = dut.step(step_3, app_diag_part_num, app_did_dict,
+                                   parameters['timeout_dict'], purpose= "Verify all DIDs in "
+                                   "app_did_dict within given range in extended session")
+        result = result_step
+    except DutTestError as error:
+        logging.error("Test failed: %s", error)
 
-        # step 3:
-        # action: Check the app_diag_part_num in SDDB and Application
-        # result: The part numbers should be equal
-        SUTE.print_test_purpose(3, "Compare APP Diagnostic Part Numbers from Data Base and APP")
-        logging.info("APP Diagnostic Part numbers are equal: %s",
-                     (app_diag_part_num == app_diag_part_num_response))
-        result = result and (app_diag_part_num == app_diag_part_num_response)
+    finally:
+        dut.postcondition(start_time, result)
 
-        # step 4:
-        # action: Test all DID:s in dictionary within interval
-        #         [0100, DBFF] or [DE00, E2FF] or [E600, ED1F] or [F010, F0FF]
-        #         (or until limit is reached)
-        # result: ECU responds with DID and size
-        result = result and step_4(can_p)
-
-
-    ############################################
-    # postCondition
-    ############################################
-    POST.postcondition(can_p, starttime, result)
 
 if __name__ == '__main__':
     run()
