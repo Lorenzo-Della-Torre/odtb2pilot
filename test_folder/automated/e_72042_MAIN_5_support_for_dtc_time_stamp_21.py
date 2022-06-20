f52a49a66a27860109907d0b563ad1b77a5c0d15
Khairkar <KKHAIRKA@volvocars.com>
2022-06-20 08:58:09 +0000

    Adding Req-72042 Support for DTC time stamp #21 (Bug fix script)

--- test_folder/automated/e_72042_MAIN_5_support_for_dtc_time_stamp_21.py
+++ test_folder/automated/e_72042_MAIN_5_support_for_dtc_time_stamp_21.py
@@ -4,7 +4,7 @@
 
 
 
-Copyright © 2021 Volvo Car Corporation. All rights reserved.
+Copyright © 2022 Volvo Car Corporation. All rights reserved.
 
 
 
@@ -18,7 +18,24 @@ Any unauthorized copying or distribution of content from this file is prohibited
 
 /*********************************************************************************/
 
-Import script - Inherited from older version of requirement
+reqprod: 72042
+version: 3
+title: Support for DTC time stamp #21
+purpose: >
+    To provide enhanced information about the occurrence of a fault, that may be useful in the
+    analysis of the fault.
+
+description: >
+    For all DTCs supported by the ECU a data record identified by DTCExtendedDataRecordNumber=21
+    shall be implemented according to the following definition:
+    •	The record value shall be equal to the global real time (data record 0xDD00) that is taken
+        the latest time FDC10 reaches UnconfirmedDTCLimit, since DTC information was last cleared
+    •	The time stamp shall only be taken the first time if the FDC10 reaches UnconfirmedDTCLimit
+        more than once during an operation cycle.
+    •	The stored data record shall be reported as a 4 byte value.
+
+details: >
+    Import script - Inherited from older version of requirement
 """
 
 from e_72042_MAIN_3_support_for_dtc_time_stamp_21 import run
