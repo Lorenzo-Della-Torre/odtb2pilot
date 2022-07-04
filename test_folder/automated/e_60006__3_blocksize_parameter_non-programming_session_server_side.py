644081c1ce721961eff53b5670c639c6d760c2f5
Khairkar <KKHAIRKA@volvocars.com>
2022-07-04 13:26:46 +0000

    Refactor code

--- test_folder/automated/e_60006__3_blocksize_parameter_non-programming_session_server_side.py
+++ test_folder/automated/e_60006__3_blocksize_parameter_non-programming_session_server_side.py
@@ -31,8 +31,8 @@ description: >
     a complete transaction.
 
 details: >
-    Verify that the FlowControl (FC) is sent as required in both default and extended session
-    (non-programming session) by sending UDS request with 13 bytes, 300 bytes and 1026 bytes.
+    Verify FlowControl(FC) response in both default and extended session(non-programming session)
+    by sending UDS request with 12 bytes, 300 bytes and 1025 bytes.
 """
 
 import logging
@@ -50,27 +50,21 @@ SC_CARCOM = SupportCARCOM()
 SIO = SupportFileIO()
 
 
-def prepare_payload(did_list, pl_max_length, payload_type):
+def prepare_payload(did_list, payload_length):
     """
-    Create a payload with multiple DIDs to send to the ECU
+    Prepare payload with multiple DIDs to send to the ECU
     Args:
-        did_list (list): DIDs
-        pl_max_length (int): Payload maximum length
-        payload_type (str): Type of payload
+        did_list (list):  Non programming DIDs
+        payload_length (int): Payload maximum length
     Returns:
-        payload (bytes): Read data by id padded payload
+        payload (bytes): Padded payload
     """
     dids_str = ''.join(did_list)
 
-    if payload_type == 'overflow':
-        # Create a payload with size greater than 1024 bytes to send to ECU, padding the payload
-        # with '00' till the size becomes 1025 bytes
-        payload = SC_CARCOM.can_m_send("ReadDataByIdentifier", bytes.fromhex(dids_str), b'')
-    else:
-        # Padding the payload with 0x00 till the size becomes pl_max_length
-        payload = bytes.fromhex(dids_str)
+    payload = bytes.fromhex(dids_str)
 
-    while len(payload) < pl_max_length:
+    # Padding the payload with 0x00 till the size becomes payload_length
+    while len(payload) < payload_length:
         payload = payload + bytes.fromhex('00')
 
     return payload
@@ -80,7 +74,7 @@ def find_did(did_list, response):
     """
     Verify DIDs are present in ECU response
     Args:
-        did_list (list): DIDs
+        did_list (list): Non programming DIDs
         response (str): ECU response
     Returns:
         (bool): True when all DIDs are present
@@ -98,39 +92,39 @@ def find_did(did_list, response):
         logging.info("Successfully verified that all the DIDs are present in ECU response")
         return True
 
-    logging.error("Test Failed: Some DIDs are not present in ECU response")
+    logging.error("Some DIDs are not present in ECU response")
     return False
 
 
-def request_read_data_by_id(dut, dids_to_read, pl_max_length, flag=False):
+def read_did_and_verify(dut, dids_to_read, payload_length, response_flag=False):
     """
     Verify ReadDataByIdentifier service 22 with multiple DIDs
     Args:
         dut (Dut): An instance of Dut
-        dids_to_read (list): DIDs
-        pl_max_length (int): Payload maximum length
-        flag (bool): Check positive response when true
+        dids_to_read (list): Non programming DIDs
+        payload_length (int): Payload maximum length
+        response_flag (bool): Check positive response when true
     Returns:
         (bool): True on successfully verified response
     """
-    payload = prepare_payload(dids_to_read, pl_max_length, payload_type='not overflow')
+    payload = prepare_payload(dids_to_read, payload_length)
     response = dut.uds.read_data_by_id_22(payload)
 
-    if flag:
+    if response_flag:
         if response.raw[4:6] == '62':
-            # Check if expected DIDs are present in reply
+            # Verify if expected DIDs are present in reply
             result = find_did(dids_to_read, response.raw)
             if not result:
                 return False
 
-            logging.info("Received positive response %s for request ReadDataByIdentifier",
-                        response.raw[4:6])
+            logging.info("Received positive response %s for request ReadDataByIdentifier as expected",
+                            response.raw[4:6])
             return True
 
         logging.error("Test Failed: Expected positive response, received %s", response.raw)
         return False
 
-    if response.raw[2:4] == '7F' and response.raw[6:8] == '13':
+    elif response.raw[2:4] == '7F' and response.raw[6:8] == '13':
         logging.info("Received NRC %s for request ReadDataByIdentifier as expected",
                       response.raw[6:8])
         return True
@@ -139,38 +133,20 @@ def request_read_data_by_id(dut, dids_to_read, pl_max_length, flag=False):
     return False
 
 
-def send_multiframe_cts(dut):
+def verify_fc_overflow(dut, dids_to_read, payload_length):
     """
-    Send a request with MultiFrame for FC.CTS state
+    Verify flow control overflow with payload of size 1025 bytes
     Args:
-        dut (class obj): Dut instance
+        dut (Dut): Dut instance
+        dids_to_read (list): Non programming DIDs
+        payload_length (int): Payload maximum length
     Return:
-        response (bool): Positive response on receiving FlowControl(FC) frame
+        response (bool): True when FlowControl(FC) overflow frame received
     """
-    # Verify FC parameters
-    logging.info("Received FC Frame: %s", SC.can_cf_received[dut["receive"]])
+    payload = prepare_payload(dids_to_read, payload_length)
 
-    fc_code = '3000' + dut.conf.default_rig_config["FC_Separation_time"]
-    logging.info("CAN MultiFrame: %s", SC.can_frames[dut["receive"]])
-
-    if SUTE.test_message(SC.can_cf_received[dut["receive"]], teststring=fc_code):
-        return True
-
-    logging.error("Test Failed: ECU did not send FlowControl(FC) frame")
-    return False
-
-
-def send_multiframe_overflow(dut, dids_to_read, pl_max_length):
-    """
-    Send a request with payload of size 1025 bytes for FC.OVFLW
-    Args:
-        dut (class obj): Dut instance
-        dids_to_read (list): DIDs
-        pl_max_length (int): Payload maximum length
-    Return:
-        response (bool): Positive response on receiving FlowControl(FC) frame
-    """
-    payload = prepare_payload(dids_to_read, pl_max_length, payload_type='overflow')
+    # Append ReadDataByIdentifier service
+    payload = SC_CARCOM.can_m_send("ReadDataByIdentifier", payload, b'')
 
     try:
         dut.uds.generic_ecu_call(payload)
@@ -191,64 +167,65 @@ def send_multiframe_overflow(dut, dids_to_read, pl_max_length):
     return False
 
 
-def verify_flowcontrol_message(dut, parameters, pl_max_length, flag):
+def verify_flowcontrol_message(dut, parameters, payload_length, response_flag):
     """
-    Verify frame control message for a multiframe request with payload size 1byte, 13 bytes
-    and 300 bytes
+    Verify flow control message for a multiframe request with payload size 12byte, 300 bytes
+    and 1025 bytes
     Args:
-        dut (class obj): Dut instance
-        parameters (dict): non_prog_dids
-        pl_max_length (int): Payload maximum length
-        flag (bool): Check overflow when false
+        dut (Dut): Dut instance
+        parameters (dict): Non programming DIDs
+        payload_length (int): Payload maximum length
+        response_flag (bool): True for positive response
     Return:
         response (bool): True when all the DIDs in the request are included in the reply and
-                         frame control(FC) is sent back
+                         flow control(FC) response is received
     """
     # Request multiple DIDs in one request and verify if DIDs are included in response
-    result_non_prog_dids = request_read_data_by_id(dut, parameters['non_prog_dids'],
-                           pl_max_length, flag)
+    result_non_prog_dids = read_did_and_verify(dut, parameters['non_prog_dids'],
+                           payload_length, response_flag)
     if not result_non_prog_dids:
         return False
 
     # Verify FC parameters
-    verify_fc_parameters = send_multiframe_cts(dut)
-    if not verify_fc_parameters:
+    fc_code = '3000' + dut.conf.default_rig_config["FC_Separation_time"]
+
+    if not SUTE.test_message(SC.can_cf_received[dut["receive"]], teststring=fc_code):
+        logging.error("Test Failed: ECU did not send FlowControl(FC) frame")
         return False
 
     return True
 
 
-def flowcontrol_response(dut, parameters):
+def verify_flowcontrol_response(dut, parameters):
     """
-    Verify that the FlowControl (FC) is sent as required by sending UDS request with 13 bytes,
-    300 bytes and 1026 bytes
+    Verify FlowControl(FC) response with payload 12 bytes, 300 bytes and 1025 bytes
     Args:
-        dut (class obj): Dut instance
-        parameters (dict): non_prog_dids
+        dut (Dut): Dut instance
+        parameters (dict): Non programming DIDs and payload lengths.
     Return:
-        response (bool): True when FlowControl (FC) is sent as required
+        response (bool): True when FlowControl(FC) response verified
     """
-    result = verify_flowcontrol_message(dut, parameters, pl_max_length=0, flag=True)
-    if not result:
+    result_positive_response = verify_flowcontrol_message(dut, parameters, payload_length=0, response_flag=True)
+    if not result_positive_response:
         logging.error("Test Failed: Some or all the DIDs in the request are not included in the"
-                      " reply and frame control(FC) is not sent back")
+                      " reply and frame control(FC) is not received")
         return False
 
-    result = result and verify_flowcontrol_message(dut, parameters,
-             pl_max_length=parameters['pl_size_for_positive_response'], flag=True)
-    if not result:
+    result_positive_response = result_positive_response and verify_flowcontrol_message(dut, parameters,
+             payload_length=parameters['pl_size_positive_response'], response_flag=True)
+    if not result_positive_response:
         logging.error("Test Failed: Some or all the DIDs in the request are not included in the"
-                      " reply and frame control(FC) is not sent back")
+                      " reply and frame control(FC) is not received")
         return False
 
-    result = result and verify_flowcontrol_message(dut, parameters,
-             pl_max_length=parameters['pl_size_for_negative_response'], flag=False)
-    if not result:
-        logging.error("Test Failed: Expected NRC-13 and frame control(FC) is not sent back")
+    result_negative_response = result_positive_response and verify_flowcontrol_message(dut, parameters,
+             payload_length=parameters['pl_size_negative_response'], response_flag=False)
+    if not result_negative_response:
+        logging.error("Test Failed: Expected NRC-13 and frame control(FC) is not received")
         return False
 
-    result_non_prog_dids = result and send_multiframe_overflow(dut, parameters['non_prog_dids'],
-                           pl_max_length=parameters['pl_size_for_overflow'])
+    result_non_prog_dids = result_negative_response and verify_fc_overflow(dut, parameters['non_prog_dids'],
+                           payload_length=parameters['pl_size_overflow'])
     if not result_non_prog_dids:
         return False
 
@@ -257,13 +234,13 @@ def flowcontrol_response(dut, parameters):
 
 def step_1(dut: Dut, parameters):
     """
-    action: Verify that the FlowControl (FC) is sent as required by sending UDS request with 13
-            bytes, 300 bytes and 1026 bytes for default session
-    expected_result: True when FlowControl (FC) is sent as required in default session
+    action: Verify FlowControl(FC) in default session with payload 12 bytes, 300 bytes and 1025
+            bytes
+    expected_result: True when FlowControl(FC) response is successfully verified in default session
     """
-    result = flowcontrol_response(dut, parameters)
+    result = verify_flowcontrol_response(dut, parameters)
     if not result:
-        logging.error("Test Failed: FlowControl (FC) is not sent as required in default session")
+        logging.error("Test Failed: FlowControl(FC) response is not received as expected in default session")
         return False
     return True
 
@@ -285,21 +262,21 @@ def step_2(dut: Dut):
 
 def step_3(dut: Dut, parameters):
     """
-    action: Verify that the FlowControl (FC) is sent as required by sending UDS request with 13
-            bytes, 300 bytes and 1026 bytes for extended session
-    expected_result: True when FlowControl (FC) is sent as required in extended session
+    action: Verify FlowControl(FC) in extended session with payload 12 bytes, 300 bytes and 1025
+            bytes
+    expected_result: True when FlowControl(FC) response is successfully verified in extended session
     """
-    result = flowcontrol_response(dut, parameters)
+    result = verify_flowcontrol_response(dut, parameters)
     if not result:
-        logging.error("Test Failed: FlowControl (FC) is not sent as required in extended session")
+        logging.error("Test Failed:FlowControl(FC) response is not received as expected in extended session")
         return False
     return True
 
 
 def run():
     """
-    Verify that the FlowControl (FC) is sent as required in both default and extended session
-    (non-programming session) by sending UDS request with 13 bytes, 300 bytes and 1026 bytes.
+    Verify FlowControl(FC) response in both default and extended session(non-programming session)
+    by sending UDS request with 12 bytes, 300 bytes and 1025 bytes.
     """
     dut = Dut()
 
@@ -308,9 +285,9 @@ def run():
     result_step = False
 
     parameters_dict = {'non_prog_dids':'',
-                       'pl_size_for_positive_response': 0,
-                       'pl_size_for_negative_response': 0,
-                       'pl_size_for_overflow': 0,}
+                       'pl_size_positive_response': 0,
+                       'pl_size_negative_response': 0,
+                       'pl_size_overflow': 0,}
     try:
         dut.precondition(timeout=70)
         parameters = SIO.parameter_adopt_teststep(parameters_dict)
@@ -318,19 +295,13 @@ def run():
         if not all(list(parameters.values())):
             raise DutTestError("yml parameters not found")
 
-        result_step = dut.step(step_1, parameters, purpose='Verify that the FlowControl (FC) is'
-                                                           ' sent as required by sending UDS'
-                                                           ' request with 13 bytes, 300 bytes'
-                                                           ' and 1026 bytes for default Session')
+        result_step = dut.step(step_1, parameters, purpose='Verify FlowControl(FC) response in'
+                                                           ' default Session')
         if result_step:
-            result_step = dut.step(step_2, purpose='Set and verify that the ECU is in extended'
-                                                   ' session')
+            result_step = dut.step(step_2, purpose='Verify ECU is in extended session')
         if result_step:
-            result_step = dut.step(step_3, parameters, purpose='Verify that the FlowControl (FC)'
-                                                               ' is sent as required by sending'
-                                                               ' UDS request with 13 bytes, 300'
-                                                               ' bytes and 1026 bytes for extended'
-                                                               ' session')
+            result_step = dut.step(step_3, parameters, purpose='Verify FlowControl(FC) response'
+                                                               ' in extended session')
         result = result_step
     except DutTestError as error:
         logging.error("Test failed: %s", error)
