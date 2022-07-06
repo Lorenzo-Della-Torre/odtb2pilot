b7fed459bebe51b3bfa406ee68e9109b5e907235
ABHALE <ABHALE@volvocar.com>
2022-07-06 11:30:49 +0000

    Adding Req-74109 Allowed diagnostic services

--- test_folder/automated/e_74109_MAIN_1_allowed_diag_services.py
+++ test_folder/automated/e_74109_MAIN_1_allowed_diag_services.py
@@ -4,7 +4,7 @@
 
 
 
-Copyright © 2021 Volvo Car Corporation. All rights reserved.
+Copyright © 2022 Volvo Car Corporation. All rights reserved.
 
 
 
@@ -19,165 +19,203 @@ Any unauthorized copying or distribution of content from this file is prohibited
 /*********************************************************************************/
 
 reqprod: 74109
-version: 2
+version: 1
 title: Allowed diagnostic services
 purpose: >
-    Define which diagnostic services to allow. Ensure all suppliers uses the
-    same diagnostic services as well as preventing supplier from using not
-    not allowed diagnostic services.
+    Define which diagnostic services to allow. Ensures all suppliers uses the same diagnostic
+    services as well as preventing supplier from using not allowed diagnostic services.
 
 description: >
-    The ECU shall only support the diagnostic services specified in the
-    requirement sections from Volvo Car Corporation.
+    The ECU shall only support the diagnostic services specified in the requirement sections from
+    Volvo Car Corporation.
 
-details:
-    Read which serives are defined in the sddb file.
-    If the service is not defined, send the request to the ECU and
-    expect a negative response.
-
-    The time for last frame sent to first frame received is measured
-    for each undefined service and compared against P2Server_max.
-
-    According to REQPROD 74140 the ECU shall use P2Server_max as P4Server_max
-    as the timing parameterfor a negative response for diagnostic
-    services not supported by the ECU.
+details: >
+    Verify negetive response for services which are not present in services in sddb, in default
+    session, PBL and SBL
 """
-import sys
-import logging
-import time
-import build.services as services
 
+import logging
+import hilding.flash as swdl
 from hilding.dut import Dut
 from hilding.dut import DutTestError
+from supportfunctions.support_file_io import SupportFileIO
 
-# Test environment jitter estimated at 30 milliseconds
-TEST_ENV_JITTER = 30
+SIO = SupportFileIO()
 
-def step_1(dut, application, p2server_max):
+
+def verify_response_time(dut, p2server_max, jitter_testenv):
     """
-    action:
-        Loop through 0 - FF and check if the service is defined
-        in the .sddb file.
-        If the service is defined, the service is not called by the script.
-
-        However if the service is not defined the script sends a request
-        to the ECU and expects a negative response.
-
-        A check of p2server_max is also done for each undefined service.
-
-    expected_result:
-        Negative response within the time limit of p2server_max.
+    Verify response time is within the time limit
+    Args:
+        dut (Dut): Dut instance
+        p2server_max (float): P2server_max time
+        jitter_testenv (float): Test environment jitter time
+    Returns:
+        (bool): True when response recieved within the defined limit
     """
-    __allowed_diagnostic_services(dut, application, p2server_max)
+    t_elapsed = dut.uds.milliseconds_since_request()
+    t_allowed = (p2server_max+jitter_testenv)*1000
 
-def step_3(dut, pbl, p2server_max):
+    if t_elapsed <= t_allowed:
+        return True
+    logging.error("Response time %s is greater than allowed time %s ", round(t_allowed, 2),
+                   round(t_elapsed, 2))
+    return False
+
+
+def verify_diagnostic_services(dut, sddb_services, p2server_max, jitter_testenv):
     """
-    action:
-        Loop through 0 - FF and check if the service is defined
-        in the .sddb file.
-        If the service is defined, the service is not called by the script.
-
-        However if the service is not defined the script sends a request
-        to the ECU and expects a negative response.
-
-        A check of p2server_max is also done for each undefined service.
-
-    expected_result:
-        Negative response within the time limit of p2server_max.
+    Verify negetive response for services which are not present in services in sddb, in default
+    session/pbl/sbl
+    Args:
+        dut (Dut): Dut instance
+        sddb_services (dict): Sddb services
+        p2server_max (float): P2server_max time
+        jitter_testenv (float): Test environment jitter time
+    Returns:
+        (bool): True when negetive response for services not present in sddb
     """
-    __allowed_diagnostic_services(dut, pbl, p2server_max)
-
-def step_5(dut, sbl, p2server_max):
-    """
-    action:
-        Loop through 0 - FF and check if the service is defined
-        in the .sddb file.
-        If the service is defined, the service is not called by the script.
-
-        However if the service is not defined the script sends a request
-        to the ECU and expects a negative response.
-
-        A check of p2server_max is also done for each undefined service.
-
-    expected_result:
-        Negative response within the time limit of p2server_max.
-    """
-    __allowed_diagnostic_services(dut, sbl, p2server_max)
-
-def __allowed_diagnostic_services(dut: Dut, sw_level, p2server_max):
-
     payload = b'\x00'
-    for i in range(0xFF + 1):
-        # Convert integer (i) to its hex representation without the '0x' prefix.
-        service_str = f'{i:X}'
+    result_list = []
 
-        if service_str not in sw_level:
-            # If the service is not defined the .sddb file we need to check the response.
-            service_str = i.to_bytes((i.bit_length() + 7) // 8 or 1, 'big')
+    # Valid service byte range is (0-256). And 0x40 added with service id in can_receive(). So
+    # service id hex range(0x00 to 0xC0) will be validated.
+    for service_id in range(0xC0):
+        service = f'{service_id:X}'
+        result = True
 
-            # Append some data bytes (irrelevant what the data is).
-            payload = service_str + b'\x00\x00'
+        if service not in sddb_services:
+            service = service_id.to_bytes((service_id.bit_length() + 7) // 8 or 1, 'big')
+            payload = service + bytes.fromhex('0000')
 
-            # Send undefined service, expect a negative response.
-            received_data = dut.uds.generic_ecu_call(payload)
+            response = dut.uds.generic_ecu_call(payload)
+            response_result = verify_response_time(dut, p2server_max, jitter_testenv)
 
-            if '037F' not in received_data.raw:
-                # Something else than a negative response was received.
-                raise DutTestError(f"Failed: Response for service {service_str} is not negative.")
-
-            if dut.uds.milliseconds_since_request() > p2server_max + TEST_ENV_JITTER:
-                # Took too long to receive response from the server.
-                raise DutTestError(f"Failed: Service {service_str} took too long to respond.")
+            # Verify negetive response and response time for services not in sddb
+            if response.raw[2:4] != '7F' and not response_result:
+                logging.error("Expected negetive response for "
+                               "service %s but recieved %s", service, response.raw)
+                result = False
         else:
-            logging.info("Service %s is defined in sddb, pass.", service_str)
+            logging.info("Service %s is defined in sddb", service)
+
+        result_list.append(result)
+
+    if all(result_list) and len(result_list) > 0:
+        return True
+    return False
+
+
+def step_1(dut, services_app, parameters):
+    """
+    action: Verify negative response of application services are not preset in sddb in defaul
+            session
+    expected_result: True when recieved negative response for sddb application services are not
+                     present
+    """
+    # Test environment jitter estimated at 30ms.
+    jitter_testenv = parameters["jitter_testenv"]
+    # P2Server_max is 50ms in default
+    p2server_max = parameters["p2server_max_default"]
+
+    result = verify_diagnostic_services(dut, services_app, p2server_max, jitter_testenv)
+    if result:
+        logging.info("Recieved negetive response of application services are not present in sddb "
+                     "as expected")
+        return True
+    logging.error("Test Failed: Unexpected response recieved for some of services in default "
+                  "session")
+    return False
+
+
+def step_2(dut, services_pbl, parameters):
+    """
+    action: Verify negative response of pbl services are not preset in sddb
+    expected_result: True when recieved negative response for pbl services are not present in sddb
+    """
+    jitter_testenv = parameters["jitter_testenv"]
+    p2server_max = parameters["p2server_max_programming"]
+
+    # Set ECU in programming session
+    dut.uds.set_mode(2)
+
+    result = verify_diagnostic_services(dut, services_pbl, p2server_max, jitter_testenv)
+    if result:
+        logging.info("Recieved negetive response of pbl services are not present in sddb "
+                     "as expected")
+        return True
+    logging.error("Test Failed: Unexpected response recieved for some services in PBL")
+    return False
+
+
+def step_3(dut, services_sbl, parameters):
+    """
+    action: Verify negative response of sbl services are not preset in sddb
+    expected_result: True when recieved negative response for pbl services are not present in sddb
+    """
+    vbf_result = swdl.load_vbf_files(dut)
+    if not vbf_result:
+        return False
+    # SBL activation
+    sbl_result = swdl.activate_sbl(dut)
+    if not sbl_result:
+        logging.error("Test Failed: SBL activation failed")
+        return False
+
+    jitter_testenv = parameters["jitter_testenv"]
+    p2server_max = parameters["p2server_max_programming"]
+
+    result = verify_diagnostic_services(dut, services_sbl, p2server_max, jitter_testenv)
+    if result:
+        logging.info("Recieved negetive response of sbl services are not present in sddb "
+                     "as expected")
+        return True
+    logging.error("Test Failed: Unexpected response recieved for some services in SBL")
+    return False
+
 
 def run():
     """
-    Run - Call other functions from here
-
-    Note: 'manage.py sddb' must be run before this script.
+    Verify negetive response for services which are not present in services in sddb, in default
+    session, PBL and SBL
     """
-    logging.basicConfig(format=' %(message)s', stream=sys.stdout, level=logging.INFO)
-
     dut = Dut()
 
-    # P2server_max is 50ms in application.
-    p2server_max = 50
+    services = dut.conf.rig.sddb_services
 
     start_time = dut.start()
     result = False
+    result_step = False
+
+    parameters_dict = {"p2server_max_programming": 0.0,
+                       "p2server_max_default": 0.0,
+                       "jitter_testenv": 0.0}
+
     try:
-        # Communication with ECU lasts 1000 seconds.
         dut.precondition(timeout=1000)
 
-        # Start with application since default session is active.
-        dut.step(step_1, services.app, p2server_max, purpose='Checking services in application.')
+        parameters = SIO.parameter_adopt_teststep(parameters_dict)
 
-        # Step down to programming session.
-        logging.info("Changing mode to mode 2 (primary bootloader)")
-        dut.step(dut.uds.set_mode, 2, False)
+        if not all(list(parameters.values())):
+            raise DutTestError("yml parameters not found")
 
-        # P2server_max is 25ms in programming session (PBL and SBL)
-        p2server_max = 25
+        result_step = dut.step(step_1, services["app"], parameters, purpose="Verify negative "
+                               "response of application services are not preset in sddb in default"
+                               "session")
+        if result_step:
+            result_step = dut.step(step_2, services["pbl"], parameters, purpose="Verify negative "
+                                   "response of pbl services are not preset in sddb")
+        if result_step:
+            result_step = dut.step(step_3, services["sbl"], parameters, purpose="Verify negative "
+                                   "response of sbl services are not preset in sddb")
 
-        # Go through the primary bootloader services.
-        dut.step(step_3, services.pbl, p2server_max, purpose='Checking services in PBL.')
-
-        # Activate secondary bootloader
-        logging.info("Activating secondary bootloader.")
-        dut.step(dut.uds.enter_sbl)
-
-        # Go through the secondary bootloader services.
-        dut.step(step_5, services.sbl, p2server_max, purpose='Checking services in SBL.')
-
-        # Wait for the ECU exit programming session.
-        time.sleep(5)
-        result = True
+        result = result_step
 
     except DutTestError as error:
         logging.error("Test failed: %s", error)
     finally:
         dut.postcondition(start_time, result)
+
 
 if __name__ == '__main__':
     run()
