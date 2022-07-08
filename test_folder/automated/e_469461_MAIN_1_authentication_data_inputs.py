ff4e33434cbdfa0a6630236ca60708613cbe345d
sghosh5 <sghosh5@volvocars.com>
2022-07-08 09:56:40 +0000

    Rewrite REQ:469461- authentication data input

--- test_folder/automated/e_469461_MAIN_1_authentication_data_inputs.py
+++ test_folder/automated/e_469461_MAIN_1_authentication_data_inputs.py
@@ -21,122 +21,134 @@ Any unauthorized copying or distribution of content from this file is prohibited
 reqprod: 469461
 version: 1
 title: Authentication data Inputs
-purpose:
+purpose: >
     Define the input data to be part of the actual Authentication Data calculation.
 
-description:
-    The Authentication Data shall be calculated using
-    all stored SecurityEvents and the SecurityEventHeader
-    (Authentication Data itself excluded) as input data.
-    A single Authentication Data is calculated per event type
-    The Authentication Data shall always be appended next to the last
-    event record for an event type, big endian.
+description: >
+    The Authentication Data shall be calculated using all stored SecurityEvents and
+    the SecurityEventHeader (Authentication Data itself excluded) as input data.
+    A single Authentication Data is calculated per event type The Authentication Data
+    shall always be appended next to the last event record for an event type, big endian.
 
-details:
-    Read with one of the Security log event DID D046 and
-    Store the response Total number of successful events and Authentication Data
-    Create an Event; Security Access ProgrammingSession
-    Send ECU back to Default session to check DID D046
-    Check the Event has been logged in D046 DID and
-    Compare with previously saved response
-    Total number of successful events and Authentication Data
-    Total number of successful events will be incremented by 1
-    Authentication Data will be different as the previously saved Data
+details: >
+    Verify authentication data is generated per event type.
+    Steps-
+        1. Read security log DID and get authentication data
+        2. Increment successful event
+        3. Read security log again and verify authentication data is generated for latest event
 """
+
 import logging
-import time
-import inspect
-from supportfunctions.support_service27 import SupportService27
-from supportfunctions.support_file_io import SupportFileIO
-from supportfunctions.support_sec_acc import SecAccessParam
 from hilding.dut import Dut
 from hilding.dut import DutTestError
+from supportfunctions.support_service27 import SupportService27
 
-SIO = SupportFileIO()
 SE27 = SupportService27()
+
+
+def get_event_count_auth_data(response):
+    """
+    Get total number of successful event count and authentication data from security log
+    Args:
+        response(dict): Security log response
+    Returns:
+        event_auth_data(dict): Successful event and authentication data
+    """
+    event_auth_data = {'successful_event': 0,
+                       'auth_data': ''}
+    for response_item in response.data['details']['response_items']:
+        if response_item['name'] == "Total number of successful events":
+            event_auth_data['successful_event'] = int(response_item['sub_payload'], 16)
+        if response_item['name'] == "Authentication Data":
+            event_auth_data['auth_data'] = response_item['sub_payload']
+
+    if event_auth_data['auth_data'] != '':
+        return event_auth_data
+
+    return None
 
 
 def step_1(dut: Dut):
     """
     action: Read Security log event DID D046
-    expected_result: Positive responce with the event data
+    expected_result: True with the event data
     """
-    res = dut.uds.read_data_by_id_22(b'\xd0\x46')
-    for response_item in res.data['details']['response_items']:
-        if response_item['name'] == "Total number of successful events":
-            total_succ_events = response_item['sub_payload']
-        if response_item['name'] == "Authentication Data":
-            cmac = response_item['sub_payload']
-    if res.raw[4:6] == '62':
-        return True, total_succ_events, cmac
-    return False, None, None
+    response = dut.uds.read_data_by_id_22(bytes.fromhex('D046'))
+    if response.raw[4:6] != '62':
+        logging.error("Test Failed: Invalid DID response %s", response.raw[4:6])
+        return False, None
+
+    event_auth_data = get_event_count_auth_data(response)
+    if event_auth_data is None:
+        logging.error("Test Failed: Event or authentication data is empty")
+        return False, None
+
+    return True, event_auth_data
 
 
 def step_2(dut: Dut):
     """
-    action: Set ECU to Programmin Session
-    expected_result: Positive response
+    action: Set ECU to programming session and Security Access to ECU
+    expected_result: True when security access successful
     """
     dut.uds.set_mode(2)
-    res = dut.uds.active_diag_session_f186()
-    if res.data['sid'] == '62' and res.data['details']['mode'] == 2:
+
+    result = SE27.activate_security_access_fixedkey(dut, sa_keys=dut.conf.default_rig_config)
+    if result:
+        logging.info("Security access successful")
         return True
+    logging.error("Test Failed: Security access not successful")
     return False
 
 
-def step_3(dut: Dut):
+def step_3(dut: Dut, event_auth_data):
     """
-    action: Security Access to ECU
-    expected_result: Positive response
+    action: Set ECU to default session and read security log event DID D046
+    expected_result: True when authentication data are unique per event type
+                     and Total number of successful events is incremented by 1
     """
-    sa_keys: SecAccessParam = {
-        "SecAcc_Gen": 'Gen2',
-        "fixed_key": '0102030405',
-        "auth_key": 'FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF',
-        "proof_key": 'FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF'
-    }
-    SIO.extract_parameter_yml(str(inspect.stack()[0][3]), sa_keys)
-    result = SE27.activate_security_access_fixedkey(
-        dut, sa_keys, step_no=3, purpose='Security Access')
-    return result
+    dut.uds.set_mode(1)
 
+    response = dut.uds.read_data_by_id_22(bytes.fromhex('D046'))
+    latest_event_auth_data = get_event_count_auth_data(response)
 
-def step_4(dut: Dut, total_succ_events, cmac):
-    """
-    action: Set ECU to Default session and
-            Read Security log event DID D046
-    expected_result: Authentication Datas are different
-                     Total number of successful events is incremented by 1
-    """
-    dut.uds.set_mode()
-    time.sleep(2)
-    res = dut.uds.read_data_by_id_22(b'\xd0\x46')
-    for response_item in res.data['details']['response_items']:
-        if response_item['name'] == "Total number of successful events":
-            total_succ_events_latest = response_item['sub_payload']
-        if response_item['name'] == "Authentication Data":
-            cmac_latest = response_item['sub_payload']
+    # Compare authentication data is generated per event type
+    auth_result = latest_event_auth_data['auth_data'] != event_auth_data['auth_data']
 
-    if cmac != cmac_latest and int(total_succ_events_latest, 16) == int(total_succ_events, 16) + 1:
+    # Verify total successful event is increased by 1 
+    event_result = latest_event_auth_data['successful_event'] == \
+            event_auth_data['successful_event'] + 1
+
+    if auth_result and event_result:
+        logging.info("Authentication data is unique per event type")
         return True
+
+    logging.error("Test Failed: Expected unique authentication data per event type "
+                  "received latest: %s equal to previous: %s",
+                  latest_event_auth_data['auth_data'], event_auth_data['auth_data'])
     return False
 
 
 def run():
-    """ Supporting functional requests """
+    """ Verify authentication data is generated per event type """
     dut = Dut()
     start_time = dut.start()
     result = False
+    result_step = False
 
     try:
-        dut.precondition()
-        result1, total_succ_events, cmac = dut.step(
-            step_1, purpose='Read DID D046')
-        result2 = dut.step(step_2, purpose='ECU into programming session')
-        result3 = dut.step(step_3, purpose='Security Access')
-        result4 = dut.step(step_4, total_succ_events, cmac,
-                           purpose='ECU to Default session and Read and compare DID D046')
-        result = result1 and result2 and result3 and result4
+        dut.precondition(timeout=30)
+
+        result_step, event_auth_data = dut.step(step_1, purpose='Read DID D046')
+
+        if result_step:
+            result_step = dut.step(step_2, purpose='Security Access to ECU')
+
+        if result_step:
+            result_step = dut.step(step_3, event_auth_data, purpose='Verify authentication '
+                                   'data is calculated per event type')
+
+        result = result_step
 
     except DutTestError as error:
         logging.error("Test failed: %s", error)
