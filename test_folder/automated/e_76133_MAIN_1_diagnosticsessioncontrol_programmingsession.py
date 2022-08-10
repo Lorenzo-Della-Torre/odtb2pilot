0cf588549c94bfa8f5e3667c240469bcce9e911c
Wankhede, Aniket <aniket.wankhede@volvocars.com>
2022-08-10 14:20:25 +0200

    Adding REQID-76133, DiagnosticSessionControl(10) programmingSession (02, 82)

--- test_folder/automated/e_76133_MAIN_1_diagnosticsessioncontrol_programmingsession.py
+++ test_folder/automated/e_76133_MAIN_1_diagnosticsessioncontrol_programmingsession.py
@@ -4,7 +4,7 @@
 
 
 
-Copyright © 2021 Volvo Car Corporation. All rights reserved.
+Copyright © 2022 Volvo Car Corporation. All rights reserved.
 
 
 
@@ -18,214 +18,171 @@ Any unauthorized copying or distribution of content from this file is prohibited
 
 /*********************************************************************************/
 
-# Testscript Hilding MEPII
-# project:  BECM basetech MEPII
-# author:   LDELLATO (Lorenzo Della Torre)
-# date:     2020-07-03
-# version:  1.1
-# reqprod:  76133
+reqprod: 76133
+version: 1
+title: DiagnosticSessionControl(10) programmingSession (02, 82)
+purpose: >
+    It shall be possible to re-program any ECU on the public network.
 
-# #inspired by https://grpc.io/docs/tutorials/basic/python.html
-# Copyright 2015 gRPC authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+description: >
+    The ECU shall support the service diagnosticSessionControl - programmingSession in:
+    •   defaultSession
+    •   extendedDiagnosticSession
+    •   programmingSession, both primary and secondary bootloader.
 
-The Python implementation of the gRPC route guide client.
+details: >
+    Verify the response of request programming session with and without reply after setting ECU
+    to default, programming and extended diagnostic session.
 """
 
-import time
-from datetime import datetime
-import sys
 import logging
-import inspect
-
-import odtb_conf
-from supportfunctions.support_can import SupportCAN, CanParam, CanTestExtra
-from supportfunctions.support_test_odtb2 import SupportTestODTB2
+from hilding.dut import Dut
+from hilding.dut import DutTestError
+from hilding.uds import UdsEmptyResponse
 from supportfunctions.support_carcom import SupportCARCOM
-from supportfunctions.support_file_io import SupportFileIO
-
-from supportfunctions.support_precondition import SupportPrecondition
-from supportfunctions.support_postcondition import SupportPostcondition
-from supportfunctions.support_service10 import SupportService10
 from supportfunctions.support_service22 import SupportService22
+from supportfunctions.support_can import SupportCAN
 
-SIO = SupportFileIO
-SC = SupportCAN()
-SUTE = SupportTestODTB2()
 SC_CARCOM = SupportCARCOM()
-PREC = SupportPrecondition()
-POST = SupportPostcondition()
-SE10 = SupportService10()
 SE22 = SupportService22()
+SC = SupportCAN()
 
-def step_3(can_p):
-    """
-    Teststep 3: Request session change to Mode2 without reply
-    """
-    etp: CanTestExtra = {
-        "step_no": 3,
-        "purpose" : "Request session change to Mode2 without reply",
-        "timeout" : 1,
-        "min_no_messages" : -1,
-        "max_no_messages" : -1
-    }
-    SIO.extract_parameter_yml(str(inspect.stack()[0][3]), etp)
-    result = SE10.diagnostic_session_control(can_p, etp, b'\x82')\
-         and not SC.can_messages[can_p["receive"]]
-    return result
 
-def step_10(can_p):
+def prog_session_with_no_reply(dut: Dut):
     """
-    Teststep 10: Request session change to Mode2 without reply
+    Send the payload with no reply in programming session
+    Args:
+        dut (Dut): Dut instance
+    Return:
+        response (bool): True on receiving no response
     """
-    etp: CanTestExtra = {
-        "step_no": 10,
-        "purpose" : "Request session change to Mode2 without reply",
-        "timeout" : 1,
-        "min_no_messages" : -1,
-        "max_no_messages" : -1
-    }
-    SIO.extract_parameter_yml(str(inspect.stack()[0][3]), etp)
-    result = SE10.diagnostic_session_control(can_p, etp, b'\x82')\
-         and not SC.can_messages[can_p["receive"]]
-    return result
+    payload = SC_CARCOM.can_m_send("DiagnosticSessionControl", b'\x82', b'')
+    try:
+        dut.uds.generic_ecu_call(payload)
+    except UdsEmptyResponse:
+        pass
 
-def step_13(can_p):
+    if not SC.can_messages[dut["receive"]]:
+        logging.info("Empty response received from ECU")
+        return True
+
+    logging.error("Test Failed: Expected empty response, received %s",
+                  SC.can_messages[dut["receive"]])
+    return False
+
+
+def step_1(dut: Dut):
     """
-    Teststep 13: Request session change to Mode2 without reply
+    action: Set to default session and request programming session with no reply
+    expected_result: True when ECU is in programming session
     """
-    etp: CanTestExtra = {
-        "step_no": 13,
-        "purpose" : "Request session change to Mode2 without reply",
-        "timeout" : 1,
-        "min_no_messages" : -1,
-        "max_no_messages" : -1
-    }
-    SIO.extract_parameter_yml(str(inspect.stack()[0][3]), etp)
-    result = SE10.diagnostic_session_control(can_p, etp, b'\x82')\
-         and not SC.can_messages[can_p["receive"]]
-    return result
+    # Set to programming session
+    dut.uds.set_mode(2)
+
+    # Set to default session
+    dut.uds.set_mode(1)
+
+    # Set to programming session with no reply
+    response = prog_session_with_no_reply(dut)
+    if not response:
+        return False
+
+    # Verify current session
+    result = SE22.read_did_f186(dut, dsession=b'\x02')
+    if result:
+        logging.info("ECU is in programming session as expected")
+        return True
+
+    logging.error("Test Failed: ECU is not in programming session")
+    return False
+
+
+def step_2(dut: Dut):
+    """
+    action: Set to extended session and request programming session with and without reply
+    expected_result: True when ECU is in programming session
+    """
+    # Set to default session
+    dut.uds.set_mode(1)
+
+    # Set to extended session
+    dut.uds.set_mode(3)
+
+    # Set to programming session
+    dut.uds.set_mode(2)
+
+    # Set to default session
+    dut.uds.set_mode(1)
+
+    # Set to extended session
+    dut.uds.set_mode(3)
+
+    # Set to programming session with no reply
+    response = prog_session_with_no_reply(dut)
+    if not response:
+        return False
+
+    # Verify current ECU session
+    result = SE22.read_did_f186(dut, dsession=b'\x02')
+    if result:
+        logging.info("ECU is in programming session as expected")
+        return True
+
+    logging.error("Test Failed: ECU is not in programming session")
+    return False
+
+
+def step_3(dut: Dut):
+    """
+    action: Set to programming session and request programming session with no reply
+    expected_result: True when ECU is in programming session
+    """
+    # Set to programming session
+    dut.uds.set_mode(2)
+
+    # Set to programming session with no reply
+    response = prog_session_with_no_reply(dut)
+    if not response:
+        return False
+
+    # Verify current ECU session
+    result = SE22.read_did_f186(dut, dsession=b'\x02')
+    if result:
+        logging.info("ECU is in programming session as expected")
+        return True
+
+    logging.error("Test Failed: ECU is not in programming session")
+    return False
+
 
 def run():
     """
-    Run - Call other functions from here
+    Verify the response of request programming session with and without reply after setting ECU
+    to default, programming and extended diagnostic session
     """
-    logging.basicConfig(format=' %(message)s', stream=sys.stdout, level=logging.INFO)
+    dut = Dut()
 
-    # start logging
-    # to be implemented
+    start_time = dut.start()
+    result = False
+    result_step = False
 
-    # where to connect to signal_broker
-    can_p: CanParam = {
-        "netstub" : SC.connect_to_signalbroker(odtb_conf.ODTB2_DUT, odtb_conf.ODTB2_PORT),
-        "send" : "Vcu1ToBecmFront1DiagReqFrame",
-        "receive" : "BecmToVcu1Front1DiagResFrame",
-        "namespace" : SC.nspace_lookup("Front1CANCfg0")
-    }
-    SIO.extract_parameter_yml(str(inspect.stack()[0][3]), can_p)
-    logging.info("Testcase start: %s", datetime.now())
-    starttime = time.time()
-    logging.info("Time: %s \n", time.time())
+    try:
+        dut.precondition(timeout=60)
 
-    ############################################
-    # precondition
-    ############################################
-    timeout = 30
-    result = PREC.precondition(can_p, timeout)
+        result_step = dut.step(step_1, purpose="Set to default session and request programming "
+                                               "session with no reply")
+        if result_step:
+            result_step = dut.step(step_2, purpose="Set to extended session and request "
+                                                   "programming session with and without reply")
+        if result_step:
+            result_step = dut.step(step_3, purpose="Set to programming session and request "
+                                                   "programming session with no reply")
+        result = result_step
+    except DutTestError as error:
+        logging.error("Test failed: %s", error)
+    finally:
+        dut.postcondition(start_time, result)
 
-    if result:
-    ############################################
-    # teststeps
-    ############################################
-    # step1:
-    # action: # Change to programming session
-    # result: BECM reports mode
-        SE10.diagnostic_session_control_mode2(can_p, 1)
-        result = result and SE10.diagnostic_session_control_mode2(can_p, 1)
-
-    # step2:
-    # action: # Change to default session
-    # result: BECM reports mode
-        result = result and SE10.diagnostic_session_control_mode1(can_p, 2)
-        time.sleep(1)
-    # step3:
-    # action: # Change to programming session without reply
-    # result: BECM reports mode
-        result = result and step_3(can_p)
-
-    # step4:
-    # action: verify current session
-    # result: BECM reports programming session
-        result = result and SE22.read_did_f186(can_p, dsession=b'\x02')#, 4)
-
-    # step5:
-    # action: # Change to default session
-    # result: BECM reports mode
-        result = result and SE10.diagnostic_session_control_mode1(can_p, 5)
-        time.sleep(1)
-    # step6:
-    # action: # Change to Extended session
-    # result: BECM reports mode
-        result = result and SE10.diagnostic_session_control_mode3(can_p, 6)
-
-    # step7:
-    # action: # Change to Programming session from extended
-    # result: BECM reports mode
-        SE10.diagnostic_session_control_mode2(can_p, 7)
-        result = result and SE10.diagnostic_session_control_mode2(can_p, 7)
-
-    # step8:
-    # action: # Change to default session
-    # result: BECM reports mode
-        result = result and SE10.diagnostic_session_control_mode1(can_p, 8)
-        time.sleep(1)
-
-    # step9:
-    # action: # Change to Extended session
-    # result: BECM reports mode
-        result = result and SE10.diagnostic_session_control_mode3(can_p, 9)
-
-    # step10:
-    # action: # Change to Programming session from extended without reply
-    # result: BECM reports mode
-        result = result and step_10(can_p)
-
-    # step11:
-    # action: verify current session
-    # result: BECM reports programming session
-        result = result and SE22.read_did_f186(can_p, dsession=b'\x02')#, 11)
-
-    # step12:
-    # action: # Change to Programming session from programming
-    # result: BECM reports mode
-        result = result and SE10.diagnostic_session_control_mode2(can_p, 12)
-
-    # step13:
-    # action: # Change to programming session from programming without reply
-    # result: BECM reports mode
-        result = result and step_13(can_p)
-
-    # step14:
-    # action: verify current session
-    # result: BECM reports programming session
-        result = result and SE22.read_did_f186(can_p, dsession=b'\x02')#, 14)
-
-    ############################################
-    # postCondition
-    ############################################
-
-    POST.postcondition(can_p, starttime, result)
 
 if __name__ == '__main__':
     run()
