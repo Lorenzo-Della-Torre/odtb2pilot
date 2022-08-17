1e955246173648070ec855416dcb457949835f38
Khairkar, Khushboo <khushboo.khairkar@volvocars.com>
2022-08-17 11:44:04 +0200

    Adding req- 68264 DTCs defined by GMRDB(Bug fix script)

--- test_folder/automated/e_68264_MAIN_1_dtc_defined_by_gmrdb.py
+++ test_folder/automated/e_68264_MAIN_1_dtc_defined_by_gmrdb.py
@@ -4,7 +4,7 @@
 
 
 
-Copyright © 2021 Volvo Car Corporation. All rights reserved.
+Copyright © 2022 Volvo Car Corporation. All rights reserved.
 
 
 
@@ -18,181 +18,105 @@ Any unauthorized copying or distribution of content from this file is prohibited
 
 /*********************************************************************************/
 
-Testscript Hilding MEPII
-project:  BECM basetech MEPII
-author:   DHJELM (Daniel Hjelm)
-date:     2020-11-23
-version:  2
-reqprod:  68264
+reqprod: 68264
+version: 1
+title: DTCs defined by GMRDB
+purpose: >
+	All DTC information shall be supported by VCC tools and must therefore be defined in GMRDB.
 
-title:
+description: >
+    Rationale-
+    Global Master Reference Data Base (GMRDB) is a part of the central diagnostic database that
+    is used by Volvo Car Corporation in order to document the implementation of diagnostics in the
+    ECUs. GMRDB is a library containing predefined DTCs, DIDs and Control Routines. The definition
+    of DTCs (both identifier and description) that are supposed to be used by Volvo tools must
+    have its origin in GMRDB. GMRDB holds only the 2-byte base DTC.
 
-    DTCs defined by GMRDB
+    Requirement-
+    DTCs supported by an ECU shall be implemented in accordance to the definition in Global Master
+    Reference Database. Development specific implementer specified DTCs are excluded from this
+    requirement.
 
-purpose:
-
-    All DTC information shall be supported by VCC tools and must therefore be
-    defined in GMRDB.
-
-description:
-
-    Rationale:
-
-    Global Master Reference Data Base (GMRDB) is a part of the central
-    diagnostic database that is used by Volvo Car Corporation in order to
-    document the implementation of diagnostics in the ECUs. GMRDB is a library
-    containing predefined DTCs, DIDs and Control Routines. The definition of
-    DTCs (both identifier and description) that are supposed to be used by
-    Volvo tools must have its origin in GMRDB. GMRDB holds only the 2-byte base
-    DTC.
-
-    Requirement:
-
-    DTCs supported by an ECU shall be implemented in accordance to the
-    definition in Global Master Reference Database. Development specific
-    implementer specified DTCs are excluded from this requirement.
-
-details:
-
-    We test this requirement by doing negative testing. That is, we attept
-    finding one DTC that does not respond appropriately to a snapshot request.
-    Appropriately mean that it should not return any error to the request and
-    the content of the snapshot should be according the SDDB. Implicitly we
-    assume that the information in SDDB corresponds with GMRDB.
-
+details: >
+    Verify positive response of all DTCs or DTC snapshot DIDs are identical according to SDDB DIDs
 """
-import time
-from datetime import datetime
-from dataclasses import dataclass
-import sys
+
 import logging
-import inspect
-
-import odtb_conf
-
-from build.dtc import sddb_dtcs
-
-from supportfunctions.support_can import SupportCAN, CanParam, CanTestExtra, CanPayload
-from supportfunctions.support_test_odtb2 import SupportTestODTB2
+from hilding.dut import Dut
+from hilding.dut import DutTestError
 from supportfunctions.support_carcom import SupportCARCOM
-from supportfunctions.support_file_io import SupportFileIO
-from supportfunctions.support_precondition import SupportPrecondition
-from supportfunctions.support_postcondition import SupportPostcondition
 
-SIO = SupportFileIO
-SC = SupportCAN()
-SUTE = SupportTestODTB2()
 SC_CARCOM = SupportCARCOM()
-PREC = SupportPrecondition()
-POST = SupportPostcondition()
 
-def dtc_snapshot(dtc_number: bytes, mask: bytes):
-    """ Report DTC Snapshot Record By DTC Number """
-    return b'\x19\x04' + dtc_number + mask
 
-@dataclass
-class Dtc:
-    """ Hybrid/EV Battery Voltage Sense "D" Circuit """
-    battery_voltage_d = b'\x0B\x4A\x00'
+def step_1(dut: Dut):
+    """
+    action: Verify positive response of all DTCs or DTC snapshot DIDs are identical according
+            to SDDB DIDs
+    expected_result: True when received positive response for all DTCs or DTC snapshot DIDs are
+                     identical according to SDDB DIDs
+    """
+    results = []
+    # Test all the DTCs are appropriate according to SDDB
+    sddb_dtcs = dut.conf.rig.sddb_dtcs["sddb_dtcs"]
+    failed_dtc = []
+    for dtc, dtc_data in sddb_dtcs.items():
+        payload = SC_CARCOM.can_m_send("ReadDTCInfoSnapshotRecordByDTCNumber",
+                                        bytearray.fromhex(dtc), bytes.fromhex('FF'))
 
-def step_1(can_p):
-    """ Test that all DTC responds appropriately """
+        dtc_response = dut.uds.generic_ecu_call(payload)
 
-    for dtc_id, dtc in sddb_dtcs.items():
-        dtc_number = bytearray.fromhex(dtc_id)
-        logging.debug('Testing DTC number: %s', dtc_number)
+        # Compare the DTC snapshot positive response
+        if dtc_response.raw[2:4] == '59':
+            logging.info("DTC %s snapshot request returns positive response %s as expected", dtc,
+                         dtc_response.raw[2:4])
+            results.append(True)
 
-        cpay: CanPayload = {
-            "payload": dtc_snapshot(dtc_number, b'\xFF'),
-            "extra": ''
-        }
-        SIO.extract_parameter_yml(str(inspect.stack()[0][3]), cpay)
-        etp: CanTestExtra = {
-            "step_no": 1,
-            "purpose": f"Test that the DTC {dtc_number} snapshot request "
-                        "responds according to SDDB",
-            "timeout": 1,
-            "min_no_messages": -1,
-            "max_no_messages": -1
-        }
-        SIO.extract_parameter_yml(str(inspect.stack()[0][3]), etp)
-
-        result = SUTE.teststep(can_p, cpay, etp)
-        if not result:
-            return False
-
-        message = SC.can_messages[can_p['receive']]
-
-        if message:
-            message = message[0][2]
-            if "7F" in message:
-                logging.error(
-                    "DTC %s snapshot request returns an error (7F)", dtc_number)
-                logging.error("Expected: %s", dtc)
-                return True
-
-            # testing that the snapshots contain dids according to SDDB
-            for snapshot in dtc["snapshot_dids"]:
-                for _, did in snapshot["did_ref"].items():
-                    if not did in message:
-                        logging.error(
-                            "DTC %s snapshot request does not return a "
-                            "snapshot containing the did: %s", dtc_number, did)
-                        return True
-
+            # Compare the DTC snapshot DIDs are according to SDDB
+            if dtc_data['snapshot_dids'] != dtc_response.data['details']['snapshot_dids']:
+                logging.error("DTC snapshot did response are not equal with SDDB DIDs which is not"
+                             " expected for DTC %s", dtc)
+                results.append(False)
+                failed_dtc.append(dtc)
+            else:
+                logging.info("DTC snapshot did response are equal with SDDB DIDs as expected"
+                              " for DTC %s", dtc)
+                results.append(True)
         else:
-            # Currently the ECU stops responding after a while, so in order to
-            # keep the test running we need to restart it to keep procerssing
-            # the DTCs
-            POST.postcondition(can_p, time.time(), True)
-            PREC.precondition(can_p, 30)
+            logging.error("DTC %s snapshot request returns negative response %s",
+                           dtc, dtc_response.raw[2:4])
+            results.append(False)
+            failed_dtc.append(dtc)
 
+    if all(results) and len(results) != 0:
+        logging.info("Successfully verified all the DTCs are identical according to SDDB DIDs")
+        return True
+
+    logging.error("Test Failed: DTC DIDs %s are not identical according to SDDB DIDs",
+                  ", ".join(failed_dtc))
     return False
 
 
 def run():
     """
-    Run - Call other functions from here
+    Verify positive response of all DTCs or DTC snapshot DIDs are identical according to SDDB DIDs
     """
-    logging.basicConfig(format=' %(message)s', stream=sys.stdout, level=logging.INFO)
+    dut = Dut()
 
-    # where to connect to signal_broker
-    can_p: CanParam = {
-        "netstub" : SC.connect_to_signalbroker(odtb_conf.ODTB2_DUT, odtb_conf.ODTB2_PORT),
-        "send" : "Vcu1ToBecmFront1DiagReqFrame",
-        "receive" : "BecmToVcu1Front1DiagResFrame",
-        "namespace" : SC.nspace_lookup("Front1CANCfg0")
-    }
-    SIO.extract_parameter_yml(str(inspect.stack()[0][3]), can_p)
+    start_time = dut.start()
+    result = False
 
-    logging.info("Testcase start: %s", datetime.now())
-    starttime = time.time()
-    logging.info("Time: %s \n", time.time())
+    try:
+        dut.precondition(timeout=500)
 
-    ############################################
-    # precondition
-    ############################################
-    timeout = 30
-    result = PREC.precondition(can_p, timeout)
+        result = dut.step(step_1, purpose='Verify positive response of all DTCs or DTC snapshot'
+                                          ' DIDs are identical according to SDDB DIDs')
 
-    if result:
-    ############################################
-    # teststeps
-    ############################################
+    except DutTestError as error:
+        logging.error("Test failed: %s", error)
+    finally:
+        dut.postcondition(start_time, result)
 
-    # step1:
-
-    # action: Negative testing: request a snapshot for all DTCs defined in SDDB
-    # result: Since we are doing negative testing we should get a True result
-    # if at least one DTC does not respond to a snapshot request or does not
-    # contain the dids accoding to the specification
-        result = result and step_1(can_p)
-
-    ############################################
-    # postCondition
-    ############################################
-
-    POST.postcondition(can_p, starttime, result)
 
 if __name__ == '__main__':
     run()
