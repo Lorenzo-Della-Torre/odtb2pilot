63353e5a9b912b8f75330703ce3ad3d0ffabec80
Khairkar, Khushboo <khushboo.khairkar@volvocars.com>
2022-08-19 14:26:56 +0200

    Adding Req- 76173 ReadMemoryByAddress (23) - addressAndLengthFormatIdentifier (ALFID)

--- test_folder/automated/e_76173_MAIN_0_readmemorybyaddress_alfid.py
+++ test_folder/automated/e_76173_MAIN_0_readmemorybyaddress_alfid.py
@@ -4,7 +4,7 @@
 
 
 
-Copyright © 2021 Volvo Car Corporation. All rights reserved.
+Copyright © 2022 Volvo Car Corporation. All rights reserved.
 
 
 
@@ -18,266 +18,214 @@ Any unauthorized copying or distribution of content from this file is prohibited
 
 /*********************************************************************************/
 
-testscript: Hilding MEPII
-project:    BECM basetech MEPII
-author:     GANDER10 (Gustav Andersson)
-date:       2020-12-09
-version:    1.0
-reqprod:    76173
+reqprod: 76173
+version: 0
+title: ReadMemoryByAddress (23) - addressAndLengthFormatIdentifier (ALFID)
+purpose: >
+	To make it easier for VOLVO CAR CORPORATION tools, the ECU shall support standardized request
 
-title:
-    ReadMemoryByAddress (23) - addressAndLengthFormatIdentifier (ALFID) ; 1
+description: >
+    The ECU shall support the service readMemoryByAddress with the data parameter
+    addressAndLengthFormatIdentifier set to one of the following values:
+    •	0x14
+    •	0x24
 
-purpose:
-    To make it easier for VOLVO CAR CORPORATION tools, the ECU shall support
-    standarized requests.
+    The ECU shall support the data parameter in all sessions where the ECU supports the service
+    readMemoryByAddress
 
-description:
-    The ECU shall support the service ReadMemoryByAddress with the data
-    parameter addressAndLengthFormatIdentifier set to one of the following
-    values:
-        -0x14
-        -0x24
-
-    The ECU shall support the data parameter in all sessions where the ECU
-    supports the service ReadMemoryByAddress which is:
-        - Default session & Extended session
-
-    Programming session is not supported.
-
-details:
-    Each session asks to read 10 bytes of data from memory address 70000000h
-    with both 0x14 and 0x24 set as ALFID.
-    Default and extended sessions expects a positive response.
-    Programming sessions expects a negative response.
+details: >
+    Verify readMemoryByAddress(0x23) with ALFIDs(0x14, 0x24) is supported in default and
+    extended session and not in programming session.
 """
 
-from datetime import datetime
-import sys
-import time
 import logging
-import inspect
-
-import odtb_conf
-
-from supportfunctions.support_can import SupportCAN
-from supportfunctions.support_can import CanParam
-from supportfunctions.support_can import CanTestExtra
-from supportfunctions.support_can import CanPayload
-from supportfunctions.support_test_odtb2 import SupportTestODTB2
-from supportfunctions.support_carcom import SupportCARCOM
+from hilding.dut import Dut
+from hilding.dut import DutTestError
 from supportfunctions.support_file_io import SupportFileIO
-from supportfunctions.support_precondition import SupportPrecondition
-from supportfunctions.support_postcondition import SupportPostcondition
-from supportfunctions.support_service22 import SupportService22
-from supportfunctions.support_service10 import SupportService10
+from supportfunctions.support_carcom import SupportCARCOM
 
-SIO = SupportFileIO
-SC = SupportCAN()
-SUTE = SupportTestODTB2()
+SIO = SupportFileIO()
 SC_CARCOM = SupportCARCOM()
-PREC = SupportPrecondition()
-POST = SupportPostcondition()
-SE10 = SupportService10()
-SE22 = SupportService22()
 
-DEF_SESSION = 'Default session'
-PRG_SESSION = 'Programming session'
-EXT_SESSION = 'Extended session'
 
-def read_memory_by_address_0x14(can_p, session, step_no):
+def request_read_memory_by_address(dut, alfid, message):
     """
-    Step Read Memory By Address: Send ReadMemoryByAddress with
-    ALFID = 0x14 and verify that the ECU replies.
+    Request readMemoryByAddress with ALFIDs 0x14,0x24
+    Args:
+        dut (Dut): An instance of Dut
+        alfid (str): 0x14, 0x24
+        message (str): Message
+    Return:
+        response.raw (str): ECU response
     """
-    if session == PRG_SESSION:
-        purpose = \
-            f"ALFID = 0x14 in {session}, verify a negative response."
-    else:
-        purpose = \
-            f"ALFID = 0x14 in {session}, verify a positive response."
+    payload = SC_CARCOM.can_m_send("ReadMemoryByAddress", bytes.fromhex(message),
+                                    bytes.fromhex(alfid))
+    response = dut.uds.generic_ecu_call(payload)
+    return response.raw
 
-    etp: CanTestExtra = {
-        "step_no": step_no,
-        "purpose" : purpose,
-        "timeout" : 1,
-        "min_no_messages" : -1,
-        "max_no_messages" : -1
-    }
-    SIO.extract_parameter_yml(str(inspect.stack()[0][3]), etp)
 
-    # Read 10 bytes from memory position 0x70000000-0x7000000A using alfid id 0x14.
-    cpay: CanPayload = {
-        "payload": b'\x23\x14\x70\x00\x00\x00\x0A',
-        "extra": ''
-    }
-    SIO.extract_parameter_yml(str(inspect.stack()[0][3]), cpay)
-
-    result = SUTE.teststep(can_p, cpay, etp)
-
-    # Check if session is programming mode.
-    if session == PRG_SESSION:
-        result = evaluate_response(can_p, '037F23')
-    else:
-        result = evaluate_response(can_p, '100B63')
-
-    return result
-
-def read_memory_by_address_0x24(can_p, session, step_no):
+def verify_active_diagnostic_session(dut, ecu_session):
     """
-    Step Read Memory By Address: Send ReadMemoryByAddress with
-    ALFID = 0x24 and verify that the ECU replies.
+    Request to check active diagnostic session
+    Args:
+        dut (Dut): An instance of Dut
+        ecu_session (int): Diagnostic session
+    Return:
+        (bool): True when ECU is in expected session
     """
-    if session == PRG_SESSION:
-        purpose = \
-            f"ALFID = 0x24 in {session}, verify a negative response."
-    else:
-        purpose = \
-            f"ALFID = 0x24 in {session}, verify a positive response."
+    result = dut.uds.active_diag_session_f186()
 
-    etp: CanTestExtra = {
-        "step_no": step_no,
-        "purpose" : purpose,
-        "timeout" : 1,
-        "min_no_messages" : -1,
-        "max_no_messages" : -1
-    }
-    SIO.extract_parameter_yml(str(inspect.stack()[0][3]), etp)
+    if result.data["details"]["mode"] == ecu_session :
+        logging.info("ECU is in mode %s as expected", ecu_session)
+        return True
 
-    # Read 10 bytes from memory position 0x70000000-0x7000000A using alfid id 0x24.
-    cpay: CanPayload = {
-        "payload": b'\x23\x24\x70\x00\x00\x00\x00\x0A',
-        "extra": ''
-    }
-    SIO.extract_parameter_yml(str(inspect.stack()[0][3]), cpay)
+    logging.error("Test Failed: Expected session %s, received session %s",
+                  ecu_session, result.data["details"]["mode"])
+    return False
 
-    result = SUTE.teststep(can_p, cpay, etp)
 
-    # Check if session is programming mode.
-    if session == PRG_SESSION:
-        result = evaluate_response(can_p, '037F23')
-    else:
-        result = evaluate_response(can_p, '100B63')
-
-    return result
-
-def print_nrc(can_p):
+def verify_service_23_and_alfid(dut, alfid, message, session):
     """
-    Get and log NRC (Negative return code).
+    Verify readMemoryByAddress(0x23) with ALFID 0x14 and ALFID 0x24
+    Args:
+        dut (Dut): An instance of Dut
+        alfid (str): 0x14, 0x24
+        message (str): Message
+        session (str): Diagnostic session
+    Return:
+        (bool): True when received positive response '63' for default and extended session
+                and negative response '7F' and NRC-11(serviceNotSupported) for programming session
     """
-    logging.info("NRC: %s",
-                 SUTE.pp_can_nrc(
-                 SC.can_messages[can_p["receive"]][0][2][6:8]))
+    response = request_read_memory_by_address(dut, alfid, message)
 
-def evaluate_response(can_p, valid_data):
-    """
-    Log result and return data comparison result (true/false)
-    """
-    # Check if empty
-    if not can_p["receive"]:
-        logging.info("No data in can_p['receive']")
+    if session == 'programming':
+        if response[2:4] == '7F' and response[6:8] == '11':
+            logging.info("Successfully verified readMemoryByAddress is not supported for ALFID"
+                         " 0x%s in %s session as expected", alfid, session)
+            return True
+
+        logging.error("Test Failed: Expected negative response '7F' and "
+                      "NRC-11(serviceNotSupported) in %s session, received %s", session, response)
         return False
 
-    nrc = SC.can_messages[can_p["receive"]][0][2][0:4] == '037F'
-    result = SUTE.test_message(SC.can_messages[can_p["receive"]],
-                               teststring=valid_data)
-    if result:
-        logging.info("Result teststep: True")
-    else:
-        logging.info("Result teststep: Failed")
+    if response[4:6] == '63':
+        logging.info("Successfully verified readMemoryByAddress for ALFID 0x%s in %s session",
+                      alfid, session)
+        return True
 
-    # Check if it was a negative return code. If so, print.
-    if nrc:
-        print_nrc(can_p)
+    logging.error("Test Failed: Expected positive response '63' in %s session, received %s",
+                  session, response)
+    return False
 
-    # If its not a NRC possible corrupt message.
-    if not result and not nrc:
-        logging.info("Unexpected data received: %s",
-                     SC.can_messages[can_p["receive"]][0][2])
-    # Log a new line
-    logging.info(" ")
-    return result
+
+def step_1(dut: Dut, parameters):
+    """
+    action: Verify readMemoryByAddress(0x23) with ALFID 0x14 and ALFID 0x24 in default session
+    expected_result: True when received positive response '63'
+    """
+    result = verify_service_23_and_alfid(dut, parameters['alfid_14'],
+             parameters['message_14'], session='default')
+    if not result:
+        return False
+
+    result = verify_service_23_and_alfid(dut, parameters['alfid_24'],
+             parameters['message_24'], session='default')
+    if not result:
+        return False
+
+    return True
+
+
+def step_2(dut: Dut, parameters):
+    """
+    action: Verify readMemoryByAddress(0x23) with ALFID 0x14 and ALFID 0x24 in extended session
+    expected_result: True on receiving positive response '63'
+    """
+    # Set to extended session
+    dut.uds.set_mode(3)
+
+    result = verify_active_diagnostic_session(dut, ecu_session=3)
+    if not result:
+        return False
+
+    result = verify_service_23_and_alfid(dut, parameters['alfid_14'],
+             parameters['message_14'], session='extended')
+    if not result:
+        return False
+
+    result = verify_service_23_and_alfid(dut, parameters['alfid_24'],
+             parameters['message_24'], session='extended')
+    if not result:
+        return False
+
+    return True
+
+
+def step_3(dut: Dut, parameters):
+    """
+    action: Verify readMemoryByAddress(0x23) with ALFID 0x14 and ALFID 0x24 in programming session
+    expected_result: True when received negative response '7F' and NRC-11(serviceNotSupported)
+    """
+    # Set to programming session
+    dut.uds.set_mode(2)
+
+    result = verify_active_diagnostic_session(dut, ecu_session=2)
+    if not result:
+        return False
+
+    result = verify_service_23_and_alfid(dut, parameters['alfid_14'],
+             parameters['message_14'], session='programming')
+    if not result:
+        return False
+
+    result = verify_service_23_and_alfid(dut, parameters['alfid_24'],
+             parameters['message_24'], session='programming')
+    if not result:
+        return False
+
+    return True
+
 
 def run():
     """
-    Run - Call other functions from here
+    Verify readMemoryByAddress(0x23) with ALFID's(0x14, 0x24) is supported in default and
+    extended session and not in programming session.
     """
-    logging.basicConfig(
-        format=' %(message)s', stream=sys.stdout, level=logging.INFO)
+    dut = Dut()
 
-    # where to connect to signal_broker
-    can_p: CanParam = {
-        "netstub" : SC.connect_to_signalbroker(
-            odtb_conf.ODTB2_DUT, odtb_conf.ODTB2_PORT),
-        "send" : "Vcu1ToBecmFront1DiagReqFrame",
-        "receive" : "BecmToVcu1Front1DiagResFrame",
-        "namespace" : SC.nspace_lookup("Front1CANCfg0")
-    }
-    SIO.extract_parameter_yml(str(inspect.stack()[0][3]), can_p)
+    start_time = dut.start()
+    result = False
+    result_step = False
 
-    logging.info("Testcase start: %s", datetime.now())
-    starttime = time.time()
-    logging.info("Time: %s \n", time.time())
+    parameters_dict = {'message_14': '',
+                       'message_24': '',
+                       'alfid_14':'',
+                       'alfid_24':''}
+    try:
+        dut.precondition(timeout=60)
 
-    ############################################
-    # precondition
-    ############################################
-    timeout = 40
-    result = PREC.precondition(can_p, timeout)
+        parameters = SIO.parameter_adopt_teststep(parameters_dict)
+        if not all(list(parameters.values())):
+            raise DutTestError("yml parameters not found")
 
-    if result:
-        ############################################
-        # teststeps
-        ############################################
-        # step 1:
-        # action: Make sure default session is set.
-        # result: ECU responds with positive message.
-        result = result and SE10.diagnostic_session_control_mode1(can_p, 1)
+        result_step = dut.step(step_1, parameters, purpose="Verify readMemoryByAddress(0x23)"
+                                                            " with ALFID 0x14 and ALFID 0x24 in"
+                                                            " default session")
+        if result_step:
+            result_step = dut.step(step_2, parameters, purpose="Verify readMemoryByAddress(0x23)"
+                                                               " with ALFID 0x14 and ALFID 0x24 in"
+                                                               " extended session")
+        if result_step:
+            result_step = dut.step(step_3, parameters, purpose="Verify readMemoryByAddress(0x23)"
+                                                                " with ALFID 0x14 and ALFID 0x24"
+                                                                " in programming session")
+        result = result_step
 
-        # step 2:
-        # action: Send ReadMemoryByAddress(ALFID: 0x14)
-        # result: ECU responds with positive message
-        result = result and read_memory_by_address_0x14(can_p, DEF_SESSION, 2)
+    except DutTestError as error:
+        logging.error("Test failed: %s", error)
+    finally:
+        dut.postcondition(start_time, result)
 
-        # step 3:
-        # action: Send ReadMemoryByAddress(ALFID: 0x24)
-        # result: ECU responds with positive message
-        result = result and read_memory_by_address_0x24(can_p, DEF_SESSION, 3)
-
-        # step 4:
-        # action: Change to extended session.
-        # result: ECU responds with positive message.
-        result = result and SE10.diagnostic_session_control_mode3(can_p, 4)
-
-        # step 5:
-        # action: Send ReadMemoryByAddress(ALFID: 0x14)
-        # result: ECU responds with positive message
-        result = result and read_memory_by_address_0x14(can_p, EXT_SESSION, 5)
-
-        # step 6:
-        # action: Send ReadMemoryByAddress(ALFID: 0x24)
-        # result: ECU responds with positive message
-        result = result and read_memory_by_address_0x24(can_p, EXT_SESSION, 6)
-
-        # step 7:
-        # action: Change to programming session.
-        # result: ECU responds with positive message.
-        result = result and SE10.diagnostic_session_control_mode2(can_p, 7)
-
-        # step 8:
-        # action: Send ReadMemoryByAddress(ALFID: 0x14)
-        # result: ECU responds with negative message
-        result = result and read_memory_by_address_0x14(can_p, PRG_SESSION, 8)
-
-        # step 9:
-        # action: Send ReadMemoryByAddress(ALFID: 0x24)
-        # result: ECU responds with negative message
-        result = result and read_memory_by_address_0x24(can_p, PRG_SESSION, 9)
-
-    ############################################
-    # postCondition
-    ############################################
-    POST.postcondition(can_p, starttime, result)
 
 if __name__ == '__main__':
     run()
