39e6e12521bacdcae8964bc65b02592338be1536
Solanki, Jayraj <jayraj.solanki@volvocars.com>
2022-09-30 09:35:51 +0200

    Adding ReqId - 129845, Secondary Bootloader Software Version Number data record

--- test_folder/automated/e_129845_MAIN_0_sbl_sw_vn_data_record.py
+++ test_folder/automated/e_129845_MAIN_0_sbl_sw_vn_data_record.py
@@ -4,7 +4,7 @@
 
 
 
-Copyright © 2021 Volvo Car Corporation. All rights reserved.
+Copyright © 2022 Volvo Car Corporation. All rights reserved.
 
 
 
@@ -18,223 +18,132 @@ Any unauthorized copying or distribution of content from this file is prohibited
 
 /*********************************************************************************/
 
-# Testscript Hilding MEPII
-# project:  ECU basetech MEPII
-# author:   J-ADSJO (Johan Adsjö)
-# date:     2021-01-11
-# version:  1.0
-# reqprod:  129845
+reqprod: 129845
+version: 0
+title: Secondary Bootloader Software Version Number data record
+purpose: >
+    To enable readout of the version number of the Secondary Bootloader SW.
 
-# inspired by https://grpc.io/docs/tutorials/basic/python.html
+description: >
+    A data record with identifier as specified in the table below shall be implemented exactly as
+    defined in Carcom - Global Master Reference Database (GMRDB).
+    --------------------------------------------------------------------------------
+    Description                                                         Identifier
+    --------------------------------------------------------------------------------
+    Secondary Bootloader Software Version Number data record            F124
+    --------------------------------------------------------------------------------
 
-# Copyright 2015 gRPC authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+    •   It shall be possible to read the data record by using the diagnostic service specified in
+        Ref[LC : Volvo Car Corporation - UDS Services - Service 0x22 (ReadDataByIdentifier) Reqs].
+    •   The identifier shall be BCD encoded, right justified and all unused digit shall be filled
+        with 0.
 
-The Python implementation of the gRPC route guide client.
+    The ECU shall support the identifier in the following sessions:
+    •   Programming session (which includes secondary bootloader).
+
+details: >
+    Verify response and part number of DID 'F124' in PBL and SBL.
 """
 
-import time
-from datetime import datetime
-import sys
 import logging
-import odtb_conf
-
-from supportfunctions.support_can import SupportCAN, CanParam, CanTestExtra, CanPayload
-from supportfunctions.support_test_odtb2 import SupportTestODTB2
-from supportfunctions.support_carcom import SupportCARCOM
-from supportfunctions.support_file_io import SupportFileIO
-from supportfunctions.support_precondition import SupportPrecondition
-from supportfunctions.support_postcondition import SupportPostcondition
+from hilding.dut import Dut
+from hilding.dut import DutTestError
 from supportfunctions.support_SBL import SupportSBL
-from supportfunctions.support_service22 import SupportService22
-from supportfunctions.support_service10 import SupportService10
-from supportfunctions.support_sec_acc import SecAccessParam
+from supportfunctions.support_file_io import SupportFileIO
+from supportfunctions.support_test_odtb2 import SupportTestODTB2
 
-SIO = SupportFileIO
-SC = SupportCAN()
-SUTE = SupportTestODTB2()
-SC_CARCOM = SupportCARCOM()
 SSBL = SupportSBL()
-PREC = SupportPrecondition()
-POST = SupportPostcondition()
-SE10 = SupportService10()
-SE22 = SupportService22()
+SIO = SupportFileIO
+SUTE = SupportTestODTB2()
+
 
 def validate_and_get_pn_f124(message):
-    '''
-    Validate and pretty print ECU Delivery Assembly Part Number
-    '''
-    pos = message.find('F124')
-    valid = SUTE.validate_part_number_record(message[pos+4:pos+18])
-    part_number = SUTE.pp_partnumber(message[pos+4:pos+18])
-    logging.info("SBL Software Version Number: %s", part_number)
-    return valid
-
-def step_3(can_p):
     """
-    Teststep 3: send requests DID F124 - in Programming Session
-      Expect: Negative response: Service: 22, requestOutOfRange (3100000000)
+    Validate and pretty print ECU delivery assembly part number.
+    Args:
+        message (str): ECU response message
+    Returns:
+        (bool): Boolean based on part number validity.
     """
-    # Parameters for the teststep
-    cpay: CanPayload = {
-        "payload": SC_CARCOM.can_m_send("ReadDataByIdentifier",
-                                        b'\xF1\x24',
-                                        b''),
-        "extra": ''
-        }
-    SIO.parameter_adopt_teststep(cpay)
-    etp: CanTestExtra = {
-        "step_no": 3,
-        "purpose": "Request DID F124 - in Programming Session",
+    return SUTE.validate_serial_number_record(message[16:])
 
-        "timeout": 1,
-        "min_no_messages": -1,
-        "max_no_messages": -1
-        }
-    SIO.parameter_adopt_teststep(etp)
 
-    result = SUTE.teststep(can_p, cpay, etp)
-    time.sleep(1)
-    # Negative response: Service: 22, requestOutOfRange (3100000000)
-    result = result and SUTE.test_message(SC.can_messages[can_p["receive"]], teststring='7F2231')
-
-    pbl_f124_result = SC.can_messages[can_p["receive"]][0][2]
-    #result = result and validate_and_get_pn_f124(pbl_f124_result)
-
-    logging.info(pbl_f124_result)
-    logging.debug("\nTeststep 3_result: %s\n", result)
-
-    return result
-
-def step_5(can_p):
+def step_1(dut: Dut, did):
     """
-    Teststep 5: send requests DID F124 - with SBL active
+    action: Set to programming session and verify DID 'F124' from PBL.
+    expected_result: ECU sends NRC-31(requestOutOfRange).
     """
-    # Parameters for the teststep
-    cpay: CanPayload = {
-        "payload": SC_CARCOM.can_m_send("ReadDataByIdentifier",
-                                        b'\xF1\x24',
-                                        b''),
-        "extra": ''
-        }
-    SIO.parameter_adopt_teststep(cpay)
-    etp: CanTestExtra = {
-        "step_no": 5,
-        "purpose": "Request DID F124 - with SBL Activated",
-        "timeout": 1,
-        "min_no_messages": -1,
-        "max_no_messages": -1
-        }
-    SIO.parameter_adopt_teststep(etp)
+    # Set to programming session
+    dut.uds.set_mode(2)
 
-    result = SUTE.teststep(can_p, cpay, etp)
-    time.sleep(1)
+    response = dut.uds.read_data_by_id_22(bytes.fromhex(did))
+    if response.raw[2:4] == '7F' and response.raw[6:8] == '31':
+        logging.info("Received NRC-%s as expected", response.raw[6:8])
+        return True
 
-    result = result and SUTE.test_message(SC.can_messages[can_p["receive"]], teststring='F124')
-    logging.info(SC.can_messages[can_p["receive"]])
+    logging.error("Test Failed: Expected negative response with NRC-31(requestOutOfRange), "
+                  "received %s", response.raw)
+    return False
 
-    sbl_f124_result = SC.can_messages[can_p["receive"]][0][2]
-    result = result and validate_and_get_pn_f124(sbl_f124_result)
 
-    return result
+def step_2(dut: Dut, did):
+    """
+    action: Activate SBL and verify did 'F124'.
+    expected_result: ECU sends positive response and part number is verified.
+    """
+    SSBL.get_vbf_files()
+    result_sbl = SSBL.sbl_activation(dut, sa_keys=dut.conf.default_rig_config)
+
+    if result_sbl is False:
+        logging.error("Test Failed: SBL activation failed")
+        return False
+
+    response = dut.uds.read_data_by_id_22(bytes.fromhex(did))
+    if response.raw[4:6] != '62' and response.raw[6:10] != did:
+        logging.error("Test Failed: Expected positive response '62' for "
+                      "requestReadDataByIdentifier(0x22), received %s", response.raw)
+        return False
+
+    result = validate_and_get_pn_f124(message=response.raw)
+    if result is False:
+        logging.error("Test Failed: Invalid part number received")
+        return False
+
+    logging.info("Valid part number received")
+    return True
 
 
 def run():
     """
-    Run - Call other functions from here
+    Verify response and part number of DID 'F124' in PBL and SBL.
     """
+    dut = Dut()
 
-    logging.basicConfig(format=' %(message)s', stream=sys.stdout, level=logging.INFO)
+    start_time = dut.start()
+    result = False
+    result_step = False
 
-    # where to connect to signal_broker
-    can_p: CanParam = {
-        "netstub" : SC.connect_to_signalbroker(odtb_conf.ODTB2_DUT, odtb_conf.ODTB2_PORT),
-        "send" : "Vcu1ToECUFront1DiagReqFrame",
-        "receive" : "ECUToVcu1Front1DiagResFrame",
-        "namespace" : SC.nspace_lookup("Front1CANCfg0")
-    }
-    SIO.parameter_adopt_teststep(can_p)
+    parameters_dict = {'did': ''}
 
-    #Init parameter for SecAccess Gen1/Gen2
-    sa_keys: SecAccessParam = {
-        "SecAcc_Gen": 'Gen1',
-        "fixed_key": 'FFFFFFFFFF',
-        "auth_key": 'FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF',
-        "proof_key": 'FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF'
-    }
-    SIO.parameter_adopt_teststep(sa_keys)
+    try:
+        dut.precondition(timeout=80)
 
-    logging.info("Testcase start: %s", datetime.now())
-    starttime = time.time()
-    logging.info("Time: %s \n", time.time())
+        parameters = SIO.parameter_adopt_teststep(parameters_dict)
+        if not all(list(parameters.values())):
+            raise DutTestError("yml parameter not found")
 
-    ############################################
-    # precondition
-    ############################################
-    # read VBF param when testscript is s started, if empty take default param
-    SSBL.get_vbf_files()
-    timeout = 2000
-    result = PREC.precondition(can_p, timeout)
+        result_step = dut.step(step_1, parameters['did'], purpose="Set to programming session and "
+                                                                  "verify DID 'F124' from PBL")
+        if result_step:
+            result_step = dut.step(step_2, parameters['did'], purpose="Activate SBL and verify "
+                                                                      "did 'F124'")
+        result = result_step
 
-    if result:
-    ############################################
-    # teststeps
-    ############################################
+    except DutTestError as error:
+        logging.error("Test failed: %s", error)
+    finally:
+        dut.postcondition(start_time, result)
 
-    # step 1:
-    # action: Check if Default session
-    # result: ECU reports mode
-        result = result and SE22.read_did_f186(can_p, b'\x01', stepno=1)
-        time.sleep(1)
-
-    # step 2:
-    # action: Change to Programming session
-    # result: ECU reports mode
-        result = result and SE10.diagnostic_session_control_mode2(can_p, 2)
-        time.sleep(1)
-
-    # step 3:
-    # action: read DID F124 from PBL
-    # result: Negative response: Service: 22, requestOutOfRange (3100000000)
-        result = result and step_3(can_p)
-        time.sleep(1)
-
-    # step 4:
-    # action: DL and activate SBL
-    # result: ECU reports mode
-        result = result and SSBL.sbl_activation(can_p,
-         sa_keys, stepno='4', purpose="DL and activate SBL")
-        time.sleep(1)
-
-    # step 5:
-    # action: read DID F124 from SBL
-    # result: Data record is returned
-        result = result and step_5(can_p)
-        time.sleep(1)
-
-    # step 6:
-    # action: Set to Default session before leaving
-    # result: ECU reports modes
-        result_end = SE10.diagnostic_session_control_mode1(can_p, 6)
-        time.sleep(1)
-        result = result and result_end
-
-    ############################################
-    # postCondition
-    ############################################
-
-    POST.postcondition(can_p, starttime, result)
 
 if __name__ == '__main__':
     run()
