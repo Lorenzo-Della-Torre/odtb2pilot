e19613531230346edec18e1914c863333e51ed3c
Khairkar, Khushboo <khushboo.khairkar@volvocars.com>
2022-10-05 13:09:05 +0200

    Adding REQID-68264, DTCs defined by GMRDB

--- test_folder/automated/e_68264_MAIN_2_dtc_defined_by_gmrdb.py
+++ test_folder/automated/e_68264_MAIN_2_dtc_defined_by_gmrdb.py
@@ -4,7 +4,7 @@
 
 
 
-Copyright © 2021 Volvo Car Corporation. All rights reserved.
+Copyright © 2022 Volvo Car Corporation. All rights reserved.
 
 
 
@@ -18,7 +18,27 @@ Any unauthorized copying or distribution of content from this file is prohibited
 
 /*********************************************************************************/
 
-Import script - Inherited from older version of requirement
+reqprod: 68264
+version: 1
+title: DTCs defined by GMRDB
+purpose: >
+	All DTC information shall be supported by VCC tools and must therefore be defined in GMRDB.
+
+description: >
+    Rationale-
+    Global Master Reference Data Base (GMRDB) is a part of the central diagnostic database that
+    is used by Volvo Car Corporation in order to document the implementation of diagnostics in the
+    ECUs. GMRDB is a library containing predefined DTCs, DIDs and Control Routines. The definition
+    of DTCs (both identifier and description) that are supposed to be used by Volvo tools must
+    have its origin in GMRDB. GMRDB holds only the 2-byte base DTC.
+
+    Requirement-
+    DTCs supported by an ECU shall be implemented in accordance to the definition in Global Master
+    Reference Database. Development specific implementer specified DTCs are excluded from this
+    requirement.
+
+details: >
+    Import script - Inherited from older version of requirement
 """
 
 from e_68264_MAIN_1_dtc_defined_by_gmrdb import run
