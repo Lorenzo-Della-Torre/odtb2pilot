857781ab7c6760f8721878834086be3616de2b1d
pi13 <pi13@example.com>
2022-10-25 15:31:36 +0100

    Padding True hardcode changed

--- supportfunctions/support_test_odtb2.py
+++ supportfunctions/support_test_odtb2.py
@@ -133,7 +133,7 @@ class SupportTestODTB2: # pylint: disable=too-many-public-methods
         logging.debug("To send:   [%s, %s, %s]", time.time(), can_p["send"],
                       (cpay["payload"]).hex().upper())
         SC.clear_all_can_messages()
-        SC.t_send_signal_can_mf(can_p, cpay, True, 0x00)
+        SC.t_send_signal_can_mf(can_p, cpay, can_p.padding, 0x00)
         #wait timeout for getting subscribed data
         if (wait_max or (etp["max_no_messages"] == -1)):
             time.sleep(etp["timeout"])
