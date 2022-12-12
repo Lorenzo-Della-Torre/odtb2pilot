9e9a3b0bb5cac3c4511a41acbbc73f5b132599b1
Bhale, Ajinkya <ajinkya.bhale@volvocars.com>
2022-12-12 09:36:27 +0100

    Fixed Blacklist

---
+++ test_folder/manual/e_397436_MAIN_2_reprogrammable_ecu_ess.py
@@ -0,0 +1,30 @@
+"""
+
+/*********************************************************************************/
+
+
+
+Copyright © 2021 Volvo Car Corporation. All rights reserved.
+
+
+
+NOTICE:
+This file contains material that is confidential and confidential to Volvo Cars and/or
+other developers. No license is granted under any intellectual or industrial property
+rights of Volvo Cars except as may be provided in an agreement with Volvo Cars.
+Any unauthorized copying or distribution of content from this file is prohibited.
+
+
+
+/*********************************************************************************/
+
+Implicitly tested script
+
+Tested implicitly by REQPROD_411891_Immediately_apply_ESS_update
+"""
+
+
+from e_411891_MAIN_1_immediatley_apply_ess_updates import run
+
+if __name__ == '__main__':
+    run()
