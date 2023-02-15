fbaf5ec5bce20631c8ec6c3093fd887a91143fdf
Khairkar <KKHAIRKA@volvocars.com>
2023-02-15 05:35:24 +0100

    Adding REQID-113482, Session Layer

--- test_folder/automated/e_113482_MAIN_3_session_layer.py
+++ test_folder/automated/e_113482_MAIN_3_session_layer.py
@@ -4,7 +4,7 @@
 
 
 
-Copyright © 2021 Volvo Car Corporation. All rights reserved.
+Copyright © 2023 Volvo Car Corporation. All rights reserved.
 
 
 
@@ -18,12 +18,28 @@ Any unauthorized copying or distribution of content from this file is prohibited
 
 /*********************************************************************************/
 
-Implicitly tested script
+reqprod: 113482
+version: 3
+title: Session Layer
+purpose: >
+    Standardise communication to ensure all ECUs uses the same diagnostic communication
+    specifications. ISO standard shall be followed to reduce cost and make implementation easier
 
-Tested implicitly by all diagnostic tests, e.g. by REQPROD 76170
+description: >
+    The session layer defined in Road vehicles — Unified diagnostic services — Part 2: Session
+    layer services Edition 1: 2013 shall be implemented with restriction/additions defined by the
+    following VCC requirements.
+
+details: >
+    Implicitly tested script
+    Tested implicitly by all diagnostic tests, e.g. by REQPROD 76170
 """
+import logging
+import sys
+from e_76170_MAIN_1_service_22_dids import run
 
-from e_76170_MAIN_2_service_22_dids import run
+logging.basicConfig(format=' %(message)s',stream=sys.stdout, level=logging.INFO)
+
 
 if __name__ == '__main__':
     run()
