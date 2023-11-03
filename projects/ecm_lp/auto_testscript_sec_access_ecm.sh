9be8f32e8257766c7e1d146ba4554744fa8941a1
Lorenzo DELLA TORRE <LDELLA@aurobay.com>
2023-11-03 14:15:07 +0100

    Update dir and yml file to include LP

---
+++ projects/ecm_lp/auto_testscript_sec_access_ecm.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+    export TESTREPO=~/Repos/odtb2pilot
+### TESTREPO and ODTBPRO taken from environment variables
+    ODTBPROJPARAM=$TESTREPO/projects/$ODTBPROJ
+    echo export ODTBPROJPARAM=$ODTBPROJPARAM
+
+### show environment variables used in automated testrun:
+    echo Variables used in testrun:
+    echo TESTREPO: $TESTREPO
+    echo ODTBPROJPARAM $ODTBPROJPARAM
+    echo PYTHONPATH: $PYTHONPATH
+    echo PATH: $PATH
+<< com
+### VBF files update in $TESTREPO/projects/$ODTBPROJ
+    [ ! -d $TESTREPO/projects/$ODTBPROJ/VBF ] && mkdir $TESTREPO/projects/$ODTBPROJ/VBF
+    rm -f $TESTREPO/projects/$ODTBPROJ/VBF/*
+    cp ~/delivery/*.vbf $TESTREPO/projects/$ODTBPROJ/VBF
+    cp ~/SBL/*.vbf $TESTREPO/projects/$ODTBPROJ/VBF
+com
+
+### uncomment if VBF should be updated in local catalog:
+###    [ ! -d VBF ] && mkdir VBF
+###    rm -f VBF/*
+###    cp ~/delivery/*.vbf VBF
+###    cp ~/SBL/*.vbf VBF
+
+
+### Generate catalog for logfiles and list of scripts to run
+    TESTRUN=$(date +Testrun_%Y%m%d_%H%M_sec_access_ECM)
+    [ ! -d $TESTRUN ] && mkdir $TESTRUN
+    echo "Results of testrun $TESTRUN:" >$TESTRUN\/Result.txt
+
+	# SWRS Testing
+	python3 $TESTREPO/autotest/testcase_picker.py --txtfile $TESTREPO/test_folder/automated/sec_access_test.txt --scriptfolder $TESTREPO/test_folder/automated > testscripts_auto.lst
+
+    ### Run all testscripts found:
+    while IFS= read -r line
+    do
+		echo "Origin: $line"
+		echo $line | sed -E "s/(.*e_)([0-9]{3,}.*)(\.py)/python3 \1\2\3 >$TESTRUN\/e_\2.log/"
+                script2run_log=$(echo $line | sed -E "s/(.*e_)([0-9]{3,}.*)(\.py)/e_\2.log/")
+		echo $script2run_log
+		script2run=$(echo $line | sed -E "s/(.*e_)([0-9]{3,}.*)(\.py)/e_\2.py/")
+	        echo $script2run
+       # echo "Set ECU to default"
+#		python3 $TESTREPO/test_folder/on_the_fly_test/BSW_Set_ECU_to_default.py >/dev/null
+        python3 $TESTREPO/manage.py run $script2run >$TESTRUN/$script2run_log
+        ### add REQ_NR, scriptresult, filename to result
+        req_tested=$(echo $line | sed -E "s/(.*?e_)([0-9]{3,})(_.*)/\2/")
+        testresult=$(tail -1 $TESTRUN/$script2run_log | sed -E "s/(Testcase result: )(.*)/\2/")
+		echo "Requirement: $req_tested"
+		echo "Testresult: $testresult"
+		echo "Log: $script2run_log"
+        echo "$req_tested $testresult $script2run_log" >>$TESTRUN\/Result.txt
+    done <testscripts_auto.lst
+
+
+    echo
+    date "+Test done. Time: %Y%m%d %H%M" 
+    date "+Test done. Time: %Y%m%d %H%M" >>$TESTRUN\/Result.txt
